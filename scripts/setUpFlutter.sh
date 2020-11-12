1d22b39d3b7b340e0d5c1a6c2122d249884fdf85
Elias Keis <git-commits@elkei.de>
2020-11-12 21:33:33 +0100

    use file encryption method for secrets.json

--- scripts/setUpFlutter.sh
+++ scripts/setUpFlutter.sh
@@ -2,4 +2,4 @@ cd frontend
 git clone https://github.com/flutter/flutter.git -b stable
 ./flutter/bin/flutter doctor
 
-echo $SECRETS_JSON > secrets.json
+openssl aes-256-cbc -K $encrypted_fb088dcee1f3_key -iv $encrypted_fb088dcee1f3_iv -in secrets.json.enc -out secrets.json -d
