f3711e125c7efb156344710f8838d0d26a188416
Elias Keis <git-commits@elkei.de>
2020-11-12 17:53:54 +0100

    adapt broken smoke test

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: HomePage(title: 'Home Page'),
+      home: HomePage(title: 'Home'),
     );
   }
 }
