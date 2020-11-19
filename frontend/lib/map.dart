b54182bd6d1ff487c8f8e363aa9b77ede184c1c4
Maximilian Ammann <max@maxammann.org>
2020-11-19 13:36:39 +0100

    Add geojson layer

--- frontend/lib/map.dart
+++ frontend/lib/map.dart
@@ -1,8 +1,12 @@
+import 'dart:convert';
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
 import 'package:mapbox_gl/mapbox_gl.dart';
 
 class FullMap extends StatefulWidget {
   final String mapboxToken;
+
   const FullMap(this.mapboxToken);
 
   @override
@@ -17,15 +21,27 @@ class FullMapState extends State<FullMap> {
 
   void _onMapCreated(MapboxMapController controller) {
     mapController = controller;
+    loadData();
+  }
+
+  void loadData() async {
+    String data = await DefaultAssetBundle.of(context)
+        .loadString("verguenstigungen.json");
+    ByteData marker = await DefaultAssetBundle.of(context)
+        .load("custom_marker.png");
+    mapController.addImage('custom-marker', marker.buffer.asUint8List());
+    mapController.addSource("sourceId", data);
+    mapController.addSymbolLayer("sourceId", "layer", {
+      'icon-image': '["image", "custom-marker"]',
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return new MapboxMap(
-        accessToken: this.mapboxToken,
-        onMapCreated: _onMapCreated,
-        initialCameraPosition:
-        const CameraPosition(target: LatLng(0.0, 0.0)),
+      accessToken: this.mapboxToken,
+      onMapCreated: _onMapCreated,
+      initialCameraPosition: const CameraPosition(target: LatLng(0.0, 0.0)),
     );
   }
 }
