87c7c8aa76271c6e7b9dc543d0e0346cfc895f8c
Elias Keis <git-commits@elkei.de>
2020-11-26 15:33:52 +0100

    #25 show name of accepting business

--- frontend/lib/map/full_map.dart
+++ frontend/lib/map/full_map.dart
@@ -1,26 +1,43 @@
+import 'dart:convert';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:mapbox_gl/mapbox_gl.dart';
 
-class FullMapState extends State<FullMap> {
-  final String mapboxToken;
+typedef void OnFeatureClickCallback(dynamic feature);
 
-  FullMapState(this.mapboxToken);
+class FullMapState extends State<FullMap> {
+  final OnFeatureClickCallback onFeatureClick;
+  final String _mapboxToken;
+  MapboxMapController _controller;
+
+  FullMapState(this._mapboxToken, this.onFeatureClick);
 
   @override
   Widget build(BuildContext context) {
     return new MapboxMap(
-      accessToken: this.mapboxToken,
+      accessToken: this._mapboxToken,
       initialCameraPosition: const CameraPosition(target: LatLng(0.0, 0.0)),
       styleString: "mapbox://styles/elkei24/ckhyn5h2l1yq519r9g3fbsogj",
+      onMapCreated: (controller) => {this._controller = controller},
+      onMapClick: this.onMapClick,
     );
+  }
+
+  void onMapClick(Point<double> point, LatLng coordinates) {
+    this._controller.queryRenderedFeatures(point, [], null).then((features) => {
+          if (features?.isNotEmpty)
+            {this.onFeatureClick(json.decode(features[0]))}
+        });
   }
 }
 
 class FullMap extends StatefulWidget {
   final String mapboxToken;
+  final OnFeatureClickCallback onFeatureClick;
 
-  const FullMap(this.mapboxToken);
+  const FullMap(this.mapboxToken, this.onFeatureClick);
 
   @override
-  State createState() => FullMapState(this.mapboxToken);
+  State createState() => FullMapState(this.mapboxToken, this.onFeatureClick);
 }
