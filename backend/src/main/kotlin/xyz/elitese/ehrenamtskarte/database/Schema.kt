f7e8b2b7f2bb226569f68acc20a59ca878c917c0
Roman Buchholz <roman.buchholz@t-online.de>
2020-12-03 14:45:39 +0100

    #43 resolve rebase conflicts

--- backend/src/main/kotlin/xyz/elitese/ehrenamtskarte/database/Schema.kt
+++ backend/src/main/kotlin/xyz/elitese/ehrenamtskarte/database/Schema.kt
@@ -4,6 +4,11 @@ import org.jetbrains.exposed.dao.IntEntity
 import org.jetbrains.exposed.dao.IntEntityClass
 import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.dao.id.IntIdTable
+import org.jetbrains.exposed.sql.Column
+import org.jetbrains.exposed.sql.ColumnType
+import org.jetbrains.exposed.sql.Table
+import org.postgis.PGgeometry
+import org.postgis.Point
 
 object Categories : IntIdTable() {
     val name = varchar("name", 50)
@@ -31,6 +36,7 @@ class ContactEntity(id: EntityID<Int>): IntEntity(id) {
 
 object AcceptingStores : IntIdTable() {
     val name = varchar("name", 50)
+    val location  = point("location")
     val contactId = reference("contactId", Contacts)
     val categoryId = reference("categoryId", Categories)
 }
@@ -40,4 +46,19 @@ class AcceptingStoreEntity(id: EntityID<Int>) : IntEntity(id) {
     var name by AcceptingStores.name
     var contactId by AcceptingStores.contactId
     var categoryId by AcceptingStores.categoryId
+}
+
+fun Table.point(name: String, srid: Int = 4326): Column<Point>
+        = registerColumn(name, PointColumnType())
+
+private class PointColumnType(val srid: Int = 4326): ColumnType() {
+    override fun sqlType() = "GEOMETRY(Point, $srid)"
+    override fun valueFromDB(value: Any) = if (value is PGgeometry) value.geometry else value
+    override fun notNullValueToDB(value: Any): Any {
+        if (value is Point) {
+            if (value.srid == Point.UNKNOWN_SRID) value.srid = srid
+            return PGgeometry(value)
+        }
+        return value
+    }
 }
