ca39b1198cf9442aa86247c5470e8d800afb6eac
Elias Keis <git-commits@elkei.de>
2020-12-03 11:00:21 +0100

    #26 request permission if status is unknown

--- frontend/lib/map/locationPermissionRequester.dart
+++ frontend/lib/map/locationPermissionRequester.dart
@@ -4,7 +4,7 @@ import 'package:location_permissions/location_permissions.dart';
 Future<PermissionStatus> requestLocationPermissionIfNotYetGranted(
     LocationPermissionLevel level) async {
   var status = await LocationPermissions().checkPermissionStatus(level: level);
-  if (status == PermissionStatus.denied) {
+  if (status == PermissionStatus.denied || status != PermissionStatus.unknown) {
     return await LocationPermissions()
         .requestPermissions(permissionLevel: level);
   }
