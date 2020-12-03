514a386c474278a12f13d6cd72f7df71c5c252c0
Maximilian Ammann <max@maxammann.org>
2020-12-03 20:12:24 +0100

    Execute scripts in backend

--- backend/src/main/kotlin/xyz/elitese/ehrenamtskarte/database/Database.kt
+++ backend/src/main/kotlin/xyz/elitese/ehrenamtskarte/database/Database.kt
@@ -1,12 +1,28 @@
 package xyz.elitese.ehrenamtskarte.database
 
-import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.Database.Companion.connect
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.StdOutSqlLogger
+import org.jetbrains.exposed.sql.addLogger
+import org.jetbrains.exposed.sql.transactions.TransactionManager
 import org.jetbrains.exposed.sql.transactions.transaction
+import java.nio.file.Files
+import java.nio.file.Paths
 
 class Database {
 
     companion object {
+        private fun executeScript(path: String) {
+            val java = Database::class.java
+            val resource = java.classLoader.getResource(path) ?: throw Exception("Failed to find script")
+
+            val uri = resource.toURI()
+            val sql = String(Files.readAllBytes(Paths.get(uri)));
+            with(TransactionManager.current()) {
+                exec(sql)
+            }
+        }
+
         fun setup() {
             Database().db
 
@@ -19,6 +35,9 @@ class Database {
                         Contacts,
                         AcceptingStores
                 )
+
+                executeScript("sql/create_tilebbox.sql")
+                executeScript("sql/create_accepting_stores_clustered.sql")
             }
         }
     }
@@ -29,7 +48,7 @@ class Database {
         val user = System.getProperty("app.postgres.user")
         val password = System.getProperty("app.postgres.password")
         val database = System.getProperty("app.postgres.database")
-        
+
         connect("jdbc:pgsql://$host:$port/$database",
                 driver = "com.impossibl.postgres.jdbc.PGDriver",
                 user = user, password = password)
