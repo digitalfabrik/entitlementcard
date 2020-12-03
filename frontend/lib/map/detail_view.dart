1ce821233601a989c04ae39213bfc537a3523f2f
Paula Wikidal <paula.doubleyu@gmail.com>
2020-12-03 13:48:27 +0100

    #31 Display data from backend request

--- frontend/lib/map/detail_view.dart
+++ frontend/lib/map/detail_view.dart
@@ -1,51 +1,93 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+
+import '../graphql/graphql_api.dart';
 
 class DetailView extends StatelessWidget {
+  int _acceptingStoreId;
+
+  DetailView(this._acceptingStoreId);
+
   @override
   Widget build(BuildContext context) {
+    final byIdQuery = AcceptingStoreByIdQuery(
+        variables: AcceptingStoreByIdArguments(
+            ids: ParamsInput(ids: [_acceptingStoreId])));
+    return Query(
+      options: QueryOptions(
+          documentNode: byIdQuery.document,
+          variables: byIdQuery.getVariablesMap()),
+      builder: (QueryResult result,
+          {VoidCallback refetch, FetchMore fetchMore}) {
+        if (result.hasException) {
+          return Text(result.exception.toString(),
+              style: TextStyle(color: Colors.red));
+        }
+
+        if (result.loading) {
+          return Text('Loading …');
+        }
+        final matchingStores =
+            AcceptingStoreByIdQuery().parse(result.data).acceptingStoreById;
+        if (matchingStores.isEmpty) {
+          return Text(
+              'Aktzeptanzstelle nicht gefunden [id: $_acceptingStoreId]');
+        }
+        print(matchingStores.first.toJson());
+        return buildContent(context, matchingStores.first);
+      },
+    );
+  }
+
+  Widget buildContent(BuildContext context,
+      AcceptingStoreById$Query$AcceptingStore acceptingStore) {
     final double sectionSpacing = 16.0;
     return Container(
-      padding: const EdgeInsets.all(16.0),
-      child: new Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Container(
-              padding: EdgeInsets.only(bottom: 5),
-              child: Text(
-                "Akzeptanzstelle XY",
-                style: Theme.of(context).textTheme.headline6,
-              )),
-          Text(
-            "vergünstigter Eintritt € 5,50 vergünstigter Eintritt € 5,50 vergünstigter Eintritt € 5,50",
-          ),
-          SizedBox(height: sectionSpacing),
-          Table(columnWidths: {
-            0: FlexColumnWidth(1),
-            1: FlexColumnWidth(4),
-          }, children: [
-            TableRow(children: [
-              Text("Adr", style: Theme.of(context).textTheme.bodyText1),
-              Text('SENDLINGER-TOR-PLATZ 11 \n80336 MÜNCHEN')
-            ]),
-            TableRow(children: [
-              Text("Tel", style: Theme.of(context).textTheme.bodyText1),
-              Text('089554636')
-            ]),
-            TableRow(children: [
-              Text("E-Mail", style: Theme.of(context).textTheme.bodyText1),
-              Text('sendlingertor@pressmar-kinos.de')
-            ]),
-            TableRow(children: [
-              Text("Website", style: Theme.of(context).textTheme.bodyText1),
-              Text('pressmar-kinos.de')
-            ]),
-          ]),
-          SizedBox(height: sectionSpacing),
-          Text("Kontakt", style: Theme.of(context).textTheme.bodyText1),
-          Text("Hier sind die Kontaktdaten der Kontakperson")
-        ],
-      ),
-    );
+        padding: const EdgeInsets.all(16.0),
+        child: new Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                  padding: EdgeInsets.only(bottom: 5),
+                  child: Text(
+                    acceptingStore.name,
+                    style: Theme.of(context).textTheme.headline6,
+                  )),
+              Text(
+                "vergünstigter Eintritt € 5,50 vergünstigter Eintritt € 5,50 vergünstigter Eintritt € 5,50",
+              ),
+              SizedBox(height: sectionSpacing),
+              Table(
+                columnWidths: {
+                  0: FlexColumnWidth(1),
+                  1: FlexColumnWidth(4),
+                },
+                children: _buildTableRows(context, acceptingStore),
+              ),
+            ]));
+  }
+
+  List<TableRow> _buildTableRows(BuildContext context,
+      AcceptingStoreById$Query$AcceptingStore acceptingStore) {
+    List<TableRow> rows = [];
+    if (acceptingStore.physicalStore != null) {
+      final address = acceptingStore.physicalStore.address;
+      rows.add(_buildTableRow(context, "Adresse",
+          "${address.street} ${address.houseNumber}\n${address.postalCode} ${address.location} ${address.state}"));
+    }
+    rows.add(_buildTableRow(context, "Tel", acceptingStore.contact.telephone));
+    rows.add(_buildTableRow(context, "E-Mail", acceptingStore.contact.email));
+    rows.add(
+        _buildTableRow(context, "Website", acceptingStore.contact.website));
+    return rows;
+  }
+
+  TableRow _buildTableRow(
+      BuildContext context, String category, String content) {
+    return TableRow(children: [
+      Text(category, style: Theme.of(context).textTheme.bodyText1),
+      Text(content)
+    ]);
   }
 }
