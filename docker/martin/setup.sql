b86ad8005e2b112712c8e6dac212acb2bf5681ed
Maximilian Ammann <max@maxammann.org>
2020-12-03 19:49:40 +0100

    Refactor query

--- docker/martin/setup.sql
+++ docker/martin/setup.sql
@@ -29,13 +29,15 @@ $func$;
 CREATE OR REPLACE FUNCTION public.accepting_stores_clustered(z integer, x integer, y integer, query_params json) RETURNS bytea AS
 $$
 DECLARE
+    clip_geom     boolean  = false;
+    buffer        integer  = 64;
+    extend        integer  = 4096;
     mvt           bytea;
-    cluster_k     int      = 3;
+    cluster_k     integer  = 3;
     tile_bbox     geometry = TileBBox(z, x, y, 4326);
     bavarian_bbox geometry = ST_MakeEnvelope(13.8350427083, 47.2703623267, 8.9771580802, 50.5644529365, 4326);
 BEGIN
-
-    SELECT INTO mvt ST_AsMVT(tile, 'accepting_stores_clustered', 4096, 'mvt_geom')
+    SELECT INTO mvt ST_AsMVT(tile, 'accepting_stores_clustered', extend, 'mvt_geom')
     FROM (
              WITH locations AS (
                  SELECT store.location
@@ -54,12 +56,13 @@ BEGIN
                       FROM locations
                   )
              SELECT cluster_index,
-                    count(*)            AS cluster_count,
-                    ST_AsMVTGeom(ST_CENTROID(ST_COLLECT(ST_Transform(clusters.location, 3857))),
-                                 TileBBox(z, x, y, 3857), 4096, 64,
-                                 false) as mvt_geom
+                    count(*) AS cluster_count,
+                    ST_AsMVTGeom(
+                            ST_CENTROID(ST_COLLECT(clusters.location)), tile_bbox, extend, buffer,
+                            clip_geom
+                        )    AS mvt_geom
              FROM clusters
-             GROUP BY cluster_index) as tile
+             GROUP BY cluster_index) AS tile
     WHERE mvt_geom IS NOT NULL;
 
     RETURN mvt;
