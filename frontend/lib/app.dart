778a2262d4466daa4c08d1718d51cdd12d2751e8
Elias Keis <git-commits@elkei.de>
2020-12-10 18:59:59 +0100

    #70 use different main files for configuration

--- frontend/lib/app.dart
+++ frontend/lib/app.dart
@@ -1,16 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 
 import 'home/home_page.dart';
+import 'configuration.dart';
 
 class App extends StatelessWidget {
+  const App({Key key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Digitale Ehrenamtskarte',
-        theme: ThemeData(
-            primarySwatch: Colors.blue,
-            brightness: Brightness.light,
-            visualDensity: VisualDensity.adaptivePlatformDensity),
-        home: HomePage(title: "Digitale Ehrenamtskarte"));
+    final config = Configuration.of(context);
+    final client = ValueNotifier(
+      GraphQLClient(
+        cache: InMemoryCache(),
+        link: Link.from([
+          HttpLink(
+            uri: config.graphqlUrl,
+          )
+        ]),
+      ),
+    );
+    final theme = ThemeData(
+        primarySwatch: Colors.blue,
+        brightness: Brightness.light,
+        visualDensity: VisualDensity.adaptivePlatformDensity);
+    return GraphQLProvider(
+        child: CacheProvider(
+            child: MaterialApp(
+                title: 'Digitale Ehrenamtskarte',
+                theme: theme,
+                home: HomePage(title: "Digitale Ehrenamtskarte"))),
+        client: client);
   }
 }
