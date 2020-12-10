316ac78a58e07fbb00a861362cf4442510bdda4d
Elias Keis <git-commits@elkei.de>
2020-12-10 17:32:32 +0100

    clean up dead code
    we have a repo for readding it in case we need it

--- frontend/lib/app.dart
+++ frontend/lib/app.dart
@@ -1,34 +1,16 @@
-import 'package:ehrenamtskarte/repositories/accepting_businesses_repository.dart';
-import 'package:ehrenamtskarte/models/accepting_businesses_model.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 import 'home/home_page.dart';
 
 class App extends StatelessWidget {
-  final AcceptingBusinessesRepository repository;
-
-  App({
-    @required this.repository,
-  });
-
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (_) => AcceptingBusinessesModel(repository: repository)
-        ..loadAcceptingBusinesses(),
-      child: MaterialApp(
+    return MaterialApp(
         title: 'Digitale Ehrenamtskarte',
         theme: ThemeData(
             primarySwatch: Colors.blue,
             brightness: Brightness.light,
             visualDensity: VisualDensity.adaptivePlatformDensity),
-        home: HomePage(title: "Digitale Ehrenamtskarte"),
-        routes: {
-          'home': (context) => HomePage(title: "Digitale Ehrenamstkarte"),
-          'home1': (context) => HomePage(title: "Digitale Ehrenamstkarte")
-        },
-      ),
-    );
+        home: HomePage(title: "Digitale Ehrenamtskarte"));
   }
 }
--- frontend/lib/home/home_page.dart
+++ frontend/lib/home/home_page.dart
@@ -1,7 +1,5 @@
-import 'package:ehrenamtskarte/list/accepting_businesses_page.dart';
 import 'package:flutter/material.dart';
 
-import '../graphql/graphql_page.dart';
 import '../map/map_page.dart';
 
 class HomePage extends StatefulWidget {
@@ -18,8 +16,8 @@ class _HomePageState extends State<HomePage> {
 
   List<Widget> _tabs = <Widget>[
     MapPage(),
-    GraphQLTestPage(),
-    AcceptingBusinessesPage()
+    Container(),
+    Container(),
   ];
 
   void _onTabTapped(int index) {
@@ -30,29 +28,18 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    var theme = Theme.of(context);
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
       body: _tabs.elementAt(_currentTabIndex),
       bottomNavigationBar: BottomNavigationBar(
         items: [
           BottomNavigationBarItem(icon: Icon(Icons.map), label: "Karte"),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: "Suche"),
           BottomNavigationBarItem(
-              icon: Icon(Icons.chat), label: "GraphQL Test"),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.chat), label: "Provider Test"),
+              icon: Icon(Icons.card_giftcard), label: "Ausweisen"),
         ],
         currentIndex: _currentTabIndex,
         onTap: _onTabTapped,
         type: BottomNavigationBarType.fixed,
-        backgroundColor: theme.primaryColor,
-        selectedItemColor:
-            theme.brightness == Brightness.light ? Colors.white : Colors.black,
-        unselectedItemColor: theme.brightness == Brightness.light
-            ? theme.primaryColorLight
-            : theme.primaryColorDark,
       ),
     );
   }
