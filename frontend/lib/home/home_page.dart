1fd4596c64e0ecd13b032c41b8dc9e3f19bd3527
Elias Keis <git-commits@elkei.de>
2020-12-11 12:41:28 +0100

    improve bottom bar entry handling

--- frontend/lib/home/home_page.dart
+++ frontend/lib/home/home_page.dart
@@ -11,13 +11,20 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
+class _TabData {
+  final Widget widget;
+  final IconData icon;
+  final String label;
+  _TabData(this.widget, this.icon, this.label);
+}
+
 class _HomePageState extends State<HomePage> {
   int _currentTabIndex = 0;
 
-  List<Widget> _tabs = <Widget>[
-    MapPage(),
-    Container(),
-    Container(),
+  List<_TabData> _tabs = <_TabData>[
+    _TabData(MapPage(), Icons.map, "Karte"),
+    _TabData(Container(), Icons.search, "Suche"),
+    _TabData(Container(), Icons.remove_red_eye, "Ausweisen"),
   ];
 
   void _onTabTapped(int index) {
@@ -29,14 +36,12 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _tabs.elementAt(_currentTabIndex),
+      body: _tabs.elementAt(_currentTabIndex).widget,
       bottomNavigationBar: BottomNavigationBar(
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.map), label: "Karte"),
-          BottomNavigationBarItem(icon: Icon(Icons.search), label: "Suche"),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.card_giftcard), label: "Ausweisen"),
-        ],
+        items: _tabs
+            .map((tabData) => BottomNavigationBarItem(
+                icon: Icon(tabData.icon), label: tabData.label))
+            .toList(),
         currentIndex: _currentTabIndex,
         onTap: _onTabTapped,
         type: BottomNavigationBarType.fixed,
