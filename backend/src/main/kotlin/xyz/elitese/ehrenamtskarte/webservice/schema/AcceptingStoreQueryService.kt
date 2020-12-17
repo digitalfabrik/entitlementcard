74ecbdb53234a616b3bf5ac82d566958838bc46a
Michael Markl <marklmichael98@gmail.com>
2020-12-17 18:17:04 +0100

    #76: Fix mean bug with data loaders by using completable futures

--- backend/src/main/kotlin/xyz/elitese/ehrenamtskarte/webservice/schema/AcceptingStoreQueryService.kt
+++ backend/src/main/kotlin/xyz/elitese/ehrenamtskarte/webservice/schema/AcceptingStoreQueryService.kt
@@ -6,21 +6,23 @@ import org.jetbrains.exposed.sql.transactions.transaction
 import xyz.elitese.ehrenamtskarte.database.repos.PhysicalStoresRepository
 import xyz.elitese.ehrenamtskarte.webservice.dataloader.PHYSICAL_STORE_LOADER_NAME
 import xyz.elitese.ehrenamtskarte.webservice.schema.types.PhysicalStore
+import java.util.concurrent.CompletableFuture
 
 class AcceptingStoreQueryService {
 
     @GraphQLDescription("Return list of all accepting stores.")
-    @Suppress("unused")
-    suspend fun physicalStores() = transaction {
+    fun physicalStores() = transaction {
         PhysicalStoresRepository.findAll().map {
             PhysicalStore(it.id.value, it.storeId.value, it.addressId.value)
         }
     }
 
-    @Suppress("unused")
-    suspend fun physicalStoresById(params: Params, dataFetchingEnvironment: DataFetchingEnvironment) =
-            dataFetchingEnvironment.getDataLoader<Int, PhysicalStore>(PHYSICAL_STORE_LOADER_NAME)
-                    .loadMany(params.ids).join()
+    @GraphQLDescription("Returns list of all accepting stores queried by ids.")
+    fun physicalStoresById(
+        params: Params,
+        environment: DataFetchingEnvironment
+    ): CompletableFuture<List<PhysicalStore>> =
+        environment.getDataLoader<Int, PhysicalStore>(PHYSICAL_STORE_LOADER_NAME).loadMany(params.ids)
 }
 
 data class Params(val ids: List<Int>)
