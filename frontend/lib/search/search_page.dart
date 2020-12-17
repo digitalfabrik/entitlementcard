f9bd846b43c88e83d16e422fe85b72d23a4721df
Elias Keis <git-commits@elkei.de>
2020-12-17 12:55:59 +0100

    #77 UI skeleton for search page

--- frontend/lib/search/search_page.dart
+++ frontend/lib/search/search_page.dart
@@ -1,8 +1,40 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class SearchPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Text("WIP");
+    return CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          title: TextField(
+            controller: TextEditingController(),
+            decoration: InputDecoration.collapsed(
+              hintText: "Tippen und schreiben, um zu suchen …",
+            ),
+          ),
+          pinned: true,
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.search),
+              onPressed: () => {},
+            )
+          ],
+        ),
+        SliverGrid.count(
+            crossAxisCount: 5,
+            children: Iterable.generate(10)
+                .map((i) => Container(
+                    color: Theme.of(context).accentColor,
+                    child: IconButton(
+                      icon: Icon(Icons.category),
+                      onPressed: () => {},
+                    )))
+                .toList()),
+        SliverList(
+            delegate: SliverChildBuilderDelegate(
+                (ctx, index) => ListTile(title: Text('Search result #$index')),
+                childCount: 500))
+      ],
+    );
   }
 }
