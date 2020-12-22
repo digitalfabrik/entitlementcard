4b0606edb540d78a4a48abf9c3e5834c1c0269e5
Roman Buchholz <roman.buchholz@t-online.de>
2020-12-22 11:16:43 +0100

    update qr code scanner dependency

--- frontend/lib/identification/qr_code_scanner.dart
+++ frontend/lib/identification/qr_code_scanner.dart
@@ -1,5 +1,7 @@
-import 'package:ehrenamtskarte/identification/parse_jwt.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+import 'dart:io';
+
 import 'package:qr_code_scanner/qr_code_scanner.dart';
 
 const flashOn = 'FLASH ON';
@@ -17,18 +19,30 @@ class QRCodeScanner extends StatefulWidget {
 }
 
 class _QRViewExampleState extends State<QRCodeScanner> {
-  var qrText = '';
+  Barcode result;
   var flashState = flashOn;
   var cameraState = frontCamera;
   QRViewController controller;
   final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+
+  // In order to get hot reload to work we need to pause the camera if the platform
+  // is android, or resume the camera if the platform is iOS.
+  @override
+  void reassemble() {
+    super.reassemble();
+    if (Platform.isAndroid) {
+      controller.pauseCamera();
+    } else if (Platform.isIOS) {
+      controller.resumeCamera();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: <Widget>[
-          Expanded(flex: 6, child: _buildQrView(context)),
+          Expanded(flex: 4, child: _buildQrView(context)),
           Expanded(
             flex: 1,
             child: FittedBox(
@@ -36,7 +50,11 @@ class _QRViewExampleState extends State<QRCodeScanner> {
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: <Widget>[
-                  Text('This is the result of scan: $qrText'),
+                  if (result != null)
+                    Text(
+                        'Barcode Type: ${describeEnum(result.format)}   Data: ${result.code}')
+                  else
+                    Text('Scan a code'),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     crossAxisAlignment: CrossAxisAlignment.center,
@@ -59,7 +77,7 @@ class _QRViewExampleState extends State<QRCodeScanner> {
                             }
                           },
                           child:
-                          Text(flashState, style: TextStyle(fontSize: 20)),
+                              Text(flashState, style: TextStyle(fontSize: 20)),
                         ),
                       ),
                       Container(
@@ -80,7 +98,7 @@ class _QRViewExampleState extends State<QRCodeScanner> {
                             }
                           },
                           child:
-                          Text(cameraState, style: TextStyle(fontSize: 20)),
+                              Text(cameraState, style: TextStyle(fontSize: 20)),
                         ),
                       )
                     ],
@@ -103,11 +121,17 @@ class _QRViewExampleState extends State<QRCodeScanner> {
   }
 
   Widget _buildQrView(BuildContext context) {
+    // For this example we check how width or tall the device is and change the scanArea and overlay accordingly.
+    var scanArea = (MediaQuery.of(context).size.width < 400 ||
+            MediaQuery.of(context).size.height < 400)
+        ? 150.0
+        : 300.0;
     // To ensure the Scanner view is properly sizes after rotation
     // we need to listen for Flutter SizeChanged notification and update controller
     return NotificationListener<SizeChangedLayoutNotification>(
         onNotification: (notification) {
-          Future.microtask(() => controller?.updateDimensions(qrKey));
+          Future.microtask(
+              () => controller?.updateDimensions(qrKey, scanArea: scanArea));
           return false;
         },
         child: SizeChangedLayoutNotifier(
@@ -120,7 +144,7 @@ class _QRViewExampleState extends State<QRCodeScanner> {
                 borderRadius: 10,
                 borderLength: 30,
                 borderWidth: 10,
-                cutOutSize: 300,
+                cutOutSize: scanArea,
               ),
             )));
   }
@@ -129,12 +153,7 @@ class _QRViewExampleState extends State<QRCodeScanner> {
     this.controller = controller;
     controller.scannedDataStream.listen((scanData) {
       setState(() {
-        try {
-          var payload = parseJwtPayLoad(scanData);
-          qrText = payload.toString();
-        } on Exception {
-          qrText = "Could not parse code!";
-        }
+        result = scanData;
       });
     });
   }
