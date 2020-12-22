5f61702f8cc6ea768c2924329aeb7e949efc7cf0
Roman Buchholz <roman.buchholz@t-online.de>
2020-12-22 13:33:14 +0100

    fix linter

--- frontend/lib/identification/card_details.dart
+++ frontend/lib/identification/card_details.dart
@@ -1,9 +1,7 @@
-
 class CardDetails {
   final String firstName;
   final String lastName;
   final DateTime expirationDate;
 
   CardDetails(this.firstName, this.lastName, this.expirationDate);
-
 }
