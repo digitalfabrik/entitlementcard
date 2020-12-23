4a931f65d92e5b411c8efddcf0b8cf21decda95c
Elias Keis <git-commits@elkei.de>
2020-12-23 13:43:28 +0100

    #105 separate navigation for every tab

--- frontend/lib/home/home_page.dart
+++ frontend/lib/home/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../map/map_page.dart';
+import './app_flow.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key key}) : super(key: key);
@@ -9,44 +10,70 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
-class _TabData {
-  final Widget widget;
-  final IconData icon;
-  final String label;
-  _TabData(this.widget, this.icon, this.label);
-}
-
 class _HomePageState extends State<HomePage> {
   int _currentTabIndex = 0;
 
-  List<_TabData> _tabs = <_TabData>[
-    _TabData(MapPage(), Icons.map, "Karte"),
-    _TabData(Container(), Icons.search, "Suche"),
-    _TabData(Container(), Icons.remove_red_eye, "Ausweisen"),
+  final List<AppFlow> appFlows = <AppFlow>[
+    AppFlow(MapPage(), Icons.map, "Karte",
+        GlobalKey<NavigatorState>(debugLabel: "Map tab key")),
+    AppFlow(Container(), Icons.search, "Suche",
+        GlobalKey<NavigatorState>(debugLabel: "Search tab key")),
+    AppFlow(Container(), Icons.remove_red_eye, "Ausweisen",
+        GlobalKey<NavigatorState>(debugLabel: "Auth tab key")),
   ];
-
-  void _onTabTapped(int index) {
-    setState(() {
-      _currentTabIndex = index;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: IndexedStack(
-        index: _currentTabIndex,
-        children: _tabs.map((tab) => tab.widget).toList(),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: _tabs
-            .map((tabData) => BottomNavigationBarItem(
-                icon: Icon(tabData.icon), label: tabData.label))
-            .toList(),
-        currentIndex: _currentTabIndex,
-        onTap: _onTabTapped,
-        type: BottomNavigationBarType.fixed,
+    return WillPopScope(
+      onWillPop: () async =>
+          // do not pop if the inner navigator can handle it
+          !await appFlows[_currentTabIndex]
+              .navigatorKey
+              .currentState
+              .maybePop(),
+      child: Scaffold(
+        body: IndexedStack(
+          index: _currentTabIndex,
+          children: appFlows.map(_buildNavigator).toList(),
+        ),
+        bottomNavigationBar: this._buildBottomNavigationBar(context),
       ),
     );
+  }
+
+  Widget _buildNavigator(AppFlow appFlow) {
+    return Navigator(
+      key: appFlow.navigatorKey,
+      onGenerateRoute: (settings) => MaterialPageRoute(
+          settings: settings, builder: (context) => appFlow.widget),
+    );
+  }
+
+  Widget _buildBottomNavigationBar(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: _currentTabIndex,
+      items: appFlows
+          .map(
+            (appFlow) => BottomNavigationBarItem(
+                icon: Icon(appFlow.iconData), label: appFlow.title),
+          )
+          .toList(),
+      onTap: _onTabTapped,
+      type: BottomNavigationBarType.fixed,
+    );
+  }
+
+  void _onTabTapped(int index) {
+    setState(() {
+      if (_currentTabIndex != index) {
+        _currentTabIndex = index;
+      } else {
+        // if clicking on tab again, reset the tab
+        appFlows[_currentTabIndex]
+            .navigatorKey
+            .currentState
+            .popUntil((route) => route.isFirst);
+      }
+    });
   }
 }
