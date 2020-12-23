80f343873b4b07e248fbe9fc559d189a4be10a10
Michael Markl <marklmichael98@gmail.com>
2020-12-23 11:37:33 +0100

    77: Restrict focus to active tab

--- frontend/lib/home/home_page.dart
+++ frontend/lib/home/home_page.dart
@@ -22,7 +22,7 @@ class _TabData {
 class _HomePageState extends State<HomePage> {
   int _currentTabIndex = 0;
 
-  List<_TabData> _tabs = <_TabData>[
+  List<_TabData> _tabs = [
     _TabData(MapPage(), Icons.map, "Karte"),
     _TabData(SearchPage(), Icons.search, "Suche"),
     _TabData(Container(), Icons.remove_red_eye, "Ausweisen"),
@@ -38,18 +38,22 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: IndexedStack(
-        index: _currentTabIndex,
-        children: _tabs.map((tab) => tab.widget).toList(),
-      ),
+          index: _currentTabIndex,
+          children: _tabs.asMap()
+              .map((index, tab) => MapEntry(
+                  index, Focus(
+                      key: ValueKey(index),
+                      child: tab.widget,
+                      descendantsAreFocusable: _currentTabIndex == index)))
+              .values.toList(growable: false)),
       bottomNavigationBar: BottomNavigationBar(
-        items: _tabs
-            .map((tabData) => BottomNavigationBarItem(
-                icon: Icon(tabData.icon), label: tabData.label))
-            .toList(),
-        currentIndex: _currentTabIndex,
-        onTap: _onTabTapped,
-        type: BottomNavigationBarType.fixed,
-      ),
+          items: _tabs.map((tabData) => BottomNavigationBarItem(
+                    icon: Icon(tabData.icon),
+                    label: tabData.label
+                  )).toList(),
+          currentIndex: _currentTabIndex,
+          onTap: _onTabTapped,
+          type: BottomNavigationBarType.fixed),
     );
   }
 }
