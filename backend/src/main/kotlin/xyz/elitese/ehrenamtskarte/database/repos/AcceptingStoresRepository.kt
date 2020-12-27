cd9a02b74b873d60e6375ca6b531c55f4948a798
Michael Markl <marklmichael98@gmail.com>
2020-12-27 23:48:31 +0100

    #127: Add coordinates to backend search endpoint

--- backend/src/main/kotlin/xyz/elitese/ehrenamtskarte/database/repos/AcceptingStoresRepository.kt
+++ backend/src/main/kotlin/xyz/elitese/ehrenamtskarte/database/repos/AcceptingStoresRepository.kt
@@ -1,10 +1,9 @@
 package xyz.elitese.ehrenamtskarte.database.repos
 
 import org.jetbrains.exposed.sql.*
-import xyz.elitese.ehrenamtskarte.database.AcceptingStoreEntity
-import xyz.elitese.ehrenamtskarte.database.AcceptingStores
-import xyz.elitese.ehrenamtskarte.database.Addresses
-import xyz.elitese.ehrenamtskarte.database.PhysicalStores
+import org.postgis.Point
+import xyz.elitese.ehrenamtskarte.database.*
+import xyz.elitese.ehrenamtskarte.webservice.schema.types.Coordinates
 
 object AcceptingStoresRepository {
 
@@ -16,7 +15,9 @@ object AcceptingStoresRepository {
     // TODO would be great to support combinations like "Tür an Tür Augsburg"
     // TODO Fulltext search is possible with tsvector in postgres: https://www.compose.com/articles/mastering-postgresql-tools-full-text-search-and-phrase-search/
     // TODO Probably not relevant right now
-    fun findBySearch(searchText: String?, categoryIds: List<Int>?): SizedIterable<AcceptingStoreEntity> {
+    fun findBySearch(
+        searchText: String?, categoryIds: List<Int>?, coordinates: Coordinates?
+    ): SizedIterable<AcceptingStoreEntity> {
         val categoryMatcher =
             if (categoryIds == null)
                 Op.TRUE
@@ -37,18 +38,35 @@ object AcceptingStoresRepository {
                     )
                 )
 
+        val sortExpression = if (coordinates != null)
+            DistanceFunction(
+                PhysicalStores.coordinates,
+                SetSRIDFucntion(MakePointFunction(doubleParam(coordinates.lng), doubleParam(coordinates.lat)))
+            )
+        else
+            AcceptingStores.name
+
         return (AcceptingStores leftJoin PhysicalStores leftJoin Addresses)
             .slice(AcceptingStores.columns)
             .select(categoryMatcher and textMatcher)
+            .orderBy(sortExpression)
             .let { AcceptingStoreEntity.wrapRows(it) }
     }
 }
 
+
 // Postgres' "like" operation uses case sensitive comparison by default.
 // Postgres has a builtin "ilike" operation which does case sensitive comparison.
-
 class InsensitiveLikeOp(expr1: Expression<*>, expr2: Expression<*>) : ComparisonOp(expr1, expr2, "ILIKE")
 
 infix fun <T : String?> Expression<T>.ilike(pattern: String): InsensitiveLikeOp =
     InsensitiveLikeOp(this, stringParam(pattern))
 
+class DistanceFunction(expr1: Expression<Point>, expr2: Expression<Point>) :
+    CustomFunction<Double>("ST_Distance", DoubleColumnType(), expr1, expr2)
+
+class MakePointFunction(expr1: Expression<Double>, expr2: Expression<Double>) :
+    CustomFunction<Point>("ST_MakePoint", DoubleColumnType(), expr1, expr2)
+
+class SetSRIDFucntion(expr1: Expression<Point>) :
+    CustomFunction<Point>("ST_SetSRID", GeometryColumnType(), expr1, intParam(4326))
---
+++ backend/src/main/kotlin/xyz/elitese/ehrenamtskarte/webservice/schema/types/Coordinates.kt
@@ -0,0 +1,6 @@
+package xyz.elitese.ehrenamtskarte.webservice.schema.types
+
+data class Coordinates(
+    val lng: Double,
+    val lat: Double
+)
