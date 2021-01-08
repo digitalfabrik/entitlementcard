7ccbdf422ac631a86f7618124b64ae57b7a97c3e
Elias Keis <git-commits@elkei.de>
2021-01-08 10:37:33 +0100

    #168 do not show http(s) prefix but add it when missing at launch

---
+++ frontend/lib/util/sanitize_contact_details.dart
@@ -0,0 +1,19 @@
+final _httpRegex = RegExp(r"^https?://");
+
+String sanitizePhoneNumber(String telephone) => telephone
+    .trim() // remove outer whitespace
+    .replaceAll(RegExp(r"\s+"), "-") // replace inner whitespace
+    .replaceAll(RegExp(r'[^\d+-]'), ""); // remove illegal characters
+
+String prepareWebsiteUrlForDisplay(String website) => website
+    .trim()
+    .replaceAll(_httpRegex, "");
+
+String prepareWebsiteUrlForLaunch(String website) {
+  var trimmed = website.trim();
+  if (_httpRegex.hasMatch(trimmed)) {
+    return trimmed;
+  } else {
+    return "https://$trimmed";
+  }
+}
