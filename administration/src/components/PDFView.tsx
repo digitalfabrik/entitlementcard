18218cfc8837f69f36a84607af1941eb3ab5c1e1
Maximilian Ammann <max@maxammann.org>
2021-01-10 15:18:25 +0100

    Switch from pdfkit to jsPDF

--- administration/src/components/PDFView.tsx
+++ administration/src/components/PDFView.tsx
@@ -1,17 +1,16 @@
 import React, {useEffect, useState} from "react";
-import PDFDocument from 'pdfkit'
-import blobStream from 'blob-stream'
 import {
     QRCodeEncoder,
     QRCodeDecoderErrorCorrectionLevel as ErrorCorrectionLevel,
     IllegalStateException,
     QRCodeEncoderQRCode as QRCode, EncodeHintType
 } from '@zxing/library';
+import { jsPDF } from "jspdf";
 
 const QUIET_ZONE_SIZE = 10
 
 // Adapted from https://github.com/zxing-js/library/blob/d1a270cb8ef3c4dba72966845991f5c876338aac/src/browser/BrowserQRCodeSvgWriter.ts#L91
-const renderPdf = (doc: typeof PDFDocument, data: string, hints?: Map<EncodeHintType, any>, width = 500, height = 500) => {
+const renderPdf = (doc: jsPDF, data: string, hints?: Map<EncodeHintType, any>, width = 200, height = 200) => {
     let errorCorrectionLevel = ErrorCorrectionLevel.L;
     let quietZone = QUIET_ZONE_SIZE;
 
@@ -49,48 +48,40 @@ const renderPdf = (doc: typeof PDFDocument, data: string, hints?: Map<EncodeHint
     const leftPadding = Math.floor((outputWidth - (inputWidth * multiple)) / 2);
     const topPadding = Math.floor((outputHeight - (inputHeight * multiple)) / 2);
 
-    let pathData = ""
     for (let inputY = 0, outputY = topPadding; inputY < inputHeight; inputY++ , outputY += multiple) {
         // Write the contents of this row of the barcode
         for (let inputX = 0, outputX = leftPadding; inputX < inputWidth; inputX++ , outputX += multiple) {
             if (input.get(inputX, inputY) === 1) {
-                // doc.rect(outputX, outputY, multiple, multiple)
-                // doc.fill('black')
-                var w = multiple + outputX
-                var h = multiple + outputY
-                pathData += ('M' + outputX + ',' + outputY + ' V' + h + ' H' + w + ' V' + outputY + ' H' + outputX + ' Z ');
+                doc.rect(outputX, outputY, multiple, multiple, "DF")
+                //var w = multiple + outputX
+                //var h = multiple + outputY
+                //pathData += ('M' + outputX + ',' + outputY + ' V' + h + ' H' + w + ' V' + outputY + ' H' + outputX + ' Z ');
             }
         }
     }
-    doc.path(pathData)
-    doc.fill('black')
-    console.log(pathData)
 }
 
 export default () => {
     const [pdfBlob, setPDFBlob] = useState<string | null>(null)
 
     useEffect(() => {
-        const fs = require('fs')
-        // eslint-disable-next-line
-        fs.writeFileSync('data/Helvetica.afm', require('!!raw-loader!pdfkit/js/data/Helvetica.afm').default)
-        const doc = new PDFDocument();
-        const stream = doc.pipe(blobStream());
+        const doc = new jsPDF();
 
         const hints = new Map()
         hints.set(EncodeHintType.MARGIN, 10)
         hints.set(EncodeHintType.QR_VERSION, 7) // 224 characters
         renderPdf(doc, "A".repeat(223), hints)
 
-        // doc.fontSize(25).text('Here is some vector graphixxx...', 100, 80);
-
-        doc.end();
-        stream.on('finish', function () {
-            setPDFBlob(stream.toBlobURL('application/pdf'))
-        });
+        doc.text('Here is some vector graphixxx...', 100, 80);
+        
+        setPDFBlob(doc.output('datauristring'))
     }, [])
 
+    if (!pdfBlob) {
+        return null
+    }
+    
     return (
-        <iframe width="775" height="775" src={pdfBlob || ""}/>
-    )
+        <iframe width="775" height="775" src={pdfBlob}/>
+    );
 }
