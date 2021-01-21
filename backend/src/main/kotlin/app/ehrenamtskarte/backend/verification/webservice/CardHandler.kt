b3d5e16ea95ba01c2b2aa3df32b9a308f53ddd33
Elias Keis <git-commits@elkei.de>
2021-01-21 18:35:25 +0100

    #189 make cards insertable

--- backend/src/main/kotlin/app/ehrenamtskarte/backend/verification/webservice/CardHandler.kt
+++ backend/src/main/kotlin/app/ehrenamtskarte/backend/verification/webservice/CardHandler.kt
@@ -1,21 +1,27 @@
 package app.ehrenamtskarte.backend.verification.webservice
 
 import io.javalin.http.Context
+import org.jetbrains.exposed.sql.transactions.transaction
 import app.ehrenamtskarte.backend.verification.database.repos.CardRepository
 import app.ehrenamtskarte.backend.verification.domain.Card
 import java.time.LocalDateTime
 import java.time.ZoneOffset
 
-data class CardTo @ExperimentalUnsignedTypes constructor(val totpSecret: ByteArray, val expirationDate: ULong, val hashModel: String)
+data class CardTo @ExperimentalUnsignedTypes constructor(var totpSecret: ByteArray = ByteArray(0), var expirationDate: ULong = 0UL, var hashModel: String = "")
 
 class CardHandler {
     @ExperimentalUnsignedTypes
     fun addCard(ctx: Context) {
         val cardTo = ctx.body<CardTo>()
-        CardRepository.insert(Card(
-            cardTo.totpSecret,
-            LocalDateTime.ofEpochSecond(cardTo.expirationDate.toLong(), 0, ZoneOffset.UTC),
-            cardTo.hashModel))
+        transaction {
+            CardRepository.insert(
+                Card(
+                    cardTo.totpSecret,
+                    LocalDateTime.ofEpochSecond(cardTo.expirationDate.toLong(), 0, ZoneOffset.UTC),
+                    cardTo.hashModel
+                )
+            )
+        }
     }
 
     fun verifyCard(ctx: Context) {
