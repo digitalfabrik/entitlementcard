a07a258208b48f64ac7770e9c80a65102a0afe31
Elias Keis <git-commits@elkei.de>
2021-01-21 16:53:23 +0100

    #189 add setup skeleton for verification

--- backend/src/main/kotlin/xyz/elitese/ehrenamtskarte/common/webservice/WebService.kt
+++ backend/src/main/kotlin/xyz/elitese/ehrenamtskarte/common/webservice/WebService.kt
@@ -2,7 +2,8 @@ package xyz.elitese.ehrenamtskarte.common.webservice
 
 import io.javalin.Javalin
 import io.javalin.http.staticfiles.Location
-import xyz.elitese.ehrenamtskarte.stores.webservice.setupApp
+import xyz.elitese.ehrenamtskarte.stores.webservice.setupApp as setupAppForStores
+import xyz.elitese.ehrenamtskarte.verification.webservice.setupApp as setupAppForVerification
 
 const val PORT = 7000
 
@@ -17,6 +18,7 @@ class WebService {
         println("Server is running at http://localhost:${PORT}")
         println("Goto http://localhost:${PORT}/graphiql for a graphical editor")
 
-        setupApp(app)
+        setupAppForStores(app)
+        setupAppForVerification(app)
     }
 }
---
+++ backend/src/main/kotlin/xyz/elitese/ehrenamtskarte/verification/database/Setup.kt
@@ -0,0 +1,5 @@
+package xyz.elitese.ehrenamtskarte.verification.database
+
+fun setupDatabase() {
+
+}
