dc6b72a14942fab2753a317df21f9db9ce97b1ba
Elias Keis <git-commits@elkei.de>
2021-01-21 16:41:46 +0100

    #189 extract database setup

--- backend/src/main/kotlin/xyz/elitese/ehrenamtskarte/stores/webservice/Setup.kt
+++ backend/src/main/kotlin/xyz/elitese/ehrenamtskarte/stores/webservice/Setup.kt
@@ -2,7 +2,7 @@ package xyz.elitese.ehrenamtskarte.stores.webservice
 
 import io.javalin.Javalin
 
-fun hookInto(app: Javalin) {
+fun setupApp(app: Javalin) {
     val graphQLHandler = GraphQLHandler()
     app.post("/") { ctx -> graphQLHandler.handle(ctx) }
 }
