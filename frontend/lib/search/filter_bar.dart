3861b0b683a3dd3ea96cd92486763881d08f3f7f
Michael Markl <marklmichael98@gmail.com>
2021-01-21 10:27:27 +0100

    #182: Remove padding

--- frontend/lib/search/filter_bar.dart
+++ frontend/lib/search/filter_bar.dart
@@ -12,29 +12,30 @@ class FilterBar extends StatelessWidget {
     var sortedCategories = [...categoryAssets];
     sortedCategories.sort((a, b) => a.shortName.compareTo(b.shortName));
     return SliverToBoxAdapter(
-      child: Column(children: [
-        Padding(
-            padding: EdgeInsets.all(8),
-            child: Row(children: [
-              Text("Nach Kategorien filtern".toUpperCase(),
-                  maxLines: 1, style: TextStyle(color: Colors.grey)),
-              Expanded(
-                  child: Padding(
-                      padding: EdgeInsets.only(left: 8),
-                      child: Divider(thickness: 0.7)))
-            ])),
-        Wrap(
-            alignment: WrapAlignment.spaceBetween,
-            runSpacing: 8,
-            spacing: 4,
-            children: sortedCategories
-                .map((e) => Padding(
-                    key: ValueKey(e.id),
-                    padding: EdgeInsets.all(0),
-                    child: FilterBarButton(
-                        asset: e, onCategoryPress: onCategoryPress)))
-                .toList())
-      ]),
-    );
+        child: Column(children: [
+      Padding(
+          padding: EdgeInsets.all(8),
+          child: Row(children: [
+            Text("Nach Kategorien filtern".toUpperCase(),
+                maxLines: 1, style: TextStyle(color: Colors.grey)),
+            Expanded(
+                child: Padding(
+                    padding: EdgeInsets.only(left: 8),
+                    child: Divider(thickness: 0.7)))
+          ])),
+      Row(children: [
+        Expanded(
+            child: Wrap(
+                alignment: WrapAlignment.spaceEvenly,
+                runSpacing: 8,
+                spacing: 4,
+                children: sortedCategories
+                    .map((e) => FilterBarButton(
+                        key: ValueKey(e.id),
+                        asset: e,
+                        onCategoryPress: onCategoryPress))
+                    .toList()))
+      ])
+    ]));
   }
 }
