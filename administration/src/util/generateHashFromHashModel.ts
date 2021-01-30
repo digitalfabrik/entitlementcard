229ccb70b9335501c415d205a6347a31b588a532
Michael Markl <marklmichael98@gmail.com>
2021-01-30 21:44:41 +0100

    #212: Hash HashModel using crypto.subtle with SHA-512

--- administration/src/util/generateHashFromHashModel.ts
+++ administration/src/util/generateHashFromHashModel.ts
@@ -1,15 +1,33 @@
 import {CardActivateModel} from "../generated/compiled";
+import isIE11 from "./isIE11";
 
-export type HashModel = {
+export interface HashModel {
     fullName: string,
-    region: number
-    cardType: CardActivateModel.CardType
+    region: number,
+    randomBytes: Uint8Array,
+    cardType: CardActivateModel.CardType,
 }
 
-const generateHashFromHashModel = (hashModel: HashModel) => {
+const generateHashFromHashModel = async (hashModel: HashModel) => {
     // todo: replace this routine with e.g. a protobuf hashModel for reliable hashes
+    // todo: (also don't use TextEncoder for IE11)
     const encoder = new TextEncoder()
-    return encoder.encode(JSON.stringify(hashModel))
+    const binary = encoder.encode(JSON.stringify(hashModel))
+
+    // In IE11, this returns CryptoOperation,
+    // see https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/digest#browser_compatibility
+    let hashArrayBuffer: ArrayBuffer
+    if (isIE11()) { // This still needs to be tested
+        hashArrayBuffer = await new Promise((resolve) => {
+            // @ts-ignore
+            window.msCrypto.subtle.digest("SHA-128", binary).oncomplete = data => resolve(data)
+        })
+        // @ts-ignore
+    } else {
+        hashArrayBuffer = await crypto.subtle.digest("SHA-512", binary)
+    }
+
+    return new Uint8Array(hashArrayBuffer)
 }
 
 export default generateHashFromHashModel
