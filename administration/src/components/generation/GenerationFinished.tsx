d6a1df9b2bdc15f836c1f0db282f16b198508db4
Michael Markl <marklmichael98@gmail.com>
2021-01-30 21:45:53 +0100

    #212: Make GenerationFinished beautiful

--- administration/src/components/generation/GenerationFinished.tsx
+++ administration/src/components/generation/GenerationFinished.tsx
@@ -4,8 +4,13 @@ import styled from "styled-components";
 
 const Container = styled.div`
   display: flex;
+  justify-content: center;
+  align-items: center;
   flex-direction: column;
   height: 100%;
+  &  > * {
+    margin: 20px;
+  }
 `;
 
 interface Props {
@@ -18,7 +23,7 @@ const GenerationController = (props: Props) => {
             <Container>
                 <Icon icon="tick-circle" color="green" iconSize={100}/>
                 <p>Die Karten wurden erstellt.</p>
-                <Button value={"Mehr Karten erstellen"} onClick={props.reset}/>
+                <Button onClick={props.reset}>Mehr Karten erstellen</Button>
             </Container>
         );
     }
