f1076df2edfddb9b4b4f87d83b03a24a12986be5
Elias Keis <git-commits@elkei.de>
2021-02-04 17:33:56 +0100

    #227 make deb script configurable using params

--- scripts/backend_deb.sh
+++ scripts/backend_deb.sh
@@ -1,4 +1,5 @@
-#!/bin/bash -e
+#!/bin/bash
+set -eo pipefail
 
 # deb constants
 version=1.0
@@ -8,15 +9,34 @@ architecture=amd64
 maintainer="The Ehrenamtskarte Team <info@ehrenamtskarte.app>"
 description="Backend server for the Ehrenamtskarte app"
 
+# read input
+while getopts v:r:a:n:t:h flag
+do
+    case "${flag}" in
+        v) version=${OPTARG};;
+        r) revision=${OPTARG};;
+        a) architecture=${OPTARG};;
+        n) name=${OPTARG};;
+        t) tarfile=${OPTARG};;
+        h) 
+            echo "$0 [-v version] [-r revision] [-a architecture] [-n name] -t backend_tar"
+            exit 0;;
+    esac
+done
+
+if [[ -z "$tarfile" ]]; then
+    echo "Must provide input tar file using -t file" 1>&2
+    exit 1
+fi
+
+debworkdir=$(mktemp -d)
 fullname=${name}_${version}-${revision}_${architecture}
-debworkdir=build/$fullname
-debfile=build/${fullname}.deb
-ctrlfile=${debworkdir}/DEBIAN/control
+debfile=${fullname}.deb
 
 # init deb workdir
-mkdir -p $debworkdir
-rm -rf ${debworkdir}/*
 mkdir ${debworkdir}/DEBIAN
+ctrlfile=${debworkdir}/DEBIAN/control
+echo "Creating control file in $ctrlfile …"
 touch $ctrlfile
 echo "Package: $name" >> $ctrlfile
 echo "Version: $version" >> $ctrlfile
@@ -25,8 +45,13 @@ echo "Maintainer: $maintainer" >> $ctrlfile
 echo "Description: $description" >> $ctrlfile
 
 # copy files to deb workdir
+echo "Copying $tarfile …"
 mkdir -p ${debworkdir}/var/ehrenamtskarte/backend
-tar -xf build/distributions/backend.tar -C ${debworkdir}/var/ehrenamtskarte
+tar -xf $tarfile -C ${debworkdir}/var/ehrenamtskarte
 
 # build the deb
 dpkg-deb --build --root-owner-group $debworkdir $debfile
+
+# clean up
+echo "Cleaning up …"
+rm -rf $debworkdir
