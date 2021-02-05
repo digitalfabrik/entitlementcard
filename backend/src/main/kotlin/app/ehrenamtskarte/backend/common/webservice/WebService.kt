543b97c173aa87e8a31e05773f12748810e0153e
Maximilian Ammann <max@maxammann.org>
2021-02-05 16:18:04 +0100

    Make host configurable

--- backend/src/main/kotlin/app/ehrenamtskarte/backend/common/webservice/WebService.kt
+++ backend/src/main/kotlin/app/ehrenamtskarte/backend/common/webservice/WebService.kt
@@ -3,18 +3,21 @@ package app.ehrenamtskarte.backend.common.webservice
 import io.javalin.Javalin
 import io.javalin.http.staticfiles.Location
 
-const val PORT = 7000
+const val DEFAULT_PORT = "7000"
 
 class WebService {
     fun start() {
+        val host = System.getProperty("app.host", "0.0.0.0")
+        val port = Integer.parseInt(System.getProperty("app.port", DEFAULT_PORT))
         val app = Javalin.create { cfg ->
+
             cfg.enableDevLogging()
             cfg.enableCorsForAllOrigins()
             cfg.addStaticFiles("/graphiql", "/graphiql", Location.CLASSPATH)
-        }.start(PORT)
+        }.start(host, port)
 
-        println("Server is running at http://localhost:${PORT}")
-        println("Goto http://localhost:${PORT}/graphiql for a graphical editor")
+        println("Server is running at http://${host}:${port}")
+        println("Goto http://${host}:${port}/graphiql for a graphical editor")
 
         val graphQLHandler = GraphQLHandler()
         app.post("/") { ctx -> graphQLHandler.handle(ctx) }
