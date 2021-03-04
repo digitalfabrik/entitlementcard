73b4b25420a17bad18b58f57280ef276f04c80e0
Elias Keis <git-commits@elkei.de>
2021-03-04 14:26:45 +0100

    #263 move exception handling UI stuff out of business logic

--- frontend/lib/verification/view/verification_view.dart
+++ frontend/lib/verification/view/verification_view.dart
@@ -1,10 +1,15 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 
+import '../../qr_code_scanner/qr_code_parser.dart';
 import '../../qr_code_scanner/qr_code_scanner_page.dart';
+import '../verification_card_details.dart';
 import '../verification_card_details_model.dart';
+import '../verification_error.dart';
+import '../verification_hasher.dart';
 import '../verification_processor.dart';
 import 'content_card.dart';
 import 'verification_info_text.dart';
@@ -20,6 +25,9 @@ class VerificationView extends StatefulWidget {
 }
 
 class _VerificationViewState extends State<VerificationView> {
+  VerificationCardDetails verificationCardDetails;
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,6 +37,7 @@ class _VerificationViewState extends State<VerificationView> {
               Icons.arrow_back_ios,
               color: Theme.of(context).textTheme.bodyText1.color,
             ),
+            // TODO the scaffold usually should add this by default already
             onPressed: () => Navigator.of(context).pop(),
           ),
           title: Text("Karte verifizieren"),
@@ -63,17 +72,43 @@ class _VerificationViewState extends State<VerificationView> {
   }
 
   void _onScanCodePressed() {
-    final provider =
-        Provider.of<VerificationCardDetailsModel>(context, listen: false);
-
     Navigator.push(
         context,
         MaterialPageRoute(
           builder: (context) => QRCodeScannerPage(
             title: "Karte verifizieren",
-            qrCodeContentParser:
-                VerificationProcessor(provider).processQrCodeContent,
+            qrCodeContentParser: _handleQrCode,
           ),
         ));
+  }
+
+  void _handleQrCode(String rawQRContent) {
+    // TODO still no beautiful function
+    final provider =
+    Provider.of<VerificationCardDetailsModel>(context, listen: false);
+    void handleError(String message, String errorCode, Exception e) {
+      print("Verification failed: ${e.toString()}");
+      provider.setVerificationFailure(VerificationError.fromStrings(message,
+          errorCode));
+    }
+    try {
+      var cardDetails = processQrCodeContent(rawQRContent);
+      final hash = hashVerificationCardDetails(cardDetails);
+      provider.setReadyForRemoteVerification(cardDetails, hash);
+    } on QRCodeFieldMissingException catch (e) {
+      handleError("Die eingescannte Ehrenamtskarte ist nicht gültig, da "
+          "erforderliche Daten fehlen.", "${e.missingFieldName}Missing", e);
+    } on CardExpiredException catch (e) {
+      final dateFormat = DateFormat("dd.MM.yyyy");
+      handleError("Die eingescannte Karte ist bereits am "
+          "${dateFormat.format(e.expiry)} abgelaufen.", "cardExpired", e);
+    } on QRCodeParseException catch (e) {
+      handleError("Der Inhalt des eingescannten Codes kann nicht verstanden "
+      "werden. Vermutlich handelt es sich um einen QR Code, der nicht für die "
+      "Ehrenamtskarte-App generiert wurde.", "invalidFormat", e);
+    } on Exception catch (e) {
+      handleError("Ein unbekannter Fehler beim Einlesen des QR-Codes ist "
+          "aufgetreten.", "unknownError", e);
+    }
   }
 }
