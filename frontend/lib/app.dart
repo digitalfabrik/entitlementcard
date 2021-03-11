b130b362297a046aec5e348e6ae789a7bbd20a6c
Roman Buchholz <roman.buchholz@t-online.de>
2021-03-11 10:39:06 +0100

    #262 move themes to separate file

--- frontend/lib/app.dart
+++ frontend/lib/app.dart
@@ -5,32 +5,13 @@ import 'configuration.dart';
 import 'graphql/configured_graphql_provider.dart';
 import 'home/home_page.dart';
 import 'identification/card_details_model.dart';
+import 'themes.dart';
 
 class App extends StatelessWidget {
   const App({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final theme = ThemeData.from(
-        colorScheme:
-            ColorScheme.light(primary: Colors.blue, secondary: Colors.blue),
-        textTheme: TextTheme(
-          headline6: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
-          bodyText1: TextStyle(fontSize: 15.0, fontWeight: FontWeight.normal),
-          bodyText2: TextStyle(fontSize: 15.0, color: Color(0xFF505050)),
-        ));
-    var darkTheme = ThemeData.from(
-        colorScheme: ColorScheme.dark(
-          primary: Color(0xff8377A9),
-          secondary: Color(0xff8377A9),
-        ),
-        textTheme: TextTheme(
-          headline6: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
-          bodyText1: TextStyle(fontSize: 15.0, fontWeight: FontWeight.normal),
-          bodyText2: TextStyle(fontSize: 15.0, color: Color(0xFFC6C4C4)),
-        ));
-    darkTheme = darkTheme.copyWith(
-        appBarTheme: AppBarTheme(color: darkTheme.colorScheme.primary));
     return ConfiguredGraphQlProvider(
       child: MultiProvider(
           providers: [
@@ -39,7 +20,7 @@ class App extends StatelessWidget {
           ],
           child: MaterialApp(
             title: 'Ehrenamtskarte',
-            theme: theme,
+            theme: lightTheme,
             darkTheme: darkTheme,
             home: HomePage(
               showVerification: Configuration.of(context).showVerification,
---
+++ frontend/lib/themes.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+final lightTheme = ThemeData.from(
+    colorScheme:
+        ColorScheme.light(primary: Colors.blue, secondary: Colors.blue),
+    textTheme: TextTheme(
+      headline6: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+      bodyText1: TextStyle(fontSize: 15.0, fontWeight: FontWeight.normal),
+      bodyText2: TextStyle(fontSize: 15.0, color: Color(0xFF505050)),
+    ));
+
+final darkTheme = ThemeData.from(
+    colorScheme: ColorScheme.dark(
+      primary: Color(0xff8377A9),
+      secondary: Color(0xff8377A9),
+    ),
+    textTheme: TextTheme(
+      headline6: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+      bodyText1: TextStyle(fontSize: 15.0, fontWeight: FontWeight.normal),
+      bodyText2: TextStyle(fontSize: 15.0, color: Color(0xFFC6C4C4)),
+    )).copyWith(appBarTheme: AppBarTheme(color: darkTheme.colorScheme.primary));
