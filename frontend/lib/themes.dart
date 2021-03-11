621ba552b4bd99000dcc771e7d8cd6b2ae4a1248
Roman Buchholz <roman.buchholz@t-online.de>
2021-03-11 10:44:33 +0100

    fix theme depending on itself

--- frontend/lib/themes.dart
+++ frontend/lib/themes.dart
@@ -9,13 +9,17 @@ final lightTheme = ThemeData.from(
       bodyText2: TextStyle(fontSize: 15.0, color: Color(0xFF505050)),
     ));
 
-final darkTheme = ThemeData.from(
-    colorScheme: ColorScheme.dark(
-      primary: Color(0xff8377A9),
-      secondary: Color(0xff8377A9),
-    ),
-    textTheme: TextTheme(
-      headline6: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
-      bodyText1: TextStyle(fontSize: 15.0, fontWeight: FontWeight.normal),
-      bodyText2: TextStyle(fontSize: 15.0, color: Color(0xFFC6C4C4)),
-    )).copyWith(appBarTheme: AppBarTheme(color: darkTheme.colorScheme.primary));
+ThemeData get darkTheme {
+  var theme = ThemeData.from(
+      colorScheme: ColorScheme.dark(
+        primary: Color(0xff8377A9),
+        secondary: Color(0xff8377A9),
+      ),
+      textTheme: TextTheme(
+        headline6: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+        bodyText1: TextStyle(fontSize: 15.0, fontWeight: FontWeight.normal),
+        bodyText2: TextStyle(fontSize: 15.0, color: Color(0xFFC6C4C4)),
+      ));
+  return theme.copyWith(
+      appBarTheme: AppBarTheme(color: theme.colorScheme.primary));
+}
