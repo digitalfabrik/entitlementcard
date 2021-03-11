1750a54c57fb11c526d47ae4b57a37538013540f
Michael Markl <marklmichael98@gmail.com>
2021-03-11 16:25:31 +0100

    #270: Add Upload Scalar to Backend

--- backend/src/main/kotlin/app/ehrenamtskarte/backend/application/webservice/applicationGraphQLParams.kt
+++ backend/src/main/kotlin/app/ehrenamtskarte/backend/application/webservice/applicationGraphQLParams.kt
@@ -1,9 +1,14 @@
 package app.ehrenamtskarte.backend.application.webservice
 
+import app.ehrenamtskarte.backend.application.webservice.schema.UploadKey
 import app.ehrenamtskarte.backend.common.webservice.GraphQLParams
 import com.expediagroup.graphql.SchemaGeneratorConfig
 import com.expediagroup.graphql.TopLevelObject
+import com.expediagroup.graphql.hooks.SchemaGeneratorHooks
+import graphql.kickstart.servlet.apollo.ApolloScalars
 import org.dataloader.DataLoaderRegistry
+import kotlin.reflect.KType
+import graphql.schema.*
 
 
 private fun createDataLoaderRegistry(): DataLoaderRegistry {
@@ -12,7 +17,13 @@ private fun createDataLoaderRegistry(): DataLoaderRegistry {
 }
 
 val applicationGraphQlParams = GraphQLParams(
-    config = SchemaGeneratorConfig(supportedPackages = listOf("app.ehrenamtskarte.backend.application.webservice.schema")),
+    config = SchemaGeneratorConfig(
+        supportedPackages = listOf("app.ehrenamtskarte.backend.application.webservice.schema"),
+        hooks = object : SchemaGeneratorHooks {
+            override fun willGenerateGraphQLType(type: KType): GraphQLType? =
+                if (type.classifier == UploadKey::class) ApolloScalars.Upload else null
+        }
+    ),
     dataLoaderRegistry = createDataLoaderRegistry(),
     queries = listOf(),
     mutations = listOf(
--- backend/src/main/kotlin/app/ehrenamtskarte/backend/common/webservice/GraphQLHandler.kt
+++ backend/src/main/kotlin/app/ehrenamtskarte/backend/common/webservice/GraphQLHandler.kt
@@ -5,6 +5,10 @@ import app.ehrenamtskarte.backend.stores.webservice.storesGraphQlParams
 import app.ehrenamtskarte.backend.verification.webservice.verificationGraphQlParams
 import app.ehrenamtskarte.backend.regions.webservice.regionsGraphQlParams
 import com.expediagroup.graphql.toSchema
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.node.ArrayNode
+import com.fasterxml.jackson.databind.node.ObjectNode
+import com.fasterxml.jackson.databind.node.TextNode
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import graphql.ExceptionWhileDataFetching
@@ -12,7 +16,10 @@ import graphql.ExecutionInput
 import graphql.ExecutionResult
 import graphql.GraphQL
 import io.javalin.http.Context
+import io.javalin.http.util.MultipartUtil
 import java.io.IOException
+import java.lang.IllegalArgumentException
+import java.lang.IllegalStateException
 
 class GraphQLHandler(
     private val graphQLParams: GraphQLParams = storesGraphQlParams stitch verificationGraphQlParams
@@ -31,11 +38,57 @@ class GraphQLHandler(
     /**
      * Get payload from the request.
      */
-    private fun getPayload(context: Context): Map<String, Any>? {
-        return try {
+    private fun getPayload(context: Context): Map<String, Any>? = try {
+        if (!context.isMultipart()) {
             mapper.readValue<Map<String, Any>>(context.body())
-        } catch (e: IOException) {
-            throw IOException("Unable to parse GraphQL payload.")
+        } else {
+            val servlet = context.req
+            MultipartUtil.preUploadFunction(servlet)
+            val parts = servlet.parts.map { it.name to it }.toMap()
+            val operationsJson = parts["operations"]?.inputStream ?: throw IOException("Missing operations part.")
+            val operations = mapper.readTree(operationsJson)
+            val mapJson = context.formParam("map")
+            if (mapJson != null) {
+                val substitutions = mapper.readValue<Map<String, List<String>>>(mapJson)
+                substitutions.forEach { (key, paths) ->
+                    if (!parts.containsKey(key) || key == "operations" || key == "map")
+                        throw IllegalArgumentException("Part with key '$key' not found or invalid!")
+                    paths.forEach { path -> operations.substitute(path, key) }
+                }
+            }
+            val result = mapper.readValue<MutableMap<String, Any>>(mapper.treeAsTokens(operations))
+            result["localContext"] = parts
+            result
+        }
+    } catch (e: IOException) {
+        throw IOException("Unable to parse GraphQL payload.")
+    }
+
+    private fun JsonNode.substitute(path: String, value: String): Unit {
+        val paths = path.split(".");
+        var node = this
+
+        for (fieldOrIndex in paths.subList(0, paths.size - 1)) {
+            node = when (node) {
+                is ArrayNode -> node.path(
+                    fieldOrIndex.toIntOrNull() ?: throw IllegalArgumentException(
+                        "Expecting array index, but could not convert to integer."
+                    )
+                )
+                is ObjectNode -> node.path(fieldOrIndex)
+                else -> throw IllegalStateException("Expected ArrayNode or ObjectNode.")
+            }
+            if (node.isMissingNode || node.isNull) throw IllegalArgumentException("Accessing unavailable field")
+        }
+
+        val lastPath = paths.last()
+        val replaceWith = ObjectNode(mapper.nodeFactory, mapOf("key" to TextNode(value)))
+        when (node) {
+            is ArrayNode -> node[lastPath.toIntOrNull()
+                ?: throw IllegalArgumentException("Expecting array index, but could not convert to integer.")] =
+                replaceWith
+            is ObjectNode -> node.set<ObjectNode>(lastPath, replaceWith)
+            else -> throw  IllegalStateException("Expected ArrayNode or ObjectNode.")
         }
     }
 
@@ -79,12 +132,15 @@ class GraphQLHandler(
         val payload = getPayload(context)
         payload?.let {
             // Execute the query against the schema
-            val executionResult = graphQL.executeAsync(
+            val executionInput =
                 ExecutionInput.Builder()
                     .query(payload["query"].toString())
                     .variables(getVariables(payload))
                     .dataLoaderRegistry(graphQLParams.dataLoaderRegistry)
-            ).get()
+                    .localContext(payload["localContext"])
+                    .build()
+
+            val executionResult = graphQL.executeAsync(executionInput).get()
             val result = getResult(executionResult)
 
             // write response as json
