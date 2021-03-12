be5b2348dda77742940f73fb31e004445d2f5f2e
Roman Buchholz <roman.buchholz@t-online.de>
2021-03-12 15:53:54 +0100

    make primary color darker and use for both light and dark mode

--- frontend/lib/themes.dart
+++ frontend/lib/themes.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 
 final lightTheme = ThemeData.from(
-    colorScheme:
-        ColorScheme.light(primary: Colors.blue, secondary: Colors.blue),
+    colorScheme: ColorScheme.light(
+      primary: Color(0xff5f5384),
+      secondary: Color(0xff5f5384),
+    ),
     textTheme: TextTheme(
       headline6: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
       bodyText1: TextStyle(fontSize: 15.0, fontWeight: FontWeight.normal),
@@ -12,8 +14,8 @@ final lightTheme = ThemeData.from(
 ThemeData get darkTheme {
   var theme = ThemeData.from(
       colorScheme: ColorScheme.dark(
-        primary: Color(0xff8377A9),
-        secondary: Color(0xff8377A9),
+        primary: Color(0xff5f5384),
+        secondary: Color(0xff5f5384),
       ),
       textTheme: TextTheme(
         headline6: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
