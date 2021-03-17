6a5af01885cb6f12cd377bab84a1a907d40de24b
Michael Markl <marklmichael98@gmail.com>
2021-03-17 21:56:27 +0100

    #306: Make IdentificationPage stateless

--- frontend/lib/identification/identification_view.dart
+++ frontend/lib/identification/identification_view.dart
@@ -3,48 +3,37 @@ import 'package:provider/provider.dart';
 
 import '../verification/verification_workflow.dart';
 import 'card_detail_view/card_detail_view.dart';
-import 'card_details.dart';
 import 'card_details_model.dart';
 import 'identification_qr_scanner_page.dart';
 import 'no_card_view.dart';
 
-const showTestDataOptions =
-bool.fromEnvironment("test_data_options", defaultValue: false);
-
-class IdentificationPage extends StatefulWidget {
-  IdentificationPage({Key key, this.title}) : super(key: key);
-
+class IdentificationPage extends StatelessWidget {
   final String title;
 
-  @override
-  _IdentificationPageState createState() => _IdentificationPageState();
-}
-
-class _IdentificationPageState extends State<IdentificationPage> {
-  CardDetails _cardDetails;
+  IdentificationPage({Key key, this.title}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Consumer<CardDetailsModel>(
         builder: (context, cardDetailsModel, child) {
-          if (!cardDetailsModel.isInitialized) {
-            return Container();
-          }
+      if (!cardDetailsModel.isInitialized) {
+        return Container();
+      }
 
-          _cardDetails = cardDetailsModel.cardDetails;
-          if (_cardDetails != null) {
-            return CardDetailView(
-                cardDetails: _cardDetails,
-                startActivateEak: () => _showActivateQrCode(context),
-                startVerification: () => _showVerificationDialog(context));
-          }
+      var cardDetails = cardDetailsModel.cardDetails;
+      if (cardDetails != null) {
+        return CardDetailView(
+            cardDetails: cardDetails,
+            startActivateEak: () => _showActivateQrCode(context),
+            startVerification: () => _showVerificationDialog(context));
+      }
 
-          return Scaffold(
-            body: NoCardView(
-                startVerification: () => _showVerificationDialog(context),
-                startActivateQrCode: () => _showActivateQrCode(context)),
-          );
-        });
+      return Scaffold(
+        body: NoCardView(
+            startVerification: () => _showVerificationDialog(context),
+            startActivateQrCode: () => _showActivateQrCode(context)),
+      );
+    });
   }
 
   void _showVerificationDialog(context) async {
