ade3d6a88b0d32bdf107b573f424ba57b94c8cfc
Michael Markl <marklmichael98@gmail.com>
2021-03-18 21:33:41 +0100

    #284: Make the input form a form

--- administration/src/components/auth/LoginForm.tsx
+++ administration/src/components/auth/LoginForm.tsx
@@ -15,26 +15,26 @@ interface Props {
 const LoginForm = (props: Props) => {
     return (
         <div style={{marginTop: "20px"}}>
-            <FormGroup label="E-Mail">
-                <InputGroup placeholder="erika.musterfrau@example.org"
-                            autoFocus
-                            value={props.email}
-                            disabled={!!props.loading}
-                            onChange={(event: ChangeEvent<HTMLInputElement>) => props.setEmail(event.target.value)}/>
-            </FormGroup>
-            <FormGroup label="Passwort">
-                <InputGroup placeholder="Passwort"
-                            value={props.password}
-                            disabled={!!props.loading}
-                            type="password"
-                            onChange={(event: ChangeEvent<HTMLInputElement>) => props.setPassword(event.target.value)}/>
-            </FormGroup>
-            <Button
-                text="Anmelden"
-                type="submit"
-                loading={!!props.loading}
-                onClick={() => props.onSubmit()}>
-            </Button>
+            <form onSubmit={props.onSubmit}>
+                <FormGroup label="E-Mail">
+                    <InputGroup placeholder="erika.musterfrau@example.org"
+                                autoFocus
+                                value={props.email}
+                                disabled={!!props.loading}
+                                onChange={(event: ChangeEvent<HTMLInputElement>) => props.setEmail(event.target.value)}/>
+                </FormGroup>
+                <FormGroup label="Passwort">
+                    <InputGroup placeholder="Passwort"
+                                value={props.password}
+                                disabled={!!props.loading}
+                                type="password"
+                                onChange={(event: ChangeEvent<HTMLInputElement>) => props.setPassword(event.target.value)}/>
+                </FormGroup>
+                <Button
+                    text="Anmelden"
+                    type="submit"
+                    loading={!!props.loading} />
+            </form>
         </div>
     )
 };
