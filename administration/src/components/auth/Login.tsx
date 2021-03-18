fd10ed2dc37cadeff6d5972eee87c230e94247b8
Michael Markl <marklmichael98@gmail.com>
2021-03-18 18:53:26 +0100

    #284: Add title to Login

--- administration/src/components/auth/Login.tsx
+++ administration/src/components/auth/Login.tsx
@@ -1,22 +1,23 @@
-import {useMutation, ApolloError} from "@apollo/client";
+import {ApolloError, useMutation} from "@apollo/client";
 import React from "react";
 import styled from "styled-components";
-import {signIn_signInPayload as SignInPayload, signIn as SignInCarrier} from "../../graphql/auth/__generated__/signIn";
+import {signIn as SignInCarrier, signIn_signInPayload as SignInPayload} from "../../graphql/auth/__generated__/signIn";
 import LoginForm from "./LoginForm";
 import {SIGN_IN} from "../../graphql/auth/mutations";
 import {LoginToaster} from "../AppToaster";
+import {Card, H2} from "@blueprintjs/core";
 
 interface Props {
     onSignIn: (payload: SignInPayload) => void
 }
 
 const Center = styled("div")`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 100%;
-    height: 100%;
-    padding: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  padding: 40px;
 `
 
 const onError = function (error: ApolloError) {
@@ -28,10 +29,13 @@ const Login = (props: Props) => {
         onCompleted: (payload: SignInCarrier) => props.onSignIn(payload.signInPayload),
         onError: onError
     })
-    const onSubmit = (email: string, password: string) => signIn({ variables: {authData: {email, password}}})
+    const onSubmit = (email: string, password: string) => signIn({variables: {authData: {email, password}}})
 
     return <Center>
-        <LoginForm onSubmit={onSubmit} loading={mutationState.loading}/>
+        <Card>
+            <H2>Ehrenamtskarte Verwaltung</H2>
+            <LoginForm onSubmit={onSubmit} loading={mutationState.loading}/>
+        </Card>
     </Center>
 }
 
--- administration/src/components/auth/LoginForm.tsx
+++ administration/src/components/auth/LoginForm.tsx
@@ -1,5 +1,5 @@
 import React, {ChangeEvent} from "react";
-import {Button, Card, FormGroup, InputGroup} from "@blueprintjs/core";
+import {Button, FormGroup, InputGroup} from "@blueprintjs/core";
 import "@blueprintjs/datetime/lib/css/blueprint-datetime.css";
 
 
@@ -14,37 +14,35 @@ interface Props {
 }
 
 const LoginForm = (props: Props) => {
-    const [state, setState] = React.useState<State>({ email: "", password: ""})
+    const [state, setState] = React.useState<State>({email: "", password: ""})
     return (
-        <div>
-            <Card>
-                <FormGroup label="E-Mail">
-                    <InputGroup placeholder="erika.musterfrau@example.org"
-                                autoFocus
-                                value={state.email}
-                                disabled={!!props.loading}
-                                onChange={(event: ChangeEvent<HTMLInputElement>) => setState({
-                                    ...state,
-                                    email: event.target.value
-                                })}/>
-                </FormGroup>
-                <FormGroup label="Passwort">
-                    <InputGroup placeholder="Passwort"
-                                value={state.password}
-                                disabled={!!props.loading}
-                                type="password"
-                                onChange={(event: ChangeEvent<HTMLInputElement>) => setState({
-                                    ...state,
-                                    password: event.target.value
-                                })}/>
-                </FormGroup>
-                <Button
-                    text="Anmelden"
-                    type="submit"
-                    loading={!!props.loading}
-                    onClick={() => props.onSubmit(state.email, state.password)}>
-                </Button>
-            </Card>
+        <div style={{marginTop: "20px"}}>
+            <FormGroup label="E-Mail">
+                <InputGroup placeholder="erika.musterfrau@example.org"
+                            autoFocus
+                            value={state.email}
+                            disabled={!!props.loading}
+                            onChange={(event: ChangeEvent<HTMLInputElement>) => setState({
+                                ...state,
+                                email: event.target.value
+                            })}/>
+            </FormGroup>
+            <FormGroup label="Passwort">
+                <InputGroup placeholder="Passwort"
+                            value={state.password}
+                            disabled={!!props.loading}
+                            type="password"
+                            onChange={(event: ChangeEvent<HTMLInputElement>) => setState({
+                                ...state,
+                                password: event.target.value
+                            })}/>
+            </FormGroup>
+            <Button
+                text="Anmelden"
+                type="submit"
+                loading={!!props.loading}
+                onClick={() => props.onSubmit(state.email, state.password)}>
+            </Button>
         </div>
     )
 };
