fc9188fe6639a1819132010e78bd3f2b72e7b247
Michael Markl <marklmichael98@gmail.com>
2021-03-18 18:36:51 +0100

    #284: Add sample user if no user is available

--- backend/src/main/kotlin/app/ehrenamtskarte/backend/auth/database/Setup.kt
+++ backend/src/main/kotlin/app/ehrenamtskarte/backend/auth/database/Setup.kt
@@ -1,9 +1,13 @@
 package app.ehrenamtskarte.backend.auth.database
 
+import app.ehrenamtskarte.backend.auth.database.repos.AdministratorsRepository
 import org.jetbrains.exposed.sql.SchemaUtils
 
 fun setupDatabase() {
     SchemaUtils.create(
         Administrators
     )
+    if (AdministratorEntity.count() == 0L) {
+        AdministratorsRepository.insert("admin@example.com", "admin")
+    }
 }
