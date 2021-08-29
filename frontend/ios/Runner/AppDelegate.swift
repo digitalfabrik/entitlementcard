5f9e6a41bb960affa989cfdc66c61b0cde4ee8a7
Max Ammann <max@maxammann.org>
2021-08-29 14:12:28 +0200

    Add code to reset data of previously installed app by it nrw

--- frontend/ios/Runner/AppDelegate.swift
+++ frontend/ios/Runner/AppDelegate.swift
@@ -7,7 +7,53 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    clearITNRWStorage()
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+    
+    func clearITNRWStorage() {
+        let preferences = UserDefaults.standard
+        let key = "preferencesSQLFilterString"
+
+        print(preferences.string(forKey: key))
+        
+        if preferences.string(forKey: key) != nil {
+            //clear preferences
+            resetDefaults()
+
+            //clear storage
+            removeCache()
+        }
+    }
+
+    func resetDefaults() {
+        let defs = UserDefaults.standard
+        let dict = defs.dictionaryRepresentation()
+        for (key, _) in dict {
+            defs.removeObject(forKey: key)
+        }
+        defs.synchronize()
+    }
+    
+    func removeCache() {
+        let manager = FileManager.default
+        let paths = FileManager.default.urls(for: .libraryDirectory, in: .userDomainMask).map(\.path)
+        let libraryDirectory = paths[0]
+        var directoryContent: [String]? = nil
+        do {
+            directoryContent = try manager.contentsOfDirectory(atPath: libraryDirectory)
+            
+            if let directoryContent = directoryContent {
+                for content in directoryContent {
+                    let path = URL(fileURLWithPath: libraryDirectory).appendingPathComponent(content).path
+                    do {
+                        try manager.removeItem(atPath: path)
+                    } catch {
+                    }
+                }
+            }
+        } catch {
+        }
+    }
 }
