6022b7a82f50cee34c12f0be2a6936fc4cd02ccd
Maximilian Ammann <max@maxammann.org>
2021-11-15 13:39:35 +0100

    Log all errors

--- frontend/lib/graphql/configured_graphql_provider.dart
+++ frontend/lib/graphql/configured_graphql_provider.dart
@@ -6,7 +6,8 @@ import '../configuration/configuration.dart';
 class ConfiguredGraphQlProvider extends StatelessWidget {
   final Widget child;
 
-  const ConfiguredGraphQlProvider({Key? key, required this.child}) : super(key: key);
+  const ConfiguredGraphQlProvider({Key? key, required this.child})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -14,6 +15,13 @@ class ConfiguredGraphQlProvider extends StatelessWidget {
       GraphQLClient(
         cache: GraphQLCache(),
         link: Link.from([
+          ErrorLink(onException:
+              (Request request, NextLink forward, LinkException exception) {
+            debugPrint(exception.toString());
+          }, onGraphQLError:
+              (Request request, NextLink forward, Response response) {
+            debugPrint(response.errors.toString());
+          }),
           HttpLink(Configuration.of(context).graphqlUrl)
         ]),
       ),
