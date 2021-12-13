04702572feb7e92c823c1bb0d22b7dabcbe1e09b
Maximilian Ammann <max@maxammann.org>
2021-12-13 12:12:23 +0100

    Upgrade to Geolocator 8.0.0 and set geolocator_android to custom repository without usage of Strings

--- frontend/android/app/src/main/java/com/google/android/gms/common/util/Strings.java
+++
@@ -1,7 +0,0 @@
-package com.google.android.gms.common.util;
-
-public class Strings {
-    public static boolean isEmptyOrWhitespace(String string) {
-        return string.trim().isEmpty();
-    }
-}
