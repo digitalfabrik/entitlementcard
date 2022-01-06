330c2cff7df4ed567fcfc78da82824e921d3904a
Steffen Kleinle <steffen.kleinle@mailbox.org>
2022-01-06 14:10:46 +0100

    Rename and fix types in AcceptingStore data class

---
+++ backend/src/main/kotlin/app/ehrenamtskarte/backend/stores/importer/types/AcceptingStore.kt
@@ -0,0 +1,20 @@
+package app.ehrenamtskarte.backend.stores.importer.types
+
+data class AcceptingStore(
+    val name: String,
+    val countryCode: String,
+    val location: String,
+    val postalCode: String?,
+    val street: String?,
+    val houseNumber: String?,
+    val longitude: Double,
+    val latitude: Double,
+    val categoryId: Int,
+    val email: String?,
+    val telephone: String?,
+    val website: String?,
+    val discount: String?,
+    val streetWithHouseNumber: String? =
+        if (street != null || houseNumber != null) listOfNotNull(street, houseNumber).joinToString(" ")
+        else null
+)
