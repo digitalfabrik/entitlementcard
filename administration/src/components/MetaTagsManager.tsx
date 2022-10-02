c89527cfe5aa95b4124ae28f08134fe11b7ecf16
Michael Markl <marklmichael98@gmail.com>
2022-10-02 19:23:18 +0200

    Whitelabel logo and login title for administration

---
+++ administration/src/components/MetaTagsManager.tsx
@@ -0,0 +1,21 @@
+import {ReactElement, useContext, useEffect} from "react";
+import {ProjectConfigContext} from "../project-configs/ProjectConfigContext";
+
+export const MetaTagsManager = (props: { children: ReactElement }) => {
+    const config = useContext(ProjectConfigContext)
+    useEffect(
+        () => {
+            document.title = config.name + " Verwaltung"
+            const links = Array.from(document.head.getElementsByTagName("link"))
+            let iconLink = links.find(link => link.rel === "icon")
+            if (iconLink == null) {
+                iconLink = document.createElement("link")
+                iconLink.rel = "icon"
+                document.head.append(iconLink)
+            }
+            iconLink.href = "/icons/" + config.projectId + ".png"
+        },
+        [config]
+    )
+    return props.children
+}
