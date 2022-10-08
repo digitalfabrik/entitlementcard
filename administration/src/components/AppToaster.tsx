03ed90894bad66c49c05283f7c4c7c37bb3c3b96
Michael Markl <marklmichael98@gmail.com>
2022-10-08 14:47:13 +0200

    Remove useCallback

--- administration/src/components/AppToaster.tsx
+++ administration/src/components/AppToaster.tsx
@@ -1,5 +1,5 @@
 import {Position, Toaster} from "@blueprintjs/core";
-import {createContext, ReactElement, useCallback, useContext, useState} from "react";
+import {createContext, ReactElement, useContext, useState} from "react";
 
 const ToasterContext = createContext<Toaster | null>(null)
 
@@ -7,9 +7,8 @@ export const useAppToaster = (): Toaster | null => useContext(ToasterContext)
 
 export const AppToasterProvider = (props: { children: ReactElement }) => {
     const [toaster, setToaster] = useState<Toaster | null>(null)
-    const toasterRef = useCallback(setToaster, [setToaster])
     return <ToasterContext.Provider value={toaster}>
-        <Toaster ref={toasterRef} position={Position.TOP}/>
+        <Toaster ref={setToaster} position={Position.TOP}/>
         {props.children}
     </ToasterContext.Provider>
 }
