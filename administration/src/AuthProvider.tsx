7cedd53bc98ee1795e8c3ac8e662ca7e41ba4db8
Michael Markl <marklmichael98@gmail.com>
2022-10-11 23:10:31 +0200

    492: Keep user logged in after closing tab

--- administration/src/AuthProvider.tsx
+++ administration/src/AuthProvider.tsx
@@ -1,18 +1,17 @@
 import { createContext, ReactNode, useMemo, useState } from 'react'
 import { Administrator, SignInPayload } from './generated/graphql'
 
-export interface AuthContextData {
+export interface AuthData {
   token: string
   expiry: Date
   administrator: Administrator
-  password: string
 }
 
 const noop = () => {}
 
 export const AuthContext = createContext<{
-  data: AuthContextData | null
-  signIn: (payload: SignInPayload, password: string) => void
+  data: AuthData | null
+  signIn: (payload: SignInPayload) => void
   signOut: () => void
 }>({ data: null, signIn: noop, signOut: noop })
 
@@ -21,26 +20,62 @@ const getExpiryFromToken = (token: string) => {
   return new Date(payload.exp * 1000) // exp is in seconds, not milliseconds
 }
 
-const convertToAuthContextData: (payload: SignInPayload, password: string) => AuthContextData = (
-  payload,
-  password
-) => ({
+const convertToAuthData: (payload: SignInPayload) => AuthData = payload => ({
   token: payload.token,
   administrator: payload.user,
   expiry: getExpiryFromToken(payload.token),
-  password,
 })
 
+const LOCAL_STORAGE_KEY = 'authdata'
+
+const loadAuthDataFromSessionStorage = (): AuthData | null => {
+  const authDataRaw = window.localStorage.getItem(LOCAL_STORAGE_KEY)
+  if (authDataRaw === null) {
+    return null
+  }
+
+  try {
+    const partialAuthData: { token: string; administrator: Administrator } = JSON.parse(authDataRaw)
+    const expiry = getExpiryFromToken(partialAuthData.token)
+    if (expiry < new Date()) {
+      window.localStorage.removeItem(LOCAL_STORAGE_KEY)
+      return null
+    }
+    return { ...partialAuthData, expiry: getExpiryFromToken(partialAuthData.token) }
+  } catch (e) {
+    window.localStorage.removeItem(LOCAL_STORAGE_KEY)
+  }
+  return null
+}
+
+const saveAuthDataToSessionStorage = (authData: AuthData) => {
+  window.localStorage.setItem(
+    LOCAL_STORAGE_KEY,
+    JSON.stringify({
+      token: authData.token,
+      administrator: authData.administrator,
+    })
+  )
+}
+
+const removeAuthDataFromSessionStorage = () => window.localStorage.removeItem(LOCAL_STORAGE_KEY)
+
 const AuthProvider = ({ children }: { children: ReactNode }) => {
-  const [authContextData, setAuthContextData] = useState<AuthContextData | null>(null)
+  const [authData, setAuthData] = useState<AuthData | null>(loadAuthDataFromSessionStorage())
   const contextValue = useMemo(
     () => ({
-      data: authContextData,
-      signIn: (payload: SignInPayload, password: string) =>
-        setAuthContextData(convertToAuthContextData(payload, password)),
-      signOut: () => setAuthContextData(null),
+      data: authData,
+      signIn: (payload: SignInPayload) => {
+        const authData = convertToAuthData(payload)
+        saveAuthDataToSessionStorage(authData)
+        setAuthData(authData)
+      },
+      signOut: () => {
+        removeAuthDataFromSessionStorage()
+        setAuthData(null)
+      },
     }),
-    [authContextData]
+    [authData]
   )
 
   return <AuthContext.Provider value={contextValue}>{children}</AuthContext.Provider>
