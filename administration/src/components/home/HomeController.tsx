fc7cb1259dad757e631043ab44927bfe8d224420
Michael Markl <marklmichael98@gmail.com>
2022-11-13 14:57:14 +0100

    Adjust margins

--- administration/src/components/home/HomeController.tsx
+++ administration/src/components/home/HomeController.tsx
@@ -13,16 +13,16 @@ const HomeController = () => {
       {role === Role.RegionAdmin || role === Role.RegionManager ? (
         <>
           <NavLink to={'/applications'}>
-            <Button style={{ marginBottom: '10px' }} icon='form' text='Eingehende Anträge' />
+            <Button style={{ margin: '10px' }} icon='form' text='Eingehende Anträge' />
           </NavLink>
           <NavLink to={'/eak-generation'}>
-            <Button icon='id-number' text='Karten erstellen' />
+            <Button style={{ margin: '10px' }} icon='id-number' text='Karten erstellen' />
           </NavLink>
         </>
       ) : null}
       {role === Role.ProjectAdmin || role === Role.RegionAdmin ? (
         <NavLink to={'/users'}>
-          <Button style={{ marginBottom: '10px' }} icon='people' text='Benutzer verwalten' />
+          <Button style={{ margin: '10px' }} icon='people' text='Benutzer verwalten' />
         </NavLink>
       ) : null}
     </div>
