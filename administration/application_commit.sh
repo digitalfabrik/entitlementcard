5166ebe34984f16c732c2aadefbcc93d2dd2a186
Michael Markl <marklmichael98@gmail.com>
2022-11-21 22:50:23 +0100

    Add comment

--- administration/application_commit.sh
+++ administration/application_commit.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+# This script determines the last commit of the folder administration/src/application.
+# This is used as a version for the application form. The form should reset whenever there are changes to the source
+# code of the application form.
+
 basedir=$(dirname "$0")
-commit=$(git log -n 1 --format="%h" -- "${basedir}/src/applicastion")
+commit=$(git log -n 1 --format="%h" -- "${basedir}/src/application")
 if [ -z "$commit" ]
 then
   >&2 echo "Could not determine last application commit!"
