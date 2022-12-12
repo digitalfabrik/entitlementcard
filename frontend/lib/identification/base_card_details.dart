e8de24ab33a6cb1a98a88170ce451ca39831f065
Max Ammann <max@maxammann.org>
2022-12-12 15:27:59 +0100

    Use date api

--- frontend/lib/identification/base_card_details.dart
+++ frontend/lib/identification/base_card_details.dart
@@ -1,21 +1,17 @@
 enum CardType { standard, gold }
 
-const secondsPerDay = 60 * 60 * 24;
-const millisecondsPerSecond = 1000;
-
 class BaseCardDetails {
   final String fullName;
   final String hashSecretBase64;
   final CardType cardType;
   final int regionId;
+
   // Days since 1970-01-01. For more information refer to the card.proto
   final int? expirationDay;
   final DateTime? expirationDate;
 
   BaseCardDetails(this.fullName, this.hashSecretBase64, this.expirationDay, this.cardType, this.regionId)
       : expirationDate = expirationDay != null && expirationDay > 0
-            ? DateTime.fromMillisecondsSinceEpoch(expirationDay *
-                secondsPerDay *
-                millisecondsPerSecond) // FIXME: this is not correct as a day is not always 24h :D
+            ? DateTime.fromMillisecondsSinceEpoch(0).add(Duration(days: expirationDay))
             : null;
 }
