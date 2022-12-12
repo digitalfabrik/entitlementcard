ba64edc45e8a1ecf513b365478e4689305886a41
Max Ammann <max@maxammann.org>
2022-12-12 16:50:57 +0100

    Format

--- administration/src/util/day.ts
+++ administration/src/util/day.ts
@@ -1,9 +1,9 @@
 import { addDays, differenceInDays, fromUnixTime, getUnixTime } from 'date-fns'
 
 export const dateToDaysSinceEpoch = (date: Date): number => {
-  const diff = differenceInDays(date, getUnixTime(0));
+  const diff = differenceInDays(date, getUnixTime(0))
   if (diff < 0) {
-    throw new Error("Can not calculate days since epoch for dates before 1970/01/01")
+    throw new Error('Can not calculate days since epoch for dates before 1970/01/01')
   }
   return diff
 }
