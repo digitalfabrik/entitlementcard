feb05acc2550ab7fe3131d93c82c9ba45bdc2958
Max Ammann <max@maxammann.org>
2022-12-19 20:35:38 +0100

    Fix workflow names

--- frontend/build-configs/bayern/index.ts
+++ frontend/build-configs/bayern/index.ts
@@ -34,8 +34,8 @@ let bayern: BuildConfigType = {
         applicationId: "de.nrw.it.giz.ehrensache.bayern.android",
         featureFlags: {
             ...bayernCommon.featureFlags,
-            excludeLocationPlayServices: false,
-            excludeX86: false
+            excludeLocationPlayServices: true,
+            excludeX86: true
         }
     },
     ios: {
