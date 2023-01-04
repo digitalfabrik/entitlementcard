59a90aee0d2dd98a4ffd0e99adff3f0070ab65c5
Michael Markl <marklmichael98@gmail.com>
2023-01-04 13:48:46 +0100

    Use compound utils for StepCardTypeForm

--- administration/src/application/components/forms/StepCardTypeForm.tsx
+++ administration/src/application/components/forms/StepCardTypeForm.tsx
@@ -1,75 +1,67 @@
 import { ApplicationType, CardType } from '../../../generated/graphql'
-import { SetState, useUpdateStateCallback } from '../../useUpdateStateCallback'
+import { useUpdateStateCallback } from '../../useUpdateStateCallback'
 import { Form } from '../../FormType'
-import { FormControlLabel, FormLabel, Radio, RadioGroup, Typography } from '@mui/material'
-import { FormControl } from '@mui/material'
-import CheckboxForm, { CheckboxFormState } from '../primitive-inputs/CheckboxForm'
+import { Typography } from '@mui/material'
+import CheckboxForm from '../primitive-inputs/CheckboxForm'
 import CustomDivider from '../CustomDivider'
+import { createRadioGroupForm } from '../primitive-inputs/RadioGroupForm'
+import {
+  CompoundState,
+  createCompoundGetArrayBufferKeys,
+  createCompoundGetValidatedInput,
+  createCompoundInitialState,
+} from '../../compoundFormUtils'
 
-const CardTypeInput = ({ state, setState }: { state: CardType | null; setState: SetState<CardType | null> }) => (
-  <FormControl>
-    <FormLabel>Antrag auf:</FormLabel>
-    <RadioGroup value={state} onChange={e => setState(() => e.target.value as CardType)}>
-      <FormControlLabel value={CardType.Blue} label='Blaue Ehrenamtskarte' control={<Radio required />} />
-      <FormControlLabel value={CardType.Golden} label='Goldene Ehrenamtskarte' control={<Radio required />} />
-    </RadioGroup>
-  </FormControl>
-)
+const CardTypeForm = createRadioGroupForm<CardType>()
+const cardTypeOptions = {
+  labelByValue: {
+    [CardType.Blue]: 'Blaue Ehrenamtskarte',
+    [CardType.Golden]: 'Goldene Ehrenamtskarte',
+  },
+}
 
-const ApplicationTypeInput = ({
-  state,
-  setState,
-}: {
-  state: ApplicationType | null
-  setState: SetState<ApplicationType | null>
-}) => {
-  return (
-    <FormControl>
-      <FormLabel>Art des Antrags:</FormLabel>
-      <RadioGroup value={state} onChange={e => setState(() => e.target.value as ApplicationType)}>
-        <FormControlLabel value={ApplicationType.FirstApplication} label='Erstantrag' control={<Radio required />} />
-        <FormControlLabel
-          value={ApplicationType.RenewalApplication}
-          label='Verlängerungsantrag'
-          control={<Radio required />}
-        />
-      </RadioGroup>
-    </FormControl>
-  )
+const ApplicationTypeForm = createRadioGroupForm<ApplicationType>()
+const applicationTypeOptions = {
+  labelByValue: {
+    [ApplicationType.FirstApplication]: 'Erstantrag',
+    [ApplicationType.RenewalApplication]: 'Verlängerungsantrag',
+  },
 }
 
 const wantsDigitalCardOptions = { required: false } as const
 
-export type StepCardTypeFormState = {
-  cardType: CardType | null
-  applicationType: ApplicationType | null
-  wantsDigitalCard: CheckboxFormState
+const SubForms = {
+  cardType: CardTypeForm,
+  applicationType: ApplicationTypeForm,
+  wantsDigitalCard: CheckboxForm,
 }
+
+export type StepCardTypeFormState = CompoundState<typeof SubForms>
 type ValidatedInput = { cardType: CardType; applicationType: ApplicationType; wantsDigitalCard: boolean }
 type Options = {}
 type AdditionalProps = {}
 const StepCardTypeForm: Form<StepCardTypeFormState, Options, ValidatedInput, AdditionalProps> = {
-  initialState: { cardType: null, applicationType: null, wantsDigitalCard: CheckboxForm.initialState },
-  getArrayBufferKeys: state => [...CheckboxForm.getArrayBufferKeys(state.wantsDigitalCard)],
-  getValidatedInput: state => {
-    const wantsDigitalCard = CheckboxForm.getValidatedInput(state.wantsDigitalCard, wantsDigitalCardOptions)
-    if (state.cardType === null || state.applicationType === null || wantsDigitalCard.type === 'error') {
-      return { type: 'error' }
-    }
-    return {
-      type: 'valid',
-      value: {
-        cardType: state.cardType,
-        applicationType: state.applicationType,
-        wantsDigitalCard: wantsDigitalCard.value,
-      },
-    }
-  },
+  initialState: createCompoundInitialState(SubForms),
+  getArrayBufferKeys: createCompoundGetArrayBufferKeys(SubForms),
+  getValidatedInput: createCompoundGetValidatedInput(SubForms, {
+    cardType: cardTypeOptions,
+    applicationType: applicationTypeOptions,
+    wantsDigitalCard: wantsDigitalCardOptions,
+  }),
   Component: ({ state, setState }) => (
     <div style={{ display: 'flex', flexDirection: 'column' }}>
-      <CardTypeInput state={state.cardType} setState={useUpdateStateCallback(setState, 'cardType')} />
+      <CardTypeForm.Component
+        divideItems={false}
+        title='Antrag auf:'
+        options={cardTypeOptions}
+        state={state.cardType}
+        setState={useUpdateStateCallback(setState, 'cardType')}
+      />
       <CustomDivider />
-      <ApplicationTypeInput
+      <ApplicationTypeForm.Component
+        divideItems={false}
+        title='Art des Antrags:'
+        options={applicationTypeOptions}
         state={state.applicationType}
         setState={useUpdateStateCallback(setState, 'applicationType')}
       />
