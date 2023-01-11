900ca2d9d7956f0b40ac92a033f40ef27abd9291
Michael Markl <marklmichael98@gmail.com>
2023-01-11 21:21:58 +0100

    Escape strings in dart build config generator

--- frontend/pubs/df_build_config/lib/builder.dart
+++ frontend/pubs/df_build_config/lib/builder.dart
@@ -35,7 +35,8 @@ void pairToField(String k, dynamic v, StringBuffer root, StringBuffer output) {
     output.write('  $name get $k => const $name();\n');
     generateDataModel(name, json, root);
   } else if (v is String) {
-    output.write('  String get $k => "$v";\n');
+    final escaped = v.replaceAll("\"","\\\"").replaceAll("\n", "\\n");
+    output.write('  String get $k => "$escaped";\n');
   } else if (v is bool) {
     output.write('  bool get $k => $v;\n');
   } else if (v is int) {
