342e301f2d19617366e79f414fde2627cbfc8ffa
Michael Markl <marklmichael98@gmail.com>
2023-01-11 11:32:40 +0100

    Make SMTP passwords configurable via cmd line args

--- backend/src/main/kotlin/app/ehrenamtskarte/backend/EntryPoint.kt
+++ backend/src/main/kotlin/app/ehrenamtskarte/backend/EntryPoint.kt
@@ -1,7 +1,10 @@
 package app.ehrenamtskarte.backend
 
 import app.ehrenamtskarte.backend.common.database.Database
+import app.ehrenamtskarte.backend.common.webservice.EAK_BAYERN_PROJECT
 import app.ehrenamtskarte.backend.common.webservice.GraphQLHandler
+import app.ehrenamtskarte.backend.common.webservice.NUERNBERG_PASS_PROJECT
+import app.ehrenamtskarte.backend.common.webservice.SHOWCASE_PROJECT
 import app.ehrenamtskarte.backend.common.webservice.WebService
 import app.ehrenamtskarte.backend.config.BackendConfiguration
 import app.ehrenamtskarte.backend.stores.importer.Importer
@@ -29,11 +32,26 @@ class Entry : CliktCommand() {
     private val postgresPassword by option()
     private val geocoding by option().choice("true", "false").convert { it.toBoolean() }
     private val geocodingHost by option()
+    private val smtpPasswordEakBayern by option()
+    private val smtpPasswordNuernbergPass by option()
+    private val smtpPasswordShowcase by option()
 
     override fun run() {
         val backendConfiguration = BackendConfiguration.load(config)
 
         currentContext.obj = backendConfiguration.copy(
+            projects = backendConfiguration.projects.map { project ->
+                project.copy(
+                    smtp = project.smtp.copy(
+                        password = when (project.id) {
+                            EAK_BAYERN_PROJECT -> smtpPasswordEakBayern
+                            NUERNBERG_PASS_PROJECT -> smtpPasswordNuernbergPass
+                            SHOWCASE_PROJECT -> smtpPasswordShowcase
+                            else -> null
+                        } ?: project.smtp.password
+                    )
+                )
+            },
             production = production ?: backendConfiguration.production,
             postgres = backendConfiguration.postgres.copy(
                 url = postgresUrl ?: backendConfiguration.postgres.url,
