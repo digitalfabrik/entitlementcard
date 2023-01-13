712362f84c377bcbd015306e945e557cb2295a1a
Michael Markl <marklmichael98@gmail.com>
2023-01-13 18:16:59 +0100

    Extract texts out of build configs

--- frontend/build-configs/nuernberg/index.ts
+++ frontend/build-configs/nuernberg/index.ts
@@ -1,4 +1,6 @@
 import BuildConfigType, { CommonBuildConfigType } from "../types";
+import publisherText from "./publisherText";
+import disclaimerText from "./disclaimerText";
 
 export const nuernbergCommon: CommonBuildConfigType = {
     appName: "Nürnberg-Pass",
@@ -51,52 +53,8 @@ export const nuernbergCommon: CommonBuildConfigType = {
     applicationUrl: "https://meinkonto.nuernberg.de/intelliform/forms/osg/standard/osg/osg-kette-starten/index?lebenslageIdAuswahl=w_500_sha_d_nuernberg-pass",
     publisherAddress: "Stadt Nürnberg\nAmt für Existenzsicherung\nund soziale Integration\nDietzstraße 4\n90443 Nürnberg",
     dataPrivacyPolicyUrl: "https://nuernberg.sozialpass.app/data-privacy-policy",
-    publisherText: `Stadt Nürnberg
-Amt für Existenzsicherung und soziale Integration
-Dietzstraße 4
-90443 Nürnberg
-
-Telefon: REPLACE
-Telefax: REPLACE
-E-Mail: REPLACE
-
-USt-Identifikations-Nr. gemäß § 27 a Umsatzsteuergesetz: DE - REPLACE
-
-Verantwortlich für den Inhalt
-
-REPLACE
-Leiter Referat REPLACE
-Telefon: REPLACE
-Telefax: REPLACE
-E-Mail: REPLACE
-
-E-Mail zum Nürnberg-Pass: REPLACE
-
-Technische Umsetzung:
-Tür an Tür - Digitalfabrik gemeinnützige GmbH
-https://tuerantuer.de/digitalfabrik/`,
-  disclaimerText: `Verantwortlich im Sinne § 7 TMG:
-REPLACE
-Leiter Referat REPLACE
-Telefon: REPLACE
-Telefax: REPLACE
-E-Mail: REPLACE
-Haftung im Sinne §§ 7 - 10 TMG
-
-Die Stadt Nürnberg stellt sein App-Angebot unter folgenden Nutzungsbedingungen zur Verfügung:
-
-• Die App "Nürnberg-Pass" ist nach § 7 Abs. 1 TMG für die eigenen Inhalte, die es zur Nutzung bereithält, nach den allgemeinen Vorschriften verantwortlich. Die Haftung für Schäden materieller oder ideeller Art, die durch die Nutzung der Inhalte verursacht wurden, ist ausgeschlossen, sofern nicht Vorsatz oder grobe Fahrlässigkeit vorliegt.
-Bei der Zusammenstellung und Abgabe der Informationen von Vergünstigungen wird alle Sorgfalt walten gelassen. Eine Gewähr für die inhaltliche Vollständigkeit und Richtigkeit der Daten kann dennoch nicht übernommen werden. Insbesondere wird jede Haftung für solche Schäden ausgeschlossen, die bei Anwendern direkt oder indirekt daraus entstehen, dass die Daten genutzt werden.
-
-• Soweit ein Text von dritter Seite erstellt ist, wird die jeweilige Verfasserin oder der jeweilige Verfasser oder die verantwortliche Einrichtung namentlich benannt. In diesen Fällen ist die Verfasserin oder der Verfasser bzw. die genannte Einrichtung des jeweiligen Dokuments für den Inhalt verantwortlich.
-
-• Die App "Nürnberg-Pass" bewertet den Inhalt der verzeichneten Web-Sites zum Zeitpunkt ihrer Aufnahme in das Verzeichnis sorgfältig, und es werden nur solche Verweise aufgenommen, deren Inhalt nach Prüfung zum Zeitpunkt der Aufnahme nicht gegen geltendes Recht verstößt.
-
-• Die App "Nürnberg-Pass" macht sich den Inhalt der zugänglich gemachten fremden Websites jedoch ausdrücklich nicht zu Eigen. Die Inhalte der Sites, auf die verwiesen wird, können sich ständig ändern - das macht gerade das Wesen eines WWW-Angebots aus. Aus diesem Grund übernimmt die App "Nürnberg-Pass" trotz Prüfung keine Gewähr für die Korrektheit, Vollständigkeit und Verfügbarkeit der jeweiligen fremden Website.
-
-• Die App "Nürnberg-Pass" hat keinen Einfluss auf die aktuelle und zukünftige Gestaltung und Inhalte der Seiten.
-
-• Die App "Nürnberg-Pass" übernimmt keine Haftung für Schäden, die aus der Benutzung der Links entstehen könnten.`,
+    publisherText: publisherText,
+    disclaimerText: disclaimerText,
 };
 
 let nuernberg: BuildConfigType = {
---
+++ frontend/build-configs/nuernberg/publisherText.ts
@@ -0,0 +1,24 @@
+export default `Stadt Nürnberg
+Amt für Existenzsicherung und soziale Integration
+Dietzstraße 4
+90443 Nürnberg
+
+Telefon: REPLACE
+Telefax: REPLACE
+E-Mail: REPLACE
+
+USt-Identifikations-Nr. gemäß § 27 a Umsatzsteuergesetz: DE - REPLACE
+
+Verantwortlich für den Inhalt
+
+REPLACE
+Leiter Referat REPLACE
+Telefon: REPLACE
+Telefax: REPLACE
+E-Mail: REPLACE
+
+E-Mail zum Nürnberg-Pass: REPLACE
+
+Technische Umsetzung:
+Tür an Tür - Digitalfabrik gemeinnützige GmbH
+https://tuerantuer.de/digitalfabrik/`
