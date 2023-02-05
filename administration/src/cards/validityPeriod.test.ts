7c50457ccb680fae4cbe3c55a1f123f6e1ccc1c6
Michael Markl <marklmichael98@gmail.com>
2023-02-05 11:31:52 +0100

    Add jasmine eslint config

--- administration/src/cards/validityPeriod.test.ts
+++ administration/src/cards/validityPeriod.test.ts
@@ -2,42 +2,47 @@ import { format } from 'date-fns'
 import { dateToDaysSinceEpoch, daysSinceEpochToDate } from './validityPeriod'
 
 describe('dateToDaysSinceEpoch', () => {
-  it('epoch', () => {
+  it('should be 0 for epoch', () => {
     expect(dateToDaysSinceEpoch(new Date('1970-01-01T00:00:00.000Z'))).toEqual(0)
   })
-  it('day after epoch', () => {
+
+  it('should be 1 for the day after epoch', () => {
     expect(dateToDaysSinceEpoch(new Date('1970-01-02T00:00:00.000Z'))).toEqual(1)
   })
-  it('day before epoch', () => {
+
+  it('should be -1 for the day before epoch', () => {
     expect(dateToDaysSinceEpoch(new Date('1969-12-31T00:00:00.000Z'))).toEqual(-1)
   })
-  it('year before epoch', () => {
+
+  it('should be -365 for the year before epoch', () => {
     expect(dateToDaysSinceEpoch(new Date('1969-01-01T00:00:00.000Z'))).toEqual(-365)
   })
-  it('randomDayIsConformantWithBackend', () => {
+
+  it('should be conformant with the backend for a random day', () => {
     // Values taken from backend
     expect(dateToDaysSinceEpoch(new Date('2052-12-20T00:00:00.000Z'))).toEqual(30304)
   })
 })
 
 describe('daysSinceEpochToDate', () => {
-  it('epoch', () => {
+  it('should return epoch for 0', () => {
     expect(daysSinceEpochToDate(0)).toEqual(new Date('1970-01-01T00:00:00.000Z'))
   })
-  it('day after epoch', () => {
+
+  it('should return the day after epoch for 1', () => {
     expect(daysSinceEpochToDate(1)).toEqual(new Date('1970-01-02T00:00:00.000Z'))
   })
 
-  it('format epoch day', () => {
+  it('should return the epoch day in the correct format using date-fns for 0', () => {
     expect(format(daysSinceEpochToDate(0), 'dd.MM.yyyy')).toEqual('01.01.1970')
   })
 
-  it('format day after epoch day', () => {
+  it('should return the day after epoch in the correct format using date-fns for 1', () => {
     expect(format(daysSinceEpochToDate(1), 'dd.MM.yyyy')).toEqual('02.01.1970')
   })
 
   // A similar test exists in the backend
-  it('format random day', () => {
+  it('should return the correct day in the correct format using date-fns for a random day', () => {
     // 40518 is defined in day.test.js
     // "07.12.2080" is the output from day.test.js
     // These values were copied to "randomDayIsConformantWithAdministrationFrontend" and the result was verified there
