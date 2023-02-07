6ed102151569b80edcea7e241967545b69e5b4fb
Andy <fischer@integreat-app.de>
2023-02-07 16:14:13 +0100

    Add camera permission check

--- frontend/lib/identification/identification_page.dart
+++ frontend/lib/identification/identification_page.dart
@@ -4,9 +4,11 @@ import 'package:ehrenamtskarte/identification/activation_code_model.dart';
 import 'package:ehrenamtskarte/identification/activation_workflow/activation_code_scanner_page.dart';
 import 'package:ehrenamtskarte/identification/card_detail_view/card_detail_view.dart';
 import 'package:ehrenamtskarte/identification/no_card_view.dart';
+import 'package:ehrenamtskarte/identification/qr_code_scanner/qr_code_camera_permission_dialog.dart';
 import 'package:ehrenamtskarte/identification/verification_workflow/verification_workflow.dart';
 import 'package:ehrenamtskarte/routing.dart';
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:provider/provider.dart';
 import 'package:url_launcher/url_launcher_string.dart';
 
@@ -44,11 +46,25 @@ class IdentificationPage extends StatelessWidget {
     );
   }
 
+  Future<bool> _isCameraPermissionDenied() async {
+    return await Permission.camera.status.isPermanentlyDenied || await Permission.camera.status.isDenied;
+  }
+
   Future<void> _showVerificationDialog(BuildContext context, SettingsModel settings) async {
+    Future<void> showDialog() async => QrCodeCameraPermissionDialog.showPermissionDialog(context);
+    if (await _isCameraPermissionDenied()) {
+      await showDialog();
+      return;
+    }
     await VerificationWorkflow.startWorkflow(context, settings);
   }
 
-  void _startActivation(BuildContext context) {
+  Future<void> _startActivation(BuildContext context) async {
+    Future<void> showDialog() async => QrCodeCameraPermissionDialog.showPermissionDialog(context);
+    if (await _isCameraPermissionDenied()) {
+      await showDialog();
+      return;
+    }
     Navigator.push(context, AppRoute(builder: (context) => const ActivationCodeScannerPage()));
   }
 
---
+++ frontend/lib/identification/qr_code_scanner/qr_code_camera_permission_dialog.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+class QrCodeCameraPermissionDialog extends StatelessWidget {
+  const QrCodeCameraPermissionDialog();
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: const Text("Kamerazugriff erforderlich"),
+      content: SingleChildScrollView(
+        child: ListBody(
+          children: const <Widget>[
+            Text("Zum Lesen von QR-Codes wird Kamerauzugriff benötigt.\nÖffnen sie die Einstellungen und klicken sie auf Erlauben"),
+          ],
+        ),
+      ),
+      actions: <Widget>[
+        TextButton(
+          child: const Text('Einstellungen'),
+          onPressed: () {
+            openAppSettings();
+          },
+        ),
+        TextButton(
+          child: const Text('Ok'),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+      ],
+    );
+  }
+
+  static Future<void> showPermissionDialog(BuildContext context) async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (context) => const QrCodeCameraPermissionDialog(),
+    );
+  }
+}
