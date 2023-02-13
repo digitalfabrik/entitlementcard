78c11b9d6738ff5e1fb5df1f5895da3066d89563
Maximilian Ammann <max@maxammann.org>
2023-02-13 15:59:18 +0100

    Revert "Remove default project in backend"
    This reverts commit 86752be630e0a0e0725a4a361cc1479bfb6ae7b7.

--- backend/src/main/kotlin/app/ehrenamtskarte/backend/common/webservice/constants.kt
+++ backend/src/main/kotlin/app/ehrenamtskarte/backend/common/webservice/constants.kt
@@ -3,3 +3,4 @@ package app.ehrenamtskarte.backend.common.webservice
 const val EAK_BAYERN_PROJECT = "bayern.ehrenamtskarte.app"
 const val NUERNBERG_PASS_PROJECT = "nuernberg.sozialpass.app"
 const val SHOWCASE_PROJECT = "showcase.entitlementcard.app"
+const val DEFAULT_PROJECT = EAK_BAYERN_PROJECT
