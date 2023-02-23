7fb332439bcac83532d61f61602f9c67cc349073
Sarah Sporck <sarah.sporck@tuerantuer.org>
2023-02-23 12:42:04 +0100

    35: log failed activation attempts

--- backend/src/main/kotlin/app/ehrenamtskarte/backend/verification/webservice/schema/CardMutationService.kt
+++ backend/src/main/kotlin/app/ehrenamtskarte/backend/verification/webservice/schema/CardMutationService.kt
@@ -13,8 +13,9 @@ import app.ehrenamtskarte.backend.verification.webservice.schema.types.CardActiv
 import app.ehrenamtskarte.backend.verification.webservice.schema.types.CardGenerationModel
 import com.expediagroup.graphql.generator.annotations.GraphQLDescription
 import graphql.schema.DataFetchingEnvironment
-import java.util.Base64
 import org.jetbrains.exposed.sql.transactions.transaction
+import org.slf4j.LoggerFactory
+import java.util.Base64
 
 @Suppress("unused")
 class CardMutationService {
@@ -26,24 +27,24 @@ class CardMutationService {
 
         transaction {
             val user =
-                    AdministratorEntity.findById(jwtPayload.adminId)
-                            ?: throw UnauthorizedException()
+                AdministratorEntity.findById(jwtPayload.adminId)
+                    ?: throw UnauthorizedException()
             val targetedRegionId = card.regionId
             if (!Authorizer.mayCreateCardInRegion(user, targetedRegionId)) {
                 throw UnauthorizedException()
             }
             val activationSecret =
-                    card.activationSecretBase64?.let {
-                        Base64.getDecoder().decode(card.activationSecretBase64)
-                    }
+                card.activationSecretBase64?.let {
+                    Base64.getDecoder().decode(card.activationSecretBase64)
+                }
 
             CardRepository.insert(
-                    Base64.getDecoder().decode(card.cardInfoHashBase64),
-                    activationSecret,
-                    card.cardExpirationDay,
-                    card.regionId,
-                    user.id.value,
-                    card.codeType
+                Base64.getDecoder().decode(card.cardInfoHashBase64),
+                activationSecret,
+                card.cardExpirationDay,
+                card.regionId,
+                user.id.value,
+                card.codeType
             )
         }
         return true
@@ -51,16 +52,17 @@ class CardMutationService {
 
     @GraphQLDescription("Activate a dynamic entitlement card")
     fun activateCard(
-            project: String,
-            cardInfoHashBase64: String,
-            activationSecretBase64: String,
-            overwrite: Boolean,
-            dfe: DataFetchingEnvironment
+        project: String,
+        cardInfoHashBase64: String,
+        activationSecretBase64: String,
+        overwrite: Boolean,
+        dfe: DataFetchingEnvironment
     ): CardActivationResultModel {
+        val logger = LoggerFactory.getLogger(CardMutationService::class.java)
         val context = dfe.getContext<GraphQLContext>()
         val projectConfig =
-                context.backendConfiguration.projects.find { it.id == project }
-                        ?: throw NullPointerException("Project not found")
+            context.backendConfiguration.projects.find { it.id == project }
+                ?: throw NullPointerException("Project not found")
         val cardHash = Base64.getDecoder().decode(cardInfoHashBase64)
         val activationSecret = Base64.getDecoder().decode(activationSecretBase64)
         val card = transaction {
@@ -68,6 +70,7 @@ class CardMutationService {
         }
 
         if (card == null) {
+            logger.info("${context.remoteIp} failed to activate")
             return CardActivationResultModel(ActivationState.failed)
         }
 
@@ -87,6 +90,6 @@ class CardMutationService {
 
     private fun validateNewCard(card: CardGenerationModel): Boolean {
         return (card.codeType == CodeType.static && card.activationSecretBase64 == null) ||
-                (card.codeType == CodeType.dynamic && card.activationSecretBase64 != null)
+            (card.codeType == CodeType.dynamic && card.activationSecretBase64 != null)
     }
 }
