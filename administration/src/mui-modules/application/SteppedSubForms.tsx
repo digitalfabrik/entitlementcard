d80a8d1af3048547846f2253b5c7a61447893cd1
Sarah Sporck <sarah.sporck@tuerantuer.org>
2023-04-03 14:00:50 +0200

    Use naming modules instead of components

---
+++ administration/src/mui-modules/application/SteppedSubForms.tsx
@@ -0,0 +1,147 @@
+import { SetState, useUpdateStateCallback } from './hooks/useUpdateStateCallback'
+import { Form, ValidationResult } from './util/FormType'
+import { ReactNode, useCallback, useEffect, useState } from 'react'
+import { Button, Divider, Step, ButtonBase, StepContent, StepLabel, Stepper, CircularProgress } from '@mui/material'
+import React from 'react'
+import { useSnackbar } from 'notistack'
+import { Send } from '@mui/icons-material'
+
+const SteppedSubForms = ({
+  activeStep,
+  setActiveStep,
+  loading,
+  subForms,
+  onSubmit,
+}: {
+  activeStep: number
+  setActiveStep: SetState<number>
+  loading: boolean
+  subForms: { label: string; element: ReactNode; validate: () => ValidationResult<unknown> }[]
+  onSubmit: () => void
+}) => {
+  const tryGoTo = (index: number) => {
+    for (let i = 0; i < index; i++) {
+      if (subForms[i].validate().type === 'error') {
+        return setActiveStep(() => i)
+      }
+    }
+    setActiveStep(() => index)
+  }
+  return (
+    <Stepper activeStep={activeStep} orientation='vertical'>
+      {subForms.map(({ validate, label, element }, index) => {
+        return (
+          <Step key={index}>
+            <ButtonBase onClick={() => tryGoTo(index)} style={{ marginLeft: '-8px' }} disabled={loading}>
+              <StepLabel style={{ cursor: 'pointer', padding: '8px' }}>{label}</StepLabel>
+            </ButtonBase>
+            <StepContent>
+              <SubForm
+                validate={validate}
+                index={index}
+                setActiveStep={setActiveStep}
+                loading={loading}
+                onSubmit={index === subForms.length - 1 ? onSubmit : undefined}>
+                {element}
+              </SubForm>
+            </StepContent>
+          </Step>
+        )
+      })}
+    </Stepper>
+  )
+}
+
+type FormContextType = {
+  showAllErrors: boolean
+  disableAllInputs: boolean
+}
+const initialFormContext: FormContextType = { showAllErrors: false, disableAllInputs: false }
+
+export const FormContext = React.createContext<FormContextType>(initialFormContext)
+
+const SubForm = ({
+  children,
+  validate,
+  index,
+  setActiveStep,
+  onSubmit,
+  loading,
+}: {
+  children: ReactNode
+  validate: () => ValidationResult<unknown>
+  index: number
+  setActiveStep: SetState<number>
+  onSubmit?: () => void
+  loading: boolean
+}) => {
+  const [formContext, setFormContxt] = useState<FormContextType>(initialFormContext)
+  const { enqueueSnackbar } = useSnackbar()
+  useEffect(() => setFormContxt(state => ({ ...state, disableAllInputs: loading })), [loading])
+
+  const handleOnSubmit = useCallback(
+    (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault()
+      if (validate().type === 'error') {
+        enqueueSnackbar('Ihre Eingaben sind ungültig oder nicht vollständig.', { variant: 'error' })
+        setFormContxt(state => ({ ...state, showAllErrors: true }))
+      } else if (onSubmit === undefined) {
+        setActiveStep(() => index + 1)
+      } else {
+        onSubmit()
+      }
+    },
+    [validate, onSubmit, setActiveStep, index, enqueueSnackbar]
+  )
+  return (
+    <FormContext.Provider value={formContext}>
+      <form onSubmit={handleOnSubmit}>
+        {children}
+        <Divider sx={{ margin: '16px' }} />
+        {index === 0 ? null : (
+          <Button onClick={() => setActiveStep(() => index - 1)} disabled={formContext.disableAllInputs}>
+            Zurück
+          </Button>
+        )}
+        {onSubmit === undefined ? (
+          <Button type='submit' variant='contained' disabled={formContext.disableAllInputs}>
+            Nächster Schritt
+          </Button>
+        ) : (
+          <Button
+            type='submit'
+            variant='contained'
+            disabled={formContext.disableAllInputs}
+            endIcon={loading ? <CircularProgress size={20} color='inherit' /> : <Send />}>
+            Antrag Abschicken
+          </Button>
+        )}
+      </form>
+    </FormContext.Provider>
+  )
+}
+
+export const useFormAsStep = <
+  Options extends {},
+  ValidatedInput,
+  AdditionalProps extends {},
+  ParentState,
+  KeyInParent extends keyof ParentState
+>(
+  label: string,
+  form: Form<ParentState[KeyInParent], Options, ValidatedInput, AdditionalProps>,
+  parentState: ParentState,
+  setParentState: SetState<ParentState>,
+  keyInParent: KeyInParent,
+  options: Options,
+  additionalProps: AdditionalProps
+): { label: string; validate: () => ValidationResult<unknown>; element: ReactNode } => {
+  const state = parentState[keyInParent]
+  const setState = useUpdateStateCallback(setParentState, keyInParent)
+  const validate = useCallback(() => form.validate(state, options), [state, form, options])
+  const formProps = { ...additionalProps, options, state, setState }
+  const element = <form.Component {...formProps} />
+  return { label, validate, element }
+}
+
+export default SteppedSubForms
---
+++ administration/src/mui-modules/application/forms/OrganizationForm.tsx
@@ -0,0 +1,117 @@
+import { OrganizationInput } from '../../../generated/graphql'
+import { useUpdateStateCallback } from '../hooks/useUpdateStateCallback'
+import { Form } from '../util/FormType'
+import ShortTextForm from '../primitive-inputs/ShortTextForm'
+import AddressForm from './AddressForm'
+import SelectForm from '../primitive-inputs/SelectForm'
+import EmailForm from '../primitive-inputs/EmailForm'
+import CheckboxForm from '../primitive-inputs/CheckboxForm'
+import {
+  CompoundState,
+  createCompoundGetArrayBufferKeys,
+  createCompoundValidate,
+  createCompoundInitialState,
+} from '../util/compoundFormUtils'
+
+const organizationCategoryOptions = {
+  items: [
+    'Soziales/Jugend/Senioren',
+    'Tierschutz',
+    'Sport',
+    'Bildung',
+    'Umwelt-/Naturschutz',
+    'Kultur',
+    'Gesundheit',
+    'Katastrophenschutz/Feuerwehr/Rettungsdienst',
+    'Kirchen',
+    'Andere',
+  ].map(item => ({ label: item, value: item })),
+}
+
+const contactHasGivenPermissionOptions = {
+  required: true,
+  notCheckedErrorMessage: 'Die Kontaktperson muss zugestimmt haben, damit Sie Ihren Antrag senden können.',
+} as const
+
+const SubForms = {
+  name: ShortTextForm,
+  address: AddressForm,
+  category: SelectForm,
+  contactName: ShortTextForm,
+  contactEmail: EmailForm,
+  contactPhone: ShortTextForm,
+  contactHasGivenPermission: CheckboxForm,
+}
+
+const getValidatedCompoundInput = createCompoundValidate(SubForms, {
+  contactHasGivenPermission: contactHasGivenPermissionOptions,
+  category: organizationCategoryOptions,
+})
+
+type State = CompoundState<typeof SubForms>
+type ValidatedInput = OrganizationInput
+type Options = {}
+type AdditionalProps = {}
+const OrganizationForm: Form<State, Options, ValidatedInput, AdditionalProps> = {
+  initialState: createCompoundInitialState(SubForms),
+  getArrayBufferKeys: createCompoundGetArrayBufferKeys(SubForms),
+  validate: state => {
+    const compoundResult = getValidatedCompoundInput(state)
+    if (compoundResult.type === 'error') return compoundResult
+    return {
+      type: 'valid',
+      value: {
+        name: compoundResult.value.name,
+        category: compoundResult.value.category,
+        address: compoundResult.value.address,
+        contact: {
+          name: compoundResult.value.contactName,
+          email: compoundResult.value.contactEmail,
+          telephone: compoundResult.value.contactPhone,
+          hasGivenPermission: compoundResult.value.contactHasGivenPermission,
+        },
+      },
+    }
+  },
+  Component: ({ state, setState }) => (
+    <>
+      <h4>Angaben zur Organisation</h4>
+      <ShortTextForm.Component
+        state={state.name}
+        setState={useUpdateStateCallback(setState, 'name')}
+        label={'Name der Organisation bzw. des Vereins'}
+      />
+      <AddressForm.Component state={state.address} setState={useUpdateStateCallback(setState, 'address')} />
+      <SelectForm.Component
+        state={state.category}
+        setState={useUpdateStateCallback(setState, 'category')}
+        label='Einsatzgebiet'
+        options={organizationCategoryOptions}
+      />
+      <h4>Kontaktperson der Organisation</h4>
+      <ShortTextForm.Component
+        state={state.contactName}
+        setState={useUpdateStateCallback(setState, 'contactName')}
+        label='Vor- und Nachname'
+      />
+      <EmailForm.Component
+        state={state.contactEmail}
+        setState={useUpdateStateCallback(setState, 'contactEmail')}
+        label='E-Mail-Adresse'
+      />
+      <ShortTextForm.Component
+        state={state.contactPhone}
+        setState={useUpdateStateCallback(setState, 'contactPhone')}
+        label='Telefon'
+      />
+      <CheckboxForm.Component
+        state={state.contactHasGivenPermission}
+        setState={useUpdateStateCallback(setState, 'contactHasGivenPermission')}
+        label='Die Kontaktperson hat der Weitergabe seiner Daten zum Zwecke der Antragsverarbeitung zugestimmt und darf zur Überprüfung kontaktiert werden.'
+        options={contactHasGivenPermissionOptions}
+      />
+    </>
+  ),
+}
+
+export default OrganizationForm
