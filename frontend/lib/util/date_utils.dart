67568f4b42a8c3a941aaabb113c6a6773539e60c
Andy <fischer@integreat-app.de>
2023-04-28 10:07:36 +0200

    639: adjust date time for secondsSinceEpoch

--- frontend/lib/util/date_utils.dart
+++ frontend/lib/util/date_utils.dart
@@ -1,7 +1,7 @@
 import 'package:fixnum/fixnum.dart';
 
 Int64 secondsSinceEpoch(DateTime date) {
-  return Int64(date.difference(DateTime(1970).toUtc()).inSeconds);
+  return Int64(date.difference(DateTime.utc(1970)).inSeconds);
 }
 
 // 7days x 24h + 1day for expiration day and 12h for UTC+12 = 192h * 3600
