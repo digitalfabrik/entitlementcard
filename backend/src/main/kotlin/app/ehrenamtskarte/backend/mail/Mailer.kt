0dafa404f0923fd1067d08f2862b556bf6cd16df
Sarah Sporck <sarah.sporck@tuerantuer.org>
2023-05-10 11:02:32 +0200

    905: improve verification mail message

--- backend/src/main/kotlin/app/ehrenamtskarte/backend/mail/Mailer.kt
+++ backend/src/main/kotlin/app/ehrenamtskarte/backend/mail/Mailer.kt
@@ -132,7 +132,7 @@ object Mailer {
         val message = """
         Guten Tag ${applicationVerification.contactName},
 
-        Sie wurden gebeten, die Angaben eines Antrags auf Ehrenamtskarte zu bestätigen. Die Antragsstellerin oder der
+        Sie wurden gebeten, die Angaben eines Antrags auf eine Ehrenamtskarte zu bestätigen. Die Antragsstellerin oder der
         Antragssteller hat Sie als Kontaktperson der Organisation ${applicationVerification.organizationName} angegeben. 
         Sie können den Antrag unter folgendem Link einsehen und die Angaben bestätigen oder ihnen widersprechen:
         ${projectConfig.administrationBaseUrl}/antrag-verifizieren/${URLEncoder.encode(applicationVerification.accessKey, StandardCharsets.UTF_8)}
