08a51ceeebcaf46d5f86a6cc76e43ab9e39def99
Sarah Sporck <sarah.sporck@tuerantuer.org>
2023-05-24 11:29:19 +0200

    900: fix tests

--- administration/src/bp-modules/cards/hooks/useCardGenerator.test.tsx
+++ administration/src/bp-modules/cards/hooks/useCardGenerator.test.tsx
@@ -42,6 +42,7 @@ describe('useCardGenerator', () => {
     id: 0,
     name: 'augsburg',
     prefix: 'a',
+    activatedForApplication: true
   }
 
   const cards = [
--- administration/src/cards/CardBlueprint.test.ts
+++ administration/src/cards/CardBlueprint.test.ts
@@ -9,6 +9,7 @@ describe('CardBlueprint', () => {
     id: 0,
     name: 'augsburg',
     prefix: 'a',
+    activatedForApplication: true
   }
 
   const cardConfig = {
