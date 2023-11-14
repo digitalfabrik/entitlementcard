3ccd60c62faef8feeec51a030d061e9d53163d17
Daniel Heid <mail@daniel-heid.de>
2023-11-14 18:29:27 +0100

    Update to Matomo Java Tracker 3.0.6

--- backend/src/main/kotlin/app/ehrenamtskarte/backend/matomo/Matomo.kt
+++ backend/src/main/kotlin/app/ehrenamtskarte/backend/matomo/Matomo.kt
@@ -12,10 +12,13 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.matomo.java.tracking.MatomoRequest
-import org.matomo.java.tracking.MatomoRequestBuilder
+import org.matomo.java.tracking.MatomoRequest.MatomoRequestBuilder
 import org.matomo.java.tracking.MatomoTracker
+import org.matomo.java.tracking.TrackerConfiguration
+import org.matomo.java.tracking.parameters.AcceptLanguage
 import org.slf4j.LoggerFactory
 import java.io.IOException
+import java.net.URI
 import java.util.concurrent.ExecutionException
 
 object Matomo {
@@ -25,7 +28,7 @@ object Matomo {
         CoroutineScope(Dispatchers.IO).launch {
             val siteId = matomoConfig.siteId
             val url = matomoConfig.url
-            val tracker = MatomoTracker(url)
+            val tracker = MatomoTracker(TrackerConfiguration.builder().apiEndpoint(URI.create(url)).build())
 
             val matomoRequest = requestBuilder
                 .siteId(siteId)
@@ -33,11 +36,8 @@ object Matomo {
                 .build()
 
             try {
-                val response = tracker.sendRequestAsync(matomoRequest)
-                val httpResponse = response.get()
-                if (httpResponse.statusLine.statusCode > 399) {
-                    logger.error("Request failed with status code ${httpResponse.statusLine.statusCode}")
-                }
+                // will log errors using it's own logger
+                tracker.sendRequestAsync(matomoRequest)
             } catch (e: Exception) {
                 when (e) {
                     is IOException -> logger.error("Could not send request to Matomo")
@@ -52,18 +52,15 @@ object Matomo {
         CoroutineScope(Dispatchers.IO).launch {
             val siteId = matomoConfig.siteId
             val url = matomoConfig.url
-            val tracker = MatomoTracker(url)
+            val tracker = MatomoTracker(TrackerConfiguration.builder().apiEndpoint(URI.create(url)).build())
             val matomoRequests = requestBuilder.map {
                 it.siteId(siteId)
                 it.authToken(matomoConfig.accessToken)
                 it.build()
             }
             try {
-                val response = tracker.sendBulkRequestAsync(matomoRequests)
-                val httpResponse = response.get()
-                if (httpResponse.statusLine.statusCode > 399) {
-                    logger.debug("Request failed with status code ${httpResponse.statusLine.statusCode}")
-                }
+                // will log errors using it's own logger
+                tracker.sendBulkRequestAsync(matomoRequests)
             } catch (e: Exception) {
                 when (e) {
                     is IOException -> logger.debug("Could not send request to Matomo")
@@ -78,17 +75,17 @@ object Matomo {
         val userAgent = request.getHeader("User-Agent")
         val acceptLanguage = request.getHeader("Accept-Language")
         return builder
-            .headerAcceptLanguage(acceptLanguage)
+            .headerAcceptLanguage(AcceptLanguage.fromHeader(acceptLanguage))
             .headerUserAgent(userAgent)
             .visitorIp(request.remoteAddr)
     }
 
     private fun buildCardsTrackingRequest(request: HttpServletRequest, regionId: Int, query: String, codeType: CodeType, numberOfCards: Int): MatomoRequestBuilder {
-        return MatomoRequest.builder()
+        return MatomoRequest.request()
             .eventAction(query)
             .eventCategory(codeType.toString())
-            .eventValue(numberOfCards)
-            .customTrackingParameters(mapOf("dimension1" to regionId))
+            .eventValue(numberOfCards.toDouble())
+            .dimensions(mapOf(1L to regionId))
             .also { attachRequestInformation(it, request) }
     }
 
@@ -126,11 +123,11 @@ object Matomo {
         val card = transaction { CardRepository.findByHash(projectConfig.id, cardHash) }
         sendTrackingRequest(
             projectConfig.matomo,
-            MatomoRequest.builder()
+            MatomoRequest.request()
                 .eventAction(query)
                 .eventCategory(codeType.toString())
                 .eventName(if (successful) "verification successful" else "verification failed")
-                .customTrackingParameters(if (card != null) mapOf("dimension1" to card.regionId) else emptyMap())
+                .dimensions(if (card != null) mapOf(1L to card.regionId) else emptyMap())
                 .also { attachRequestInformation(it, request) }
         )
     }
@@ -140,10 +137,10 @@ object Matomo {
         val card = transaction { CardRepository.findByHash(projectConfig.id, cardHash) }
         sendTrackingRequest(
             projectConfig.matomo,
-            MatomoRequest.builder()
+            MatomoRequest.request()
                 .eventAction(query)
-                .eventValue(if (successful) 1 else 0)
-                .customTrackingParameters(if (card != null) mapOf("dimension1" to card.regionId) else emptyMap())
+                .eventValue(if (successful) 1.0 else 0.0)
+                .dimensions(if (card != null) mapOf(1L to card.regionId) else emptyMap())
                 .also { attachRequestInformation(it, request) }
         )
     }
@@ -153,7 +150,7 @@ object Matomo {
         if (params.searchText === null && params.categoryIds === null) return
         sendTrackingRequest(
             projectConfig.matomo,
-            MatomoRequest.builder()
+            MatomoRequest.request()
                 .actionName(query)
                 .searchQuery((params.searchText ?: "") + "?categories=" + (params.categoryIds?.joinToString(",") ?: ""))
                 .searchResultsCount(numResults.toLong())
