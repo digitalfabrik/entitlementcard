e52c7cda0262e65ef271d8c6bfde16a2efba5c1e
Andy <fischer@integreat-app.de>
2023-11-24 19:51:02 +0100

    1224: add translations for permissions for ios

--- frontend/lib/about/language_change.dart
+++ frontend/lib/about/language_change.dart
@@ -1,9 +1,8 @@
 import 'package:ehrenamtskarte/build_config/build_config.dart' show buildConfig;
+import 'package:ehrenamtskarte/configuration/settings_model.dart';
 import 'package:ehrenamtskarte/l10n/translations.g.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
-import '../configuration/settings_model.dart';
 
 class LanguageChange extends StatelessWidget {
   const LanguageChange({super.key});
