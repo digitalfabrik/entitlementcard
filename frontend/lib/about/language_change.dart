1039d936dece0b1a389d660a2de80151047936b8
Andy <fischer@integreat-app.de>
2023-11-25 15:16:51 +0100

    1224: move language change languages from translations to constant

--- frontend/lib/about/language_change.dart
+++ frontend/lib/about/language_change.dart
@@ -4,12 +4,12 @@ import 'package:ehrenamtskarte/l10n/translations.g.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+Map<String, String> nativeLanguageNames = {'en': 'English', 'de': 'Deutsch'};
+
 class LanguageChange extends StatelessWidget {
   const LanguageChange({super.key});
-
   @override
   Widget build(BuildContext context) {
-    final t = context.t;
     return Column(children: [
       ...buildConfig.appLocales.map((item) => DecoratedBox(
           decoration: BoxDecoration(
@@ -18,7 +18,7 @@ class LanguageChange extends StatelessWidget {
                   : null),
           child: ListTile(
               title: Text(
-                t.about.languages[item]!,
+                nativeLanguageNames[item]!,
                 textAlign: TextAlign.center,
                 style: TextStyle(fontWeight: FontWeight.bold),
               ),
