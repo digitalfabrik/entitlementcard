edd907fa6b0964672851c2903811644e684e9953
Andy <fischer@integreat-app.de>
2024-03-06 10:39:10 +0100

    1212: use staging link scheme for development

--- administration/src/util/getDeepLinkFromQrCode.test.ts
+++ administration/src/util/getDeepLinkFromQrCode.test.ts
@@ -3,7 +3,6 @@ import {
   ACTIVATION_PATH,
   BAYERN_PRODUCTION_ID,
   BAYERN_STAGING_ID,
-  CUSTOM_SCHEME,
   HTTPS_SCHEME,
 } from 'build-configs'
 
@@ -45,7 +44,7 @@ describe('DeepLink generation', () => {
     value: code.dynamicActivationCode,
   }
 
-  const encodedActivationCodeBase64 = 'ChsKGQoJVGhlYSBUZXN0EJGjARoICgIIACICCAA%3D'
+  const encodedActivationCodeBase64 = 'ChsKGQoJVGhlYSBUZXN0EJOjARoICgIIACICCAA%3D'
   const overrideHostname = (hostname: string) =>
     Object.defineProperty(window, 'location', {
       value: {
@@ -55,10 +54,10 @@ describe('DeepLink generation', () => {
     })
 
   it('should generate a correct link for development', () => {
-    localStorage.setItem(LOCAL_STORAGE_PROJECT_KEY, BAYERN_PRODUCTION_ID)
-    const projectId = getBuildConfig(window.location.hostname).common.projectId.local
+    localStorage.setItem(LOCAL_STORAGE_PROJECT_KEY, BAYERN_STAGING_ID)
+    const projectId = getBuildConfig(window.location.hostname).common.projectId.staging
     expect(getDeepLinkFromQrCode(dynamicPdfQrCode)).toBe(
-      `${CUSTOM_SCHEME}://${projectId}/${ACTIVATION_PATH}/${ACTIVATION_FRAGMENT}${encodedActivationCodeBase64}/`
+      `${HTTPS_SCHEME}://${projectId}/${ACTIVATION_PATH}/${ACTIVATION_FRAGMENT}${encodedActivationCodeBase64}/`
     )
   })
   it('should generate a correct link for staging', () => {
--- administration/src/util/getDeepLinkFromQrCode.ts
+++ administration/src/util/getDeepLinkFromQrCode.ts
@@ -1,4 +1,4 @@
-import { ACTIVATION_FRAGMENT, ACTIVATION_PATH, CUSTOM_SCHEME, HTTPS_SCHEME } from 'build-configs/constants'
+import { ACTIVATION_FRAGMENT, ACTIVATION_PATH, HTTPS_SCHEME } from 'build-configs/constants'
 
 import { PdfQrCode } from '../cards/pdf/PdfQrCodeElement'
 import { QrCode } from '../generated/card_pb'
@@ -6,16 +6,14 @@ import { uint8ArrayToBase64 } from './base64'
 import { getBuildConfig } from './getBuildConfig'
 import { isDevMode, isStagingMode } from './helper'
 
-type DeepLinkScheme = typeof CUSTOM_SCHEME | typeof HTTPS_SCHEME
-
 const getDeepLinkFromQrCode = (qrCode: PdfQrCode): string => {
   const qrCodeContent = new QrCode({
     qrCode: qrCode,
   }).toBinary()
-  const scheme: DeepLinkScheme = isDevMode() ? CUSTOM_SCHEME : HTTPS_SCHEME
   const buildConfigProjectId = getBuildConfig(window.location.hostname).common.projectId
-  const host = isStagingMode() ? buildConfigProjectId.staging : buildConfigProjectId.production
-  const deepLink = `${scheme}://${host}/${ACTIVATION_PATH}/${ACTIVATION_FRAGMENT}${encodeURIComponent(
+  // custom link schemes don't work in browsers or pdf thats why we use the staging link scheme also for development
+  const host = isStagingMode() || isDevMode() ? buildConfigProjectId.staging : buildConfigProjectId.production
+  const deepLink = `${HTTPS_SCHEME}://${host}/${ACTIVATION_PATH}/${ACTIVATION_FRAGMENT}${encodeURIComponent(
     uint8ArrayToBase64(qrCodeContent)
   )}/`
   // Log deepLink for development and testing purposes
