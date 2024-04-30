68cec6c68f6cfd63fb99c0c6694cdb05ea9e8397
Andy <fischer@integreat-app.de>
2024-04-30 16:35:46 +0200

    1399: create custom legend, create custom tooltip, move legend to top

--- administration/src/bp-modules/statistics/components/StatisticsBarChart.tsx
+++ administration/src/bp-modules/statistics/components/StatisticsBarChart.tsx
@@ -5,6 +5,7 @@ import styled from 'styled-components'
 
 import { CardStatisticsResultModel } from '../../../generated/graphql'
 import { ProjectConfigContext } from '../../../project-configs/ProjectConfigContext'
+import StatisticsBarTooltip from './StatisticsBarTooltip'
 
 const BarContainer = styled.div<{ height: number }>`
   height: ${props => props.height}px;
@@ -14,6 +15,10 @@ type StatisticsBarChartProps = {
   statistics: CardStatisticsResultModel[]
 }
 
+export const statisticKeyLabels = new Map<string, string>([
+  ['cardsCreated', 'Erstellte Karten'],
+  ['cardsActivated', 'Davon aktiviert'],
+])
 const StatisticsBarChart = ({ statistics }: StatisticsBarChartProps): ReactElement => {
   const { cardStatistics } = useContext(ProjectConfigContext)
   const barHeight = 50
@@ -29,12 +34,14 @@ const StatisticsBarChart = ({ statistics }: StatisticsBarChartProps): ReactEleme
     )
   }
 
-  const statisticKeys = Object.keys(statistics[0])
+  const statisticKeys = Object.keys(statistics[0]).filter(item => item !== 'region')
+
   return (
     <BarContainer height={statistics.length * barHeight + axisHeight}>
       <ResponsiveBar
         data={statistics}
-        keys={[statisticKeys[2], statisticKeys[3]]}
+        tooltip={StatisticsBarTooltip}
+        keys={[statisticKeys[0], statisticKeys[1]]}
         indexBy='region'
         margin={{ top: 20, right: 300, bottom: 50, left: 300 }}
         innerPadding={2.0}
@@ -49,13 +56,6 @@ const StatisticsBarChart = ({ statistics }: StatisticsBarChartProps): ReactEleme
           legendPosition: 'middle',
           legendOffset: 44,
         }}
-        axisLeft={{
-          tickSize: 10,
-          tickPadding: 5,
-          legend: 'Region',
-          legendOffset: -180,
-          legendPosition: 'middle',
-        }}
         theme={{
           axis: { legend: { text: { fontSize: 14, fontWeight: 700 } } },
           text: { fontSize: 14 },
@@ -64,10 +64,17 @@ const StatisticsBarChart = ({ statistics }: StatisticsBarChartProps): ReactEleme
         legends={[
           {
             dataFrom: 'keys',
-            anchor: 'bottom-right',
+            data: statisticKeys.map((item, index) => {
+              return {
+                color: [cardStatistics.theme.primaryColor, cardStatistics.theme.primaryColorLight][index],
+                id: item,
+                label: statisticKeyLabels.get(item)!,
+              }
+            }),
+            anchor: 'top-right',
             direction: 'column',
             translateX: 140,
-            translateY: 54,
+            translateY: 20,
             itemsSpacing: 2,
             itemWidth: 100,
             itemHeight: 30,
