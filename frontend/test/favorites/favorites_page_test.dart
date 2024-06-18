d1ff680317fe30ecb08e5e0151ca9068e122e678
seluianova <viktoria.seluianova@tuerantuer.org>
2024-06-18 13:19:54 +0200

    1498: Add component tests for Favorites page

---
+++ frontend/test/favorites/favorites_page_test.dart
@@ -0,0 +1,174 @@
+import 'package:ehrenamtskarte/configuration/configuration.dart';
+import 'package:ehrenamtskarte/favorites/favorites_model.dart';
+import 'package:ehrenamtskarte/favorites/favorites_page.dart';
+import 'package:ehrenamtskarte/l10n/translations.g.dart';
+import 'package:ehrenamtskarte/store_widgets/accepting_store_summary.dart';
+import 'package:ehrenamtskarte/store_widgets/detail/detail_app_bar.dart';
+import 'package:ehrenamtskarte/store_widgets/removed_store_content.dart';
+import 'package:ehrenamtskarte/store_widgets/removed_store_summary.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../utils/fakes.dart';
+import '../utils/mocks.dart';
+
+void main() {
+  group('Favorites page', () {
+    setUpAll(() {
+      registerFallbackValue(FakeQueryOptions());
+      registerFallbackValue(FakeRoute());
+    });
+
+    final mockClient = MockGraphQLClient();
+    final mockObserver = MockNavigatorObserver();
+
+    // Create a wrapper widget for FavoritesPage to provide necessary context
+    Widget createTestWidget() {
+      return Configuration(
+        mapStyleUrl: 'dummy',
+        graphqlUrl: 'dummy',
+        projectId: 'dummy',
+        showDevSettings: false,
+        child: ChangeNotifierProvider<FavoritesModel>(
+          create: (_) => FavoritesModel()..initialize(),
+          child: MaterialApp(
+            navigatorObservers: [mockObserver],
+            home: TranslationProvider(
+              child: GraphQLProvider(
+                client: ValueNotifier(mockClient),
+                child: const FavoritesPage(),
+              ),
+            ),
+          ),
+        ),
+      );
+    }
+
+    testWidgets('Favorite store is available', (WidgetTester tester) async {
+      SharedPreferences.setMockInitialValues(<String, List<String>>{
+        'favorites': ['{"storeId":1,"storeName":"Test store","categoryId":9}']
+      });
+
+      when(() => mockClient.query(any())).thenAnswer((invocation) async {
+        final options = invocation.positionalArguments.first as QueryOptions;
+        return QueryResult(
+          data: {
+            'physicalStoresByIdInProject': [
+              {
+                'id': 1,
+                'coordinates': {'lat': 48.235256, 'lng': 12.678656},
+                'store': {
+                  'id': 1,
+                  'name': 'Test store',
+                  'description': 'Test description',
+                  'contact': {'id': 1, 'email': null, 'telephone': '08671 95 80 45', 'website': null},
+                  'category': {'id': 9, 'name': 'Sonstiges'}
+                },
+                'address': {'street': 'Bahnhofstraße 1', 'postalCode': '84503', 'location': 'Altötting'}
+              }
+            ]
+          },
+          source: QueryResultSource.network,
+          options: options,
+        );
+      });
+
+      await tester.pumpWidget(createTestWidget());
+      await tester.pumpAndSettle();
+
+      expect(find.text(t.favorites.title), findsOneWidget);
+
+      final acceptingStoreSummary = find.byType(AcceptingStoreSummary);
+      expect(acceptingStoreSummary, findsOneWidget);
+      expect(find.descendant(of: acceptingStoreSummary, matching: find.text('Test store')), findsOneWidget);
+      expect(find.descendant(of: acceptingStoreSummary, matching: find.text('Test description')), findsOneWidget);
+    });
+
+    testWidgets('Favorite store is no longer available', (WidgetTester tester) async {
+      SharedPreferences.setMockInitialValues(<String, List<String>>{
+        'favorites': ['{"storeId":1,"storeName":"Test store","categoryId":9}']
+      });
+
+      when(() => mockClient.query(any())).thenAnswer((invocation) async {
+        final options = invocation.positionalArguments.first as QueryOptions;
+        return QueryResult(
+          data: {
+            'physicalStoresByIdInProject': [null]
+          },
+          source: QueryResultSource.network,
+          options: options,
+        );
+      });
+
+      await tester.pumpWidget(createTestWidget());
+      await tester.pumpAndSettle();
+
+      expect(find.text(t.favorites.title), findsOneWidget);
+
+      final removedStoreSummary = find.byType(RemovedStoreSummary);
+      expect(removedStoreSummary, findsOneWidget);
+      expect(find.descendant(of: removedStoreSummary, matching: find.text('Test store')), findsOneWidget);
+      expect(find.descendant(of: removedStoreSummary, matching: find.text(t.store.acceptingStoreNotAvailable)),
+          findsOneWidget);
+
+      await tester.tap(find.byType(RemovedStoreSummary));
+      await tester.pumpAndSettle();
+
+      final detailAppBar = find.byType(DetailAppBar);
+      expect(detailAppBar, findsOneWidget);
+      expect(find.descendant(of: detailAppBar, matching: find.text('Test store')), findsOneWidget);
+      expect(find.descendant(of: detailAppBar, matching: find.text(t.category.other)), findsOneWidget);
+
+      final removedStoreContent = find.byType(RemovedStoreContent);
+      expect(removedStoreContent, findsOneWidget);
+      expect(find.descendant(of: removedStoreContent, matching: find.text(t.store.acceptingStoreNotAvailable)),
+          findsOneWidget);
+      expect(find.descendant(of: removedStoreContent, matching: find.text(t.store.removeDescription)), findsOneWidget);
+      expect(find.widgetWithText(OutlinedButton, t.store.removeButtonText), findsOneWidget);
+
+      await tester.tap(find.widgetWithText(OutlinedButton, t.store.removeButtonText));
+      await tester.pump();
+
+      final snackBar = find.byType(SnackBar);
+      expect(snackBar, findsOneWidget);
+      expect(find.descendant(of: snackBar, matching: find.text(t.favorites.favoriteHasBeenRemoved)), findsOneWidget);
+
+      await tester.pumpAndSettle();
+
+      expect(find.text(t.favorites.title), findsOneWidget);
+      expect(find.text(t.favorites.noFavoritesFound), findsOneWidget);
+    });
+
+    testWidgets('No favorites found', (WidgetTester tester) async {
+      SharedPreferences.setMockInitialValues({});
+
+      await tester.pumpWidget(createTestWidget());
+      await tester.pumpAndSettle();
+
+      expect(find.text(t.favorites.title), findsOneWidget);
+      expect(find.text(t.favorites.noFavoritesFound), findsOneWidget);
+    });
+
+    testWidgets('Failed to load favorites', (WidgetTester tester) async {
+      SharedPreferences.setMockInitialValues(<String, String>{'favorites': 'incorrect data'});
+
+      await tester.pumpWidget(createTestWidget());
+      await tester.pumpAndSettle();
+
+      expect(find.text(t.favorites.title), findsOneWidget);
+      expect(find.text(t.favorites.loadingFailed), findsOneWidget);
+      expect(find.widgetWithText(OutlinedButton, t.common.tryAgain), findsOneWidget);
+
+      await tester.tap(find.widgetWithText(OutlinedButton, t.common.tryAgain));
+      await tester.pumpAndSettle();
+
+      expect(find.text(t.favorites.title), findsOneWidget);
+      expect(find.text(t.favorites.loadingFailed), findsOneWidget);
+      expect(find.widgetWithText(OutlinedButton, t.common.tryAgain), findsOneWidget);
+    });
+  });
+}
