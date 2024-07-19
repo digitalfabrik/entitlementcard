f239b21a4d041229db64b0d6152afdbe33fd95cd
Michael Markl <marklmichael98@gmail.com>
2024-07-19 16:39:30 +0200

    Do not send stack traces to users

--- backend/src/main/kotlin/app/ehrenamtskarte/backend/exception/webservice/exceptions/InvalidCredentialsException.kt
+++ backend/src/main/kotlin/app/ehrenamtskarte/backend/exception/webservice/exceptions/InvalidCredentialsException.kt
@@ -4,4 +4,4 @@ package app.ehrenamtskarte.backend.exception.webservice.exceptions
 import app.ehrenamtskarte.backend.exception.GraphQLBaseException
 import app.ehrenamtskarte.backend.exception.webservice.schema.GraphQLExceptionCode
 
-class InvalidCredentialsException() : GraphQLBaseException(GraphQLExceptionCode.INVALID_CREDENTIALS)
+class InvalidCredentialsException : GraphQLBaseException(GraphQLExceptionCode.INVALID_CREDENTIALS)
--- backend/src/main/kotlin/app/ehrenamtskarte/backend/exception/webservice/exceptions/InvalidDataPolicySizeException.kt
+++ backend/src/main/kotlin/app/ehrenamtskarte/backend/exception/webservice/exceptions/InvalidDataPolicySizeException.kt
@@ -1,13 +1,9 @@
 package app.ehrenamtskarte.backend.exception.webservice.exceptions
 
+import app.ehrenamtskarte.backend.exception.GraphQLBaseException
 import app.ehrenamtskarte.backend.exception.webservice.schema.GraphQLExceptionCode
-import graphql.GraphqlErrorException
 
-open class InvalidDataPolicySizeException(maxSize: Int) : GraphqlErrorException(
-    newErrorException().extensions(
-        mapOf(
-            Pair("code", GraphQLExceptionCode.INVALID_DATA_POLICY_SIZE),
-            Pair("maxSize", maxSize)
-        )
-    )
+open class InvalidDataPolicySizeException(maxSize: Int) : GraphQLBaseException(
+    GraphQLExceptionCode.INVALID_DATA_POLICY_SIZE,
+    mapOf("maxSize" to maxSize)
 )
