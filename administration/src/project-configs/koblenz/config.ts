249e5b32aab734d76aa7038db80f40d3964a5161
Andy <fischer@integreat-app.de>
2024-07-30 11:56:40 +0200

    1547: add basic project config for koblenz

---
+++ administration/src/project-configs/koblenz/config.ts
@@ -0,0 +1,29 @@
+import BirthdayExtension from '../../cards/extensions/BirthdayExtension'
+import RegionExtension from '../../cards/extensions/RegionExtension'
+import StartDayExtension from '../../cards/extensions/StartDayExtension'
+import { ProjectConfig } from '../getProjectConfig'
+import { DataPrivacyBaseText, dataPrivacyBaseHeadline } from './dataPrivacyBase'
+import pdfConfig from './pdf'
+
+const config: ProjectConfig = {
+  name: 'Digitaler Koblenz-Pass',
+  projectId: 'koblenz.sozialpass.app',
+  staticQrCodesEnabled: true,
+  card: {
+    nameColumnName: 'Name',
+    expiryColumnName: 'Ablaufdatum',
+    extensionColumnNames: ['Startdatum', 'Geburtsdatum', null],
+    defaultValidity: { years: 1 },
+    extensions: [StartDayExtension, BirthdayExtension, RegionExtension],
+  },
+  dataPrivacyHeadline: dataPrivacyBaseHeadline,
+  dataPrivacyContent: DataPrivacyBaseText,
+  timezone: 'Europe/Berlin',
+  pdf: pdfConfig,
+  csvExport: {
+    enabled: false,
+  },
+  cardStatistics: { enabled: false },
+}
+
+export default config
