1748a9bc0cb2b0c9ce11c981be8941d0025acf83
seluianova <viktoria.seluianova@tuerantuer.org>
2024-08-27 12:15:39 +0200

    1551: Update null regionIdentifier to dummy value in the migration

--- backend/src/main/kotlin/app/ehrenamtskarte/backend/migration/migrations/V0018_AlterRegionIdentifierConstraint.kt
+++ backend/src/main/kotlin/app/ehrenamtskarte/backend/migration/migrations/V0018_AlterRegionIdentifierConstraint.kt
@@ -7,11 +7,12 @@ import app.ehrenamtskarte.backend.migration.Statement
  * Adjust regionidentifier_unique constraint to allow regions with the same identifier across different projects
  */
 @Suppress("ClassName")
-internal class V0018_AlterRegionIdentifierConstraint() : Migration() {
+internal class V0018_AlterRegionIdentifierConstraint : Migration() {
     override val migrate: Statement = {
         exec(
             """
             ALTER TABLE regions DROP CONSTRAINT regions_regionidentifier_unique;
+            UPDATE regions SET "regionIdentifier" = '00000' WHERE "regionIdentifier" IS NULL;
             ALTER TABLE regions ALTER COLUMN "regionIdentifier" SET NOT NULL;
             ALTER TABLE regions ADD CONSTRAINT unique_projectid_regionidentifier UNIQUE ("projectId", "regionIdentifier");
             """.trimIndent()
