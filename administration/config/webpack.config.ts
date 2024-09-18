5f5bea9a481e230320a8aa279d8b07a033c059f7
Steffen Kleinle <steffen.kleinle@mailbox.org>
2024-09-18 13:33:37 +0200

    1596: Fix webpack config

--- administration/config/webpack.config.ts
+++ administration/config/webpack.config.ts
@@ -210,7 +210,11 @@ function createWebpackConfig(webpackEnv: 'development' | 'production'): webpack.
       // We placed these paths second because we want `node_modules` to "win"
       // if there are any conflicts. This matches Node resolution mechanism.
       // https://github.com/facebook/create-react-app/issues/253
-      modules: ['node_modules', paths.appNodeModules].concat(modules.additionalModulePaths ?? []),
+      modules: ['node_modules', paths.appNodeModules].concat(
+        modules.additionalModulePaths !== null && modules.additionalModulePaths !== ''
+          ? modules.additionalModulePaths
+          : []
+      ),
       // These are the reasonable defaults supported by the Node ecosystem.
       // We also include JSX as a common component filename extension to support
       // some tools, although we do not recommend using it, see:
