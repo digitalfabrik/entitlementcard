b31e3a6feaddb0b8dcf68eafef98d8ed5e391231
Andy <fischer@integreat-app.de>
2024-10-02 10:12:08 +0200

    1609: add tests for koblenz

--- administration/src/bp-modules/stores/__tests__/AcceptanceStoreEntry.test.ts
+++ administration/src/bp-modules/stores/__tests__/AcceptanceStoreEntry.test.ts
@@ -1,18 +1,27 @@
 import { LOCAL_STORAGE_PROJECT_KEY } from '../../../project-configs/constants'
+import koblenzConfig from '../../../project-configs/koblenz/config'
 import nuernbergConfig from '../../../project-configs/nuernberg/config'
 import { AcceptingStoreEntry } from '../AcceptingStoreEntry'
 import { invalidStoreData, validStoreData } from '../__mock__/mockStoreEntry'
 
-const fields = nuernbergConfig.storeManagement.enabled ? nuernbergConfig.storeManagement.fields : []
 describe('AcceptanceStoreEntry', () => {
-  localStorage.setItem(LOCAL_STORAGE_PROJECT_KEY, 'nuernberg.sozialpass.app')
-  it('should be validated false store invalid entries', () => {
-    const store = new AcceptingStoreEntry(invalidStoreData, fields)
-    expect(store.isValid()).toBeFalsy()
-  })
+  it.each([{ projectConfig: nuernbergConfig }, { projectConfig: koblenzConfig }])(
+    `should be validated false store invalid entries for $projectConfig.name`,
+    ({ projectConfig }) => {
+      localStorage.setItem(LOCAL_STORAGE_PROJECT_KEY, projectConfig.projectId)
+      const fields = projectConfig.storeManagement.enabled ? projectConfig.storeManagement.fields : []
+      const store = new AcceptingStoreEntry(invalidStoreData, fields)
+      expect(store.isValid()).toBeFalsy()
+    }
+  )
 
-  it('should be validated true store valid entries', () => {
-    const store = new AcceptingStoreEntry(validStoreData, fields)
-    expect(store.isValid()).toBeTruthy()
-  })
+  it.each([{ projectConfig: nuernbergConfig }, { projectConfig: koblenzConfig }])(
+    'should be validated true store valid entries $projectConfig.name',
+    ({ projectConfig }) => {
+      localStorage.setItem(LOCAL_STORAGE_PROJECT_KEY, projectConfig.projectId)
+      const fields = projectConfig.storeManagement.enabled ? projectConfig.storeManagement.fields : []
+      const store = new AcceptingStoreEntry(validStoreData, fields)
+      expect(store.isValid()).toBeTruthy()
+    }
+  )
 })
