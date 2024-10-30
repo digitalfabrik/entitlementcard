8742d911f602c77ee41ac668dbb02d4cd50e9e62
Steffen Kleinle <steffen.kleinle@mailbox.org>
2024-10-30 10:55:20 +0100

    1573: Only rate limit if no entitlement present

--- backend/src/main/kotlin/app/ehrenamtskarte/backend/cards/webservice/schema/CardMutationService.kt
+++ backend/src/main/kotlin/app/ehrenamtskarte/backend/cards/webservice/schema/CardMutationService.kt
@@ -147,7 +147,9 @@ class CardMutationService {
 
         val userEntitlements = transaction { UserEntitlementsRepository.findUserEntitlements(userHash.toByteArray()) }
         if (userEntitlements == null || userEntitlements.revoked || userEntitlements.endDate.isBefore(LocalDate.now())) {
-            logger.info("${context.remoteIp} ${userEntitlements?.userHash} failed to create a new card")
+            if (userEntitlements == null) {
+                logger.info("${context.remoteIp} failed to create a new card")
+            }
             throw InvalidUserEntitlementsException()
         }
 
