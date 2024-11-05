7301b8f43848b5470f64520eefd53f8e47d3a130
seluianova <viktoria.seluianova@tuerantuer.org>
2024-11-05 13:08:08 +0100

    1430: Add extendable field to the card verification result

--- backend/src/main/kotlin/app/ehrenamtskarte/backend/cards/webservice/schema/types/CardVerificationResultModel.kt
+++ backend/src/main/kotlin/app/ehrenamtskarte/backend/cards/webservice/schema/types/CardVerificationResultModel.kt
@@ -4,5 +4,6 @@ import java.time.Instant
 
 data class CardVerificationResultModel(
     val valid: Boolean,
+    val extendable: Boolean = false,
     val verificationTimeStamp: String = Instant.now().toString()
 )
