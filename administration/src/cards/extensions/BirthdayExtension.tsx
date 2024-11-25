35e550b9d4e8e0aaf9d702b0097f4d208345d9ef
Tory <115008338+seluianova@users.noreply.github.com>
2024-11-25 11:13:16 +0100

    1430: Add query params to card self service form (#1749)
    * 1430: Add query params to card self service form

--- administration/src/cards/extensions/BirthdayExtension.tsx
+++ administration/src/cards/extensions/BirthdayExtension.tsx
@@ -62,7 +62,7 @@ const BirthdayExtension: Extension<BirthdayExtensionState> = {
     },
   }),
   isValid: ({ birthday }: BirthdayExtensionState) => {
-    if (birthday === null) {
+    if (!birthday) {
       return false
     }
     const today = PlainDate.fromLocalDate(new Date())
