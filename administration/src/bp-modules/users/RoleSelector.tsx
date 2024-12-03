eefb855648d577e3d00a096ae5ce823d52bf84e3
ztefanie <stefanie.metzger@tuerantuer.org>
2024-12-03 12:46:53 +0100

    1624: Improve readability of role checking

--- administration/src/bp-modules/users/RoleSelector.tsx
+++ administration/src/bp-modules/users/RoleSelector.tsx
@@ -1,6 +1,7 @@
 import { HTMLSelect } from '@blueprintjs/core'
 import React, { ReactElement, useContext } from 'react'
 
+import { WhoAmIContext } from '../../WhoAmIProvider'
 import { Role } from '../../generated/graphql'
 import { ProjectConfigContext } from '../../project-configs/ProjectConfigContext'
 import { roleToText } from './UsersTable'
@@ -15,6 +16,7 @@ const RoleSelector = ({
   hideProjectAdmin: boolean
 }): ReactElement => {
   const config = useContext(ProjectConfigContext)
+  const { role: currentUserRole } = useContext(WhoAmIContext).me!
 
   return (
     <HTMLSelect fill onChange={e => onChange((e.target.value as Role | null) ?? null)} value={role ?? ''} required>
@@ -22,9 +24,9 @@ const RoleSelector = ({
         Auswählen...
       </option>
       {hideProjectAdmin ? null : <option value={Role.ProjectAdmin}>{roleToText(Role.ProjectAdmin)}</option>}
-      {hideProjectAdmin || !config.applicationFeature?.applicationUsableWithApiToken ? null : (
+      {config.applicationFeature?.applicationUsableWithApiToken && currentUserRole === Role.ProjectAdmin ? (
         <option value={Role.ExternalVerifiedApiUser}>{roleToText(Role.ExternalVerifiedApiUser)}</option>
-      )}
+      ) : null}
       <option value={Role.RegionAdmin}>{roleToText(Role.RegionAdmin)}</option>
       <option value={Role.RegionManager}>{roleToText(Role.RegionManager)}</option>
     </HTMLSelect>
