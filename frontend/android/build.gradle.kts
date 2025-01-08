e78246c17747345612edd16799c91b0b909f2547
Michael Markl <marklmichael98@gmail.com>
2025-01-08 17:05:40 +0100

    Migrate Groovy to Kotlin

---
+++ frontend/android/build.gradle.kts
@@ -0,0 +1,20 @@
+import org.gradle.api.tasks.Delete
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.buildDir = file("../build")
+subprojects {
+    project.buildDir = file("${rootProject.buildDir}/${project.name}")
+}
+subprojects {
+    project.evaluationDependsOn(":app")
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.buildDir)
+}
