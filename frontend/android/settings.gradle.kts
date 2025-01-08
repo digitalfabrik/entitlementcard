f26af6fccd1c19541ab6bca5e76f78900f112633
Michael Markl <marklmichael98@gmail.com>
2025-01-08 17:05:40 +0100

    Migrate Groovy to Kotlin

---
+++ frontend/android/settings.gradle.kts
@@ -0,0 +1,22 @@
+pluginManagement {
+    val properties = java.util.Properties()
+    file("local.properties").inputStream().use { properties.load(it) }
+    val flutterSdkPath = properties.getProperty("flutter.sdk")
+    require(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
+
+    includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
+
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+plugins {
+    id("dev.flutter.flutter-plugin-loader") version "1.0.0" apply true
+    id("com.android.application") version "8.8.2" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+}
+
+include("app")
