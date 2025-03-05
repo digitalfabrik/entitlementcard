d4a786a5be389be38cd1fcfeb083dbbe32f0582e
André Wachter <andre.wachter@tuerantuer.org>
2025-03-05 13:48:54 +0100

    Pass additional props to internal component in `<CustomDatePicker>`

--- administration/src/bp-modules/components/CustomDatePicker.tsx
+++ administration/src/bp-modules/components/CustomDatePicker.tsx
@@ -1,22 +1,29 @@
 import { DesktopDatePicker } from '@mui/x-date-pickers'
 import { deDE } from '@mui/x-date-pickers/locales'
-import React, { ReactElement } from 'react'
+import React, { CSSProperties, ReactElement } from 'react'
 
 import useWindowDimensions from '../../hooks/useWindowDimensions'
 
 type CustomDatePickerProps = {
   date: Date | null
+  disabled?: boolean
+  label?: string
   onBlur?: () => void
   onChange?: (date: Date | null) => void
   onClear?: () => void
   isValid: boolean
   minDate?: Date
   maxDate?: Date
-  disableFuture: boolean
+  disableFuture?: boolean
+  disablePast?: boolean
+  textFieldHelperText?: string
+  textFieldStyle?: CSSProperties
 }
 
 const CustomDatePicker = ({
   date,
+  disabled,
+  label,
   onBlur,
   onChange,
   onClear,
@@ -24,6 +31,9 @@ const CustomDatePicker = ({
   minDate,
   maxDate,
   disableFuture,
+  disablePast,
+  textFieldHelperText,
+  textFieldStyle,
 }: CustomDatePickerProps): ReactElement => {
   const { viewportSmall } = useWindowDimensions()
   const formStyle = viewportSmall ? { fontSize: 16, padding: '9px 10px' } : { fontSize: 14, padding: '6px 10px' }
@@ -31,6 +41,8 @@ const CustomDatePicker = ({
     '0 0 0 0 rgba(205, 66, 70, 0), 0 0 0 0 rgba(205, 66, 70, 0), inset 0 0 0 1px #cd4246, inset 0 0 0 1px rgba(17, 20, 24, 0.2), inset 0 1px 1px rgba(17, 20, 24, 0.3)'
   return (
     <DesktopDatePicker
+      disabled={disabled}
+      label={label}
       views={['year', 'month', 'day']}
       value={date}
       format='dd.MM.yyyy'
@@ -39,10 +51,12 @@ const CustomDatePicker = ({
         openPickerIcon: { fontSize: 'small' },
         field: { clearable: true, onClear },
         textField: {
+          helperText: textFieldHelperText,
           placeholder: 'TT.MM.JJJJ',
           error: !isValid,
           spellCheck: false,
           onBlur,
+          style: textFieldStyle,
           sx: {
             width: '100%',
             input: formStyle,
@@ -52,6 +66,7 @@ const CustomDatePicker = ({
       }}
       localeText={deDE.components.MuiLocalizationProvider.defaultProps.localeText}
       disableFuture={disableFuture}
+      disablePast={disablePast}
       minDate={minDate}
       maxDate={maxDate}
       onChange={onChange}
