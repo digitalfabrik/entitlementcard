6f9bddecec3dc23724fa34d35087c85350611d65
Andy <fischer@integreat-app.de>
2025-03-11 15:41:19 +0100

    1899: use getAdministrator

--- backend/src/main/kotlin/app/ehrenamtskarte/backend/freinet/webservice/FreinetAgencyMutationService.kt
+++ backend/src/main/kotlin/app/ehrenamtskarte/backend/freinet/webservice/FreinetAgencyMutationService.kt
@@ -1,12 +1,11 @@
 package app.ehrenamtskarte.backend.freinet.webservice
 
-import app.ehrenamtskarte.backend.auth.database.AdministratorEntity
+import app.ehrenamtskarte.backend.auth.getAdministrator
 import app.ehrenamtskarte.backend.auth.service.Authorizer
 import app.ehrenamtskarte.backend.common.webservice.GraphQLContext
 import app.ehrenamtskarte.backend.exception.service.ForbiddenException
 import app.ehrenamtskarte.backend.exception.service.NotImplementedException
 import app.ehrenamtskarte.backend.exception.service.ProjectNotFoundException
-import app.ehrenamtskarte.backend.exception.service.UnauthorizedException
 import app.ehrenamtskarte.backend.exception.webservice.exceptions.FreinetAgencyNotFoundException
 import app.ehrenamtskarte.backend.freinet.database.repos.FreinetAgencyRepository
 import app.ehrenamtskarte.backend.projects.database.ProjectEntity
@@ -20,15 +19,12 @@ class FreinetAgencyMutationService {
     @GraphQLDescription("Updates the data transfer to freinet. Works only for the EAK project.")
     fun updateDataTransferToFreinet(dfe: DataFetchingEnvironment, regionId: Int, project: String, dataTransferActivated: Boolean): Boolean {
         val context = dfe.getContext<GraphQLContext>()
-        val jwtPayload = context.enforceSignedIn()
+        val admin = context.getAdministrator()
         val projectConfig = dfe.getContext<GraphQLContext>().backendConfiguration.getProjectConfig(project)
         if (projectConfig.freinet == null) {
             throw NotImplementedException()
         }
         transaction {
-            val admin =
-                AdministratorEntity.findById(jwtPayload.adminId)
-                    ?: throw UnauthorizedException()
             ProjectEntity.find { Projects.project eq project }.firstOrNull()
                 ?: throw ProjectNotFoundException(project)
             if (!Authorizer.mayUpdateFreinetAgencyInformationInRegion(admin, regionId)) {
