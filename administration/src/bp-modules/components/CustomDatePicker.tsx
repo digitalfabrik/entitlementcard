0a8e0a4149ab95e5a26c3895938d14fc78a4cb43
André Wachter <andre.wachter@tuerantuer.org>
2025-03-17 14:13:12 +0100

    Do not adjust font size in `<CustomDatePicker>`

--- administration/src/bp-modules/components/CustomDatePicker.tsx
+++ administration/src/bp-modules/components/CustomDatePicker.tsx
@@ -4,8 +4,6 @@ import type { DesktopDatePickerSlotProps } from '@mui/x-date-pickers/DesktopDate
 import { deDE } from '@mui/x-date-pickers/locales'
 import React, { ReactElement } from 'react'
 
-import useWindowDimensions from '../../hooks/useWindowDimensions'
-
 type CustomDatePickerProps = {
   value?: Date | null
   disabled?: boolean
@@ -36,60 +34,55 @@ const CustomDatePicker = ({
   disablePast,
   textFieldHelperText,
   textFieldSlotProps,
-}: CustomDatePickerProps): ReactElement => {
-  const { viewportSmall } = useWindowDimensions()
-
-  return (
-    <DesktopDatePicker
-      disabled={disabled}
-      label={label}
-      views={['year', 'month', 'day']}
-      openTo='year'
-      value={value}
-      format='dd.MM.yyyy'
-      sx={{ '& input[value=""]:not(:focus)': { color: 'transparent' } }}
-      slotProps={{
-        openPickerIcon: { fontSize: 'small' },
-        field: {
-          clearable: true,
-          onClear,
-        },
-        textField: deepmerge(
-          {
-            helperText: textFieldHelperText,
-            placeholder: 'TT.MM.JJJJ',
-            error: !isValid,
-            spellCheck: false,
-            onBlur,
-            size: 'small',
-            sx: {
-              width: '100%',
-              boxShadow: !isValid
-                ? `
+}: CustomDatePickerProps): ReactElement => (
+  <DesktopDatePicker
+    disabled={disabled}
+    label={label}
+    views={['year', 'month', 'day']}
+    openTo='year'
+    value={value}
+    format='dd.MM.yyyy'
+    sx={{ '& input[value=""]:not(:focus)': { color: 'transparent' } }}
+    slotProps={{
+      openPickerIcon: { fontSize: 'small' },
+      field: {
+        clearable: true,
+        onClear,
+      },
+      textField: deepmerge(
+        {
+          helperText: textFieldHelperText,
+          placeholder: 'TT.MM.JJJJ',
+          error: !isValid,
+          spellCheck: false,
+          onBlur,
+          size: 'small',
+          sx: {
+            width: '100%',
+            boxShadow: !isValid
+              ? `
                 0 0 0 0 rgba(205, 66, 70, 0),
                 inset 0 0 0 1px #cd4246,
                 inset 0 0 0 1px rgba(17, 20, 24, 0.2),
                 inset 0 1px 1px rgba(17, 20, 24, 0.3),
               `
-                : undefined,
-              '.MuiPickersInputBase-root': {
-                fontSize: viewportSmall ? '16px' : '14px',
-                borderRadius: '2px',
-              },
+              : undefined,
+            '.MuiPickersInputBase-root': {
+              borderRadius: '2px',
             },
           },
-          textFieldSlotProps
-        ),
-      }}
-      enableAccessibleFieldDOMStructure
-      localeText={deDE.components.MuiLocalizationProvider.defaultProps.localeText}
-      disableFuture={disableFuture}
-      disablePast={disablePast}
-      minDate={minDate}
-      maxDate={maxDate}
-      onChange={onChange}
-    />
-  )
-}
+        },
+        textFieldSlotProps
+      ),
+    }}
+    enableAccessibleFieldDOMStructure
+    localeText={deDE.components.MuiLocalizationProvider.defaultProps.localeText}
+    disableFuture={disableFuture}
+    disablePast={disablePast}
+    minDate={minDate}
+    maxDate={maxDate}
+    onChange={onChange}
+  />
+)
 
 export default CustomDatePicker
