94db5df742ee1214af79df95f28c89a50e08a845
André Wachter <andre.wachter@tuerantuer.org>
2025-03-18 21:45:10 +0100

    Add Gradle plugin `io.gitlab.arturbosch.detekt`

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -12,6 +12,7 @@ val exposedVersion: String by project
 plugins {
     id("com.google.protobuf") version "0.9.4"
     id("com.expediagroup.graphql") version "8.3.0"
+    id("io.gitlab.arturbosch.detekt") version "1.23.8"
     // Apply the Kotlin JVM plugin to add support for Kotlin.
     id("org.jetbrains.kotlin.jvm") version "1.9.10"
     id("org.jetbrains.kotlinx.kover") version "0.8.3"
@@ -87,6 +88,14 @@ dependencies {
     testImplementation("org.testcontainers:postgresql")
     // Replace the library version used in testcontainers to avoid vulnerability
     testImplementation("org.apache.commons:commons-compress:1.26.2")
+}
+
+detekt {
+    // https://detekt.dev/docs/gettingstarted/gradle
+    toolVersion = "1.23.8"
+    config.setFrom(file("../detekt.yml"))
+    buildUponDefaultConfig = true
+    basePath = project.layout.projectDirectory.toString()
 }
 
 ktlint {
