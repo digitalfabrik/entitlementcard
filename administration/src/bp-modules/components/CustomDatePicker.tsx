6643d1517fb20e93c48607e21ce50e2a8b8cf4d8
André Wachter <andre.wachter@tuerantuer.org>
2025-03-31 14:31:04 +0200

    Use `enableAccessibleFieldDOMStructure` for mobile UAs only

--- administration/src/bp-modules/components/CustomDatePicker.tsx
+++ administration/src/bp-modules/components/CustomDatePicker.tsx
@@ -1,3 +1,4 @@
+import { useMediaQuery } from '@mui/system'
 import { deepmerge } from '@mui/utils'
 import { DesktopDatePicker } from '@mui/x-date-pickers'
 import type { DesktopDatePickerSlotProps } from '@mui/x-date-pickers/DesktopDatePicker/DesktopDatePicker.types'
@@ -34,55 +35,64 @@ const CustomDatePicker = ({
   disablePast,
   textFieldHelperText,
   textFieldSlotProps,
-}: CustomDatePickerProps): ReactElement => (
-  <DesktopDatePicker
-    disabled={disabled}
-    label={label}
-    views={['year', 'month', 'day']}
-    openTo='year'
-    value={value}
-    format='dd.MM.yyyy'
-    sx={{ '& input[value=""]:not(:focus)': { color: 'transparent' } }}
-    slotProps={{
-      openPickerIcon: { fontSize: 'small' },
-      field: {
-        clearable: true,
-        onClear,
-      },
-      textField: deepmerge(
-        {
-          helperText: textFieldHelperText,
-          placeholder: 'TT.MM.JJJJ',
-          error: !isValid,
-          spellCheck: false,
-          onBlur,
-          size: 'small',
-          sx: {
-            width: '100%',
-            boxShadow: !isValid
-              ? `
-                0 0 0 0 rgba(205, 66, 70, 0),
-                inset 0 0 0 1px #cd4246,
-                inset 0 0 0 1px rgba(17, 20, 24, 0.2),
-                inset 0 1px 1px rgba(17, 20, 24, 0.3),
-              `
-              : undefined,
-            '.MuiPickersInputBase-root': {
-              borderRadius: '2px',
+}: CustomDatePickerProps): ReactElement => {
+  const isMobile = !useMediaQuery('(pointer: fine)')
+
+  return (
+    <DesktopDatePicker
+      disabled={disabled}
+      label={label}
+      views={['year', 'month', 'day']}
+      openTo='year'
+      value={value}
+      format='dd.MM.yyyy'
+      sx={{ '& input[value=""]:not(:focus)': { color: 'transparent' } }}
+      slotProps={{
+        openPickerIcon: { fontSize: 'small' },
+        field: {
+          clearable: true,
+          onClear,
+        },
+        textField: deepmerge(
+          {
+            helperText: textFieldHelperText,
+            placeholder: 'TT.MM.JJJJ',
+            error: !isValid,
+            spellCheck: false,
+            onBlur,
+            size: 'small',
+            sx: {
+              width: '100%',
+              boxShadow: !isValid
+                ? `
+                  0 0 0 0 rgba(205, 66, 70, 0),
+                  inset 0 0 0 1px #cd4246,
+                  inset 0 0 0 1px rgba(17, 20, 24, 0.2),
+                  inset 0 1px 1px rgba(17, 20, 24, 0.3),
+                `
+                : undefined,
+              '.MuiPickersInputBase-root': {
+                borderRadius: '2px',
+              },
             },
           },
-        },
-        textFieldSlotProps
-      ),
-    }}
-    enableAccessibleFieldDOMStructure
-    localeText={deDE.components.MuiLocalizationProvider.defaultProps.localeText}
-    disableFuture={disableFuture}
-    disablePast={disablePast}
-    minDate={minDate}
-    maxDate={maxDate}
-    onChange={onChange}
-  />
-)
+          textFieldSlotProps
+        ),
+      }}
+      // We use this property, since it (sort of) fixes some issues for Firefox Mobile (keyboard
+      // does not show digits only). On the other hand, it introduces a bug with keyboard
+      // navigation (inability to navigate out of component), so we switch on a media query here,
+      // to provide a satisfying result.
+      // TODO This property will become be the default and by then, hopefully all bugs will be fixed
+      enableAccessibleFieldDOMStructure={isMobile}
+      localeText={deDE.components.MuiLocalizationProvider.defaultProps.localeText}
+      disableFuture={disableFuture}
+      disablePast={disablePast}
+      minDate={minDate}
+      maxDate={maxDate}
+      onChange={onChange}
+    />
+  )
+}
 
 export default CustomDatePicker
