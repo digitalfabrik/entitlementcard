5b3733476c504f08cb4946ab78727d8a570a766f
André Wachter <andre.wachter@tuerantuer.org>
2025-04-01 11:14:41 +0200

    2165: Refactor class `Database` to an object

--- backend/src/main/kotlin/app/ehrenamtskarte/backend/common/database/Database.kt
+++ backend/src/main/kotlin/app/ehrenamtskarte/backend/common/database/Database.kt
@@ -19,67 +19,65 @@ import org.jetbrains.exposed.sql.transactions.transaction
 import java.io.BufferedReader
 import java.io.InputStreamReader
 
-class Database {
-    companion object {
-        fun executeSqlResource(path: String) {
-            val resource = Database::class.java.classLoader.getResource(path)
-                ?: throw Exception("Failed to find script")
+object Database {
+    fun executeSqlResource(path: String) {
+        val resource = Database::class.java.classLoader.getResource(path)
+            ?: throw Exception("Failed to find script")
 
-            with(TransactionManager.current()) {
-                exec(resource.openStream().use { BufferedReader(InputStreamReader(it)).readText() })
-            }
+        with(TransactionManager.current()) {
+            exec(resource.openStream().use { BufferedReader(InputStreamReader(it)).readText() })
         }
-
-        fun createAccount(project: String, email: String, password: String, roleDbValue: String) {
-            val role = Role.fromDbValue(roleDbValue)
-            transaction {
-                val testRegionId = if (role in setOf(
-                        Role.REGION_MANAGER,
-                        Role.REGION_ADMIN,
-                    )
-                ) {
-                    RegionsRepository.findAllInProject(project).first().id.value
-                } else {
-                    null
-                }
-                AdministratorsRepository.insert(project, email, password, role, testRegionId)
-            }
-        }
-
-        fun setupWithInitialDataAndMigrationChecks(config: BackendConfiguration): org.jetbrains.exposed.sql.Database =
-            setupWithoutMigrationCheck(config).apply {
-                val agencies = FreinetAgenciesLoader().loadAgenciesFromXml(config.projects)
-                transaction {
-                    assertDatabaseIsInSync()
-                    insertOrUpdateProjects(config)
-                    insertOrUpdateRegions(agencies, config)
-                    insertOrUpdateCategories()
-                    createOrReplaceStoreFunctions()
-                }
-            }
-
-        fun setupWithoutMigrationCheck(
-            config: BackendConfiguration,
-            log: Boolean = true,
-        ): org.jetbrains.exposed.sql.Database =
-            connect(
-                config.postgres.url,
-                driver = "org.postgresql.Driver",
-                user = config.postgres.user,
-                password = config.postgres.password,
-                setupConnection = {
-                    // Set session time zone to UTC, to make timestamps work properly in every configuration.
-                    // Note(michael-markl): I believe this is postgres specific syntax.
-                    it.prepareStatement("SET TIME ZONE 'UTC';").executeUpdate()
-                },
-                databaseConfig = DatabaseConfig.invoke {
-                    // Nested transactions are helpful for applying migrations in subtransactions.
-                    useNestedTransactions = true
-
-                    if (config.environment == Environment.DEVELOPMENT && log) {
-                        this.sqlLogger = StdOutSqlLogger
-                    }
-                },
-            )
     }
+
+    fun createAccount(project: String, email: String, password: String, roleDbValue: String) {
+        val role = Role.fromDbValue(roleDbValue)
+        transaction {
+            val testRegionId = if (role in setOf(
+                    Role.REGION_MANAGER,
+                    Role.REGION_ADMIN,
+                )
+            ) {
+                RegionsRepository.findAllInProject(project).first().id.value
+            } else {
+                null
+            }
+            AdministratorsRepository.insert(project, email, password, role, testRegionId)
+        }
+    }
+
+    fun setupWithInitialDataAndMigrationChecks(config: BackendConfiguration): org.jetbrains.exposed.sql.Database =
+        setupWithoutMigrationCheck(config).apply {
+            val agencies = FreinetAgenciesLoader().loadAgenciesFromXml(config.projects)
+            transaction {
+                assertDatabaseIsInSync()
+                insertOrUpdateProjects(config)
+                insertOrUpdateRegions(agencies, config)
+                insertOrUpdateCategories()
+                createOrReplaceStoreFunctions()
+            }
+        }
+
+    fun setupWithoutMigrationCheck(
+        config: BackendConfiguration,
+        log: Boolean = true,
+    ): org.jetbrains.exposed.sql.Database =
+        connect(
+            config.postgres.url,
+            driver = "org.postgresql.Driver",
+            user = config.postgres.user,
+            password = config.postgres.password,
+            setupConnection = {
+                // Set session time zone to UTC, to make timestamps work properly in every configuration.
+                // Note(michael-markl): I believe this is postgres specific syntax.
+                it.prepareStatement("SET TIME ZONE 'UTC';").executeUpdate()
+            },
+            databaseConfig = DatabaseConfig.invoke {
+                // Nested transactions are helpful for applying migrations in subtransactions.
+                useNestedTransactions = true
+
+                if (config.environment != Environment.PRODUCTION && log) {
+                    this.sqlLogger = StdOutSqlLogger
+                }
+            },
+        )
 }
