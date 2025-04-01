854d780837bb234b922aa30ba5fd711d351f63f5
André Wachter <andre.wachter@tuerantuer.org>
2025-04-01 15:23:55 +0200

    Add `import-dev` command

--- backend/src/main/kotlin/app/ehrenamtskarte/backend/EntryPoint.kt
+++ backend/src/main/kotlin/app/ehrenamtskarte/backend/EntryPoint.kt
@@ -24,6 +24,7 @@ import org.slf4j.LoggerFactory
 import java.io.File
 import java.nio.file.Path
 import java.nio.file.Paths
+import java.sql.SQLException
 import java.util.TimeZone
 import kotlin.io.path.exists
 
@@ -142,6 +143,41 @@ class Import : CliktCommand(
     }
 }
 
+class ImportDev : CliktCommand(
+    name = "import-dev",
+    help = "Import dummy developer data.",
+) {
+    private val directoryPath by argument(
+        name = "directory",
+        help = "Directory to load the SQL files from.",
+    )
+    private val config by requireObject<BackendConfiguration>()
+    private val sqlFilePattern = Regex("""\.sql$""")
+
+    override fun run() {
+        val dir = File(directoryPath).absoluteFile
+
+        if (dir.exists() && dir.isDirectory) {
+            Database.setupWithoutMigrationCheck(config, log = false)
+
+            dir.walk().forEach {
+                if (it.isFile && sqlFilePattern.containsMatchIn(it.path)) {
+                    logger.info("Loading SQL file: ${it.absolutePath.substring(dir.absolutePath.length + 1)}")
+                    try {
+                        Database.executeSqlFile(it)
+                    } catch (err: SQLException) {
+                        logger.error("Error in SQL file (code = ${err.errorCode}):\n${err.message}")
+                        throw ProgramResult(statusCode = 4)
+                    }
+                }
+            }
+        } else {
+            logger.error("Path '$dir' does not exist or is not a directory.")
+            throw ProgramResult(statusCode = 4)
+        }
+    }
+}
+
 class CreateAdmin : CliktCommand(
     name = "create-admin",
     help = "Creates an admin account with the specified email and password",
@@ -216,6 +252,7 @@ fun main(args: Array<String>) {
         Execute(),
         Import(),
         ImportSingle(),
+        ImportDev(),
         Migrate(),
         MigrateSkipBaseline(),
         CreateAdmin(),
