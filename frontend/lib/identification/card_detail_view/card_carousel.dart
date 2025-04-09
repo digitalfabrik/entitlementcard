80bd785c9dbc7f86a722710c4a82ef9dc098a5e4
Andy <fischer@integreat-app.de>
2025-04-09 18:14:08 +0200

    1802: set a proper height for the container and remove padding

--- frontend/lib/identification/card_detail_view/card_carousel.dart
+++ frontend/lib/identification/card_detail_view/card_carousel.dart
@@ -22,7 +22,7 @@ class CardCarouselState extends State<CardCarousel> {
   @override
   Widget build(BuildContext context) {
     final int cardAmount = widget.cards.length;
-    final double indicatorHeight = cardAmount > 1 ? 16 : 0;
+    final double indicatorHeight = cardAmount > 1 ? 32 : 0;
 
     return LayoutBuilder(builder: (context, constraints) {
       return Column(
@@ -45,27 +45,24 @@ class CardCarouselState extends State<CardCarousel> {
             ),
           ),
           if (cardAmount > 1)
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 12),
-              child: Container(
-                height: indicatorHeight,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: widget.cards.asMap().entries.map((entry) {
-                    return GestureDetector(
-                      onTap: () => widget.carouselController.animateToPage(entry.key),
-                      child: Container(
-                        width: 8.0,
-                        height: 8.0,
-                        margin: EdgeInsets.symmetric(horizontal: 4.0),
-                        decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            color: (Theme.of(context).brightness == Brightness.dark ? Colors.white : Colors.black)
-                                .withOpacity(widget.cardIndex == entry.key ? 0.9 : 0.4)),
-                      ),
-                    );
-                  }).toList(),
-                ),
+            Container(
+              height: indicatorHeight,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: widget.cards.asMap().entries.map((entry) {
+                  return GestureDetector(
+                    onTap: () => widget.carouselController.animateToPage(entry.key),
+                    child: Container(
+                      width: 8.0,
+                      height: 8.0,
+                      margin: EdgeInsets.symmetric(horizontal: 4.0),
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: (Theme.of(context).brightness == Brightness.dark ? Colors.white : Colors.black)
+                              .withOpacity(widget.cardIndex == entry.key ? 0.9 : 0.4)),
+                    ),
+                  );
+                }).toList(),
               ),
             ),
         ],
