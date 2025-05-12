cadd22d70707bf4325b9d255375e3708f8601321
André Wachter <andre.wachter@tuerantuer.org>
2025-05-12 22:53:55 +0200

    1981.1: Use MUI accordion components

--- administration/src/bp-modules/applications/ApplicationCard.tsx
+++ administration/src/bp-modules/applications/ApplicationCard.tsx
@@ -1,8 +1,10 @@
 /* eslint-disable react/destructuring-assignment */
 import { MutationResult } from '@apollo/client'
-import { Colors, H4, Section } from '@blueprintjs/core'
-import { CreditScore, Delete, EditNote, Print, Warning } from '@mui/icons-material'
+import { ArrowDropDown, CreditScore, Delete, EditNote, Print, Warning } from '@mui/icons-material'
 import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
   Box,
   Button,
   CircularProgress,
@@ -12,6 +14,7 @@ import {
   Divider,
   Stack,
   Tooltip,
+  Typography,
   useTheme,
 } from '@mui/material'
 import React, { memo, useContext, useMemo, useState } from 'react'
@@ -34,20 +37,6 @@ import VerificationsQuickIndicator from './VerificationsQuickIndicator'
 import VerificationsView from './VerificationsView'
 import { printAwareCss } from './constants'
 
-const ApplicationViewCard = styled(Section)<{ $hideInPrintMode?: boolean }>`
-  width: 1000px;
-  max-width: 90%;
-  overflow: hidden;
-  margin: 10px;
-  position: relative;
-  @media print {
-    width: 100%;
-    height: auto;
-    box-shadow: none;
-  }
-  ${props => props.$hideInPrintMode && printAwareCss};
-`
-
 const PrintAwarePrimaryButton = styled(Button)`
   margin-right: 10px;
   ${printAwareCss};
@@ -60,15 +49,6 @@ const PrintAwarePrimaryButton = styled(Button)`
 const PrintAwareButton = styled(Button)`
   margin-right: 10px;
   ${printAwareCss};
-`
-
-const Title = styled(H4)`
-  margin: 0;
-  display: inline-block;
-`
-
-const CardContentHint = styled(Title)`
-  color: ${Colors.GRAY1};
 `
 
 const Spacer = styled.div`
@@ -145,7 +125,6 @@ const ApplicationCard = ({
   isSelectedForPrint,
   onChange,
 }: ApplicationCardProps) => {
-  const [isExpanded, setIsExpanded] = useState(false)
   const { t } = useTranslation('applicationsOverview')
   const theme = useTheme()
   const { createdDate: createdDateString, id, withdrawalDate, cardCreated } = application
@@ -182,102 +161,106 @@ const ApplicationCard = ({
   )
 
   return (
-    <ApplicationViewCard
-      title={
-        <div>
-          <Title>
-            {t('applicationFrom')} {formatDateWithTimezone(createdDateString, config.timezone)}&emsp;
-          </Title>{' '}
-          {personalData && personalData.forenames !== undefined && personalData.surname !== undefined && (
-            <CardContentHint>
-              {t('name')}: {personalData.surname}, {personalData.forenames}
-            </CardContentHint>
-          )}
-        </div>
-      }
-      rightElement={<ApplicationIndicators application={application} applicationJsonData={jsonValueParsed} />}
-      elevation={1}
-      icon={withdrawalDate ? <Warning color='warning' /> : undefined}
-      collapseProps={{ isOpen: isExpanded, onToggle: () => setIsExpanded(!isExpanded), keepChildrenMounted: true }}
-      collapsible={!isSelectedForPrint}
-      $hideInPrintMode={!isSelectedForPrint}>
-      <Stack sx={{ p: 2 }} gap={2}>
-        <Stack direction='row' spacing={2} alignItems='flex-start'>
-          {withdrawalDate ? (
-            <Box sx={{ bgcolor: theme.palette.warning.light, p: 2, flexGrow: 1 }}>
-              {t('withdrawalMessage', { withdrawalDate: formatDateWithTimezone(withdrawalDate, config.timezone) })}
-              <br />
-              {t('deleteApplicationSoonPrompt')}
-            </Box>
-          ) : (
-            <Spacer />
-          )}
-          <NoteDialogController
-            application={application}
-            isOpen={openNoteDialog}
-            onOpenNoteDialog={setOpenNoteDialog}
-            onChange={onChange}
-          />
+    <Accordion
+      sx={{
+        displayPrint: isSelectedForPrint ? 'block' : 'none',
+      }}
+      aria-controls='panel-content'>
+      <AccordionSummary expandIcon={<ArrowDropDown />}>
+        <Stack direction='row' spacing={2} width='100%'>
+          {withdrawalDate ? <Warning color='warning' /> : undefined}
+          <Typography variant='h4' sx={{ flexGrow: 1 }}>
+            {t('applicationFrom')} {formatDateWithTimezone(createdDateString, config.timezone)}
+            {personalData && personalData.forenames !== undefined && personalData.surname !== undefined && (
+              <Box component='span' sx={{ color: theme.palette.text.secondary }}>
+                &emsp;&emsp;&emsp;&emsp;
+                {t('name')}: {personalData.surname}, {personalData.forenames}
+              </Box>
+            )}
+          </Typography>
+          <ApplicationIndicators application={application} applicationJsonData={jsonValueParsed} />
+        </Stack>
+      </AccordionSummary>
+
+      <AccordionDetails>
+        <Stack sx={{ p: 2 }} gap={2}>
+          <Stack direction='row' spacing={2} alignItems='flex-start'>
+            {withdrawalDate ? (
+              <Box sx={{ bgcolor: theme.palette.warning.light, p: 2, flexGrow: 1 }}>
+                {t('withdrawalMessage', { withdrawalDate: formatDateWithTimezone(withdrawalDate, config.timezone) })}
+                <br />
+                {t('deleteApplicationSoonPrompt')}
+              </Box>
+            ) : (
+              <Spacer />
+            )}
+            <NoteDialogController
+              application={application}
+              isOpen={openNoteDialog}
+              onOpenNoteDialog={setOpenNoteDialog}
+              onChange={onChange}
+            />
+          </Stack>
+
+          {/* TODO: <JsonFieldView> does not emit a root element and thus, <Stack> would insert a gap here */}
+          <div>
+            <JsonFieldView
+              jsonField={jsonValueParsed}
+              baseUrl={baseUrl}
+              key={0}
+              hierarchyIndex={0}
+              attachmentAccessible
+              expandedRoot={false}
+            />
+          </div>
         </Stack>
 
-        {/* TODO: <JsonFieldView> does not emit a root element and thus, <Stack> would insert a gap here */}
-        <div>
-          <JsonFieldView
-            jsonField={jsonValueParsed}
-            baseUrl={baseUrl}
-            key={0}
-            hierarchyIndex={0}
-            attachmentAccessible
-            expandedRoot={false}
-          />
-        </div>
-      </Stack>
+        <Divider />
 
-      <Divider />
+        <Box sx={{ p: 2 }}>
+          <VerificationsView verifications={application.verifications} />
+        </Box>
 
-      <Box sx={{ p: 2 }}>
-        <VerificationsView verifications={application.verifications} />
-      </Box>
+        <Divider />
 
-      <Divider />
+        <Stack sx={{ p: 2 }} spacing={2} direction='row'>
+          <Tooltip title={createCardQuery ? undefined : t('incompleteMappingTooltip')}>
+            {/* Make the outer Tooltip independent of the button's disabled state */}
+            <span>
+              <PrintAwarePrimaryButton
+                disabled={!createCardQuery}
+                variant='contained'
+                color='primary'
+                href={createCardQuery ? `./cards/add${createCardQuery}` : undefined}
+                startIcon={<CreditScore />}>
+                {cardCreated ? t('createCardAgain') : t('createCard')}
+              </PrintAwarePrimaryButton>
+            </span>
+          </Tooltip>
+          <PrintAwareButton
+            onClick={() => setDeleteDialogOpen(true)}
+            startIcon={<Delete />}
+            variant='outlined'
+            color='error'>
+            {t('deleteApplication')}
+          </PrintAwareButton>
+          <PrintAwareButton
+            onClick={() => printApplicationById(id)}
+            startIcon={<Print />}
+            variant='outlined'
+            color='inherit'>
+            {t('exportPdf')}
+          </PrintAwareButton>
+        </Stack>
 
-      <Stack sx={{ p: 2 }} spacing={2} direction='row'>
-        <Tooltip title={createCardQuery ? undefined : t('incompleteMappingTooltip')}>
-          {/* Make the outer Tooltip independent of the button's disabled state */}
-          <span>
-            <PrintAwarePrimaryButton
-              disabled={!createCardQuery}
-              variant='contained'
-              color='primary'
-              href={createCardQuery ? `./cards/add${createCardQuery}` : undefined}
-              startIcon={<CreditScore />}>
-              {cardCreated ? t('createCardAgain') : t('createCard')}
-            </PrintAwarePrimaryButton>
-          </span>
-        </Tooltip>
-        <PrintAwareButton
-          onClick={() => setDeleteDialogOpen(true)}
-          startIcon={<Delete />}
-          variant='outlined'
-          color='error'>
-          {t('deleteApplication')}
-        </PrintAwareButton>
-        <PrintAwareButton
-          onClick={() => printApplicationById(id)}
-          startIcon={<Print />}
-          variant='outlined'
-          color='inherit'>
-          {t('exportPdf')}
-        </PrintAwareButton>
-      </Stack>
-
-      <DeleteDialog
-        isOpen={deleteDialogOpen}
-        deleteResult={deleteResult}
-        onConfirm={() => deleteApplication({ variables: { applicationId: application.id } })}
-        onCancel={() => setDeleteDialogOpen(false)}
-      />
-    </ApplicationViewCard>
+        <DeleteDialog
+          isOpen={deleteDialogOpen}
+          deleteResult={deleteResult}
+          onConfirm={() => deleteApplication({ variables: { applicationId: application.id } })}
+          onCancel={() => setDeleteDialogOpen(false)}
+        />
+      </AccordionDetails>
+    </Accordion>
   )
 }
 
