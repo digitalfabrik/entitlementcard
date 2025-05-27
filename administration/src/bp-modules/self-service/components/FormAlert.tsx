173bdfdb0c5c2bf564093f44a3a55210aebbc107
Andy <fischer@integreat-app.de>
2025-05-27 14:46:42 +0200

    2211: adjust text in translation, add pre-line in FormAlert to respect new lines in texts

--- administration/src/bp-modules/self-service/components/FormAlert.tsx
+++ administration/src/bp-modules/self-service/components/FormAlert.tsx
@@ -14,6 +14,7 @@ const Container = styled('div')<{ $severity: 'info' | 'error'; $isToast: boolean
   gap: 8px;
   align-items: center;
   font-size: 14px;
+  white-space: pre-line;
 `
 
 type FormAlertProps = {
