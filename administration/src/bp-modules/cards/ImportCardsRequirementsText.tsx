97340b8239e439260f249bc3b006957bd1644855
bahaa <bahaa.tuffaha@yahoo.com>
2025-06-23 19:06:01 +0300

    1738:  Checked for isMandatory for each extension and added an asterisk

--- administration/src/bp-modules/cards/ImportCardsRequirementsText.tsx
+++ administration/src/bp-modules/cards/ImportCardsRequirementsText.tsx
@@ -1,7 +1,8 @@
-import React, { ReactElement } from 'react'
+import React, { ReactElement, useContext, useMemo } from 'react'
 import { useTranslation } from 'react-i18next'
 import styled from 'styled-components'
 
+import { ProjectConfigContext } from '../../project-configs/ProjectConfigContext'
 import { ENTRY_LIMIT, FILE_SIZE_LIMIT_MEGA_BYTES } from './constants'
 
 const RequirementsList = styled.ul`
@@ -18,7 +19,23 @@ const ImportCardsRequirementsText = ({
   csvHeaders,
   isFreinetFormat = false,
 }: ImportCardsRequirementsProps): ReactElement => {
+  const projectConfig = useContext(ProjectConfigContext)
+  const { card: cardConfig } = projectConfig
   const { t } = useTranslation('cards')
+
+  const requiredHeaders = useMemo(() => {
+    const headers = [cardConfig.nameColumnName, cardConfig.expiryColumnName]
+    cardConfig.extensions.forEach((extension, index) => {
+      const header = cardConfig.extensionColumnNames[index]
+      if (extension.isMandatory && header) {
+        headers.push(header)
+      }
+    })
+    return headers
+  }, [cardConfig])
+
+  const decoratedHeaders = csvHeaders.map(header => (requiredHeaders.includes(header) ? `${header}*` : header))
+
   return (
     <RequirementsList>
       <li>{t('maxFileSize', { maxFileSize: FILE_SIZE_LIMIT_MEGA_BYTES })}</li>
@@ -26,7 +43,7 @@ const ImportCardsRequirementsText = ({
       <li>
         {t('maxNumberOfEntries')}: {ENTRY_LIMIT}
       </li>
-      <li>{isFreinetFormat ? t('minColumnsForFreinet') : `${t('columnFormat')}:  ${csvHeaders.join(', ')}`}</li>
+      <li>{isFreinetFormat ? t('minColumnsForFreinet') : `${t('columnFormat')}:  ${decoratedHeaders.join(', ')}`}</li>
       <li>{t('dateFormatHint')}</li>
     </RequirementsList>
   )
--- administration/src/bp-modules/stores/StoresRequirementsText.tsx
+++ administration/src/bp-modules/stores/StoresRequirementsText.tsx
@@ -1,7 +1,8 @@
-import React, { ReactElement } from 'react'
+import React, { ReactElement, useMemo } from 'react'
 import { useTranslation } from 'react-i18next'
 import styled from 'styled-components'
 
+import { StoresFieldConfig } from '../../project-configs/getProjectConfig'
 import { FILE_SIZE_LIMIT_MEGA_BYTES } from './constants'
 
 const RequirementsList = styled.ul`
@@ -10,17 +11,18 @@ const RequirementsList = styled.ul`
 `
 
 type ImportCardsRequirementsProps = {
-  header: string[]
+  header: StoresFieldConfig[]
 }
 
 const StoresRequirementsText = ({ header }: ImportCardsRequirementsProps): ReactElement => {
+  const headers = useMemo(() => header.map(field => (field.isMandatory ? `${field.name}*` : `${field.name}`)), [header])
   const { t } = useTranslation('stores')
   return (
     <RequirementsList>
       <li>{t('maxFileSize', { maxFileSize: FILE_SIZE_LIMIT_MEGA_BYTES })} </li>
       <li>{t('fileFormat')}</li>
       <li>
-        {t('neededColumns')} {header.join(', ')}
+        {t('neededColumns')} {headers.join(', ')}
       </li>
     </RequirementsList>
   )
