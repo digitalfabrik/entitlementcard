649779a978873847bdf567ac3ac7c5f3bc622d2d
bahaa <bahaa.tuffaha@yahoo.com>
2025-06-25 19:26:18 +0300

    1738: Added test for each project at ImportCardsRequirementsText test file

--- administration/src/bp-modules/cards/ImportCardsRequirementsText.test.tsx
+++ administration/src/bp-modules/cards/ImportCardsRequirementsText.test.tsx
@@ -1,16 +1,59 @@
 import React from 'react'
 
+import bayernConfig from '../../project-configs/bayern/config'
+import { getCsvHeaders } from '../../project-configs/helper'
+import koblenzConfig from '../../project-configs/koblenz/config'
+import nuernbergConfig from '../../project-configs/nuernberg/config'
+import showcaseConfig from '../../project-configs/showcase/config'
 import { renderWithTranslation } from '../../testing/render'
-import ImportCardsRequirementsText from './ImportCardsRequirementsText'
+import ImportCardsRequirementsText, { getRequiredHeaders } from './ImportCardsRequirementsText'
 
 describe('ImportCardsRequirementsText', () => {
-  const csvHeaders = ['Name', 'Ablaufdatum', 'Kartentyp', 'Address']
-
-  it('should show mandatory requirements with asterisks', () => {
+  it('should show mandatory requirements with asterisks at Showcase', () => {
+    const projectConfig = showcaseConfig
+    const csvHeaders = getCsvHeaders(projectConfig)
     const { getByText } = renderWithTranslation(
-      <ImportCardsRequirementsText csvHeaders={csvHeaders} isFreinetFormat={false} />
+      <ImportCardsRequirementsText csvHeaders={csvHeaders} isFreinetFormat={false} />,
+      { projectConfig }
     )
+    const requiredHeaders = getRequiredHeaders(projectConfig)
+    const decoratedHeaders = csvHeaders.map(header => (requiredHeaders.includes(header) ? `${header}*` : header))
+    expect(getByText(`Spaltenformat: ${decoratedHeaders.join(', ')}`)).toBeTruthy()
+  })
 
-    expect(getByText('Spaltenformat: Name*, Ablaufdatum*, Kartentyp*, Address')).toBeTruthy()
+  it('should show mandatory requirements with asterisks at Bayern', () => {
+    const projectConfig = bayernConfig
+    const csvHeaders = getCsvHeaders(projectConfig)
+    const { getByText } = renderWithTranslation(
+      <ImportCardsRequirementsText csvHeaders={csvHeaders} isFreinetFormat={false} />,
+      { projectConfig }
+    )
+    const requiredHeaders = getRequiredHeaders(projectConfig)
+    const decoratedHeaders = csvHeaders.map(header => (requiredHeaders.includes(header) ? `${header}*` : header))
+    expect(getByText(`Spaltenformat: ${decoratedHeaders.join(', ')}`)).toBeTruthy()
+  })
+
+  it('should show mandatory requirements with asterisks at Nuernberg', () => {
+    const projectConfig = nuernbergConfig
+    const csvHeaders = getCsvHeaders(projectConfig)
+    const { getByText } = renderWithTranslation(
+      <ImportCardsRequirementsText csvHeaders={csvHeaders} isFreinetFormat={false} />,
+      { projectConfig }
+    )
+    const requiredHeaders = getRequiredHeaders(projectConfig)
+    const decoratedHeaders = csvHeaders.map(header => (requiredHeaders.includes(header) ? `${header}*` : header))
+    expect(getByText(`Spaltenformat: ${decoratedHeaders.join(', ')}`)).toBeTruthy()
+  })
+
+  it('should show mandatory requirements with asterisks at Koblenz', () => {
+    const projectConfig = koblenzConfig
+    const csvHeaders = getCsvHeaders(projectConfig)
+    const { getByText } = renderWithTranslation(
+      <ImportCardsRequirementsText csvHeaders={csvHeaders} isFreinetFormat={false} />,
+      { projectConfig }
+    )
+    const requiredHeaders = getRequiredHeaders(projectConfig)
+    const decoratedHeaders = csvHeaders.map(header => (requiredHeaders.includes(header) ? `${header}*` : header))
+    expect(getByText(`Spaltenformat: ${decoratedHeaders.join(', ')}`)).toBeTruthy()
   })
 })
--- administration/src/bp-modules/cards/ImportCardsRequirementsText.tsx
+++ administration/src/bp-modules/cards/ImportCardsRequirementsText.tsx
@@ -3,6 +3,7 @@ import { useTranslation } from 'react-i18next'
 import styled from 'styled-components'
 
 import { ProjectConfigContext } from '../../project-configs/ProjectConfigContext'
+import { ProjectConfig } from '../../project-configs/getProjectConfig'
 import { ENTRY_LIMIT, FILE_SIZE_LIMIT_MEGA_BYTES } from './constants'
 
 const RequirementsList = styled.ul`
@@ -15,23 +16,24 @@ type ImportCardsRequirementsProps = {
   isFreinetFormat: boolean | undefined
 }
 
+export const getRequiredHeaders = (projectConfig: ProjectConfig): string[] => [
+  projectConfig.card.nameColumnName,
+  projectConfig.card.expiryColumnName,
+  ...projectConfig.card.extensions.flatMap((extension, index) => {
+    const header = projectConfig.card.extensionColumnNames[index]
+    return extension.isMandatory && header !== null ? [header] : []
+  }),
+]
+
 const ImportCardsRequirementsText = ({
   csvHeaders,
   isFreinetFormat = false,
 }: ImportCardsRequirementsProps): ReactElement => {
   const projectConfig = useContext(ProjectConfigContext)
   const { t } = useTranslation('cards')
-
-  const requiredHeaders = [
-    projectConfig.card.nameColumnName,
-    projectConfig.card.expiryColumnName,
-    ...projectConfig.card.extensions.flatMap((extension, index) => {
-      const header = projectConfig.card.extensionColumnNames[index]
-      return extension.isMandatory && header !== null ? [header] : []
-    }),
-  ]
-
-  const decoratedHeaders = csvHeaders.map(header => (requiredHeaders.includes(header) ? `${header}*` : header))
+  const decoratedHeaders = csvHeaders.map(header =>
+    getRequiredHeaders(projectConfig).includes(header) ? `${header}*` : header
+  )
 
   return (
     <RequirementsList>
