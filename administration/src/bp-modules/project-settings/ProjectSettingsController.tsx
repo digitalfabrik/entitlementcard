975059332e11b0ef7a7b0901a822cda55adaea07
André Wachter <andre.wachter@tuerantuer.org>
2025-07-03 12:01:43 +0200

    2305: Add scroll container for `<ProjectSettingsController>`

--- administration/src/bp-modules/project-settings/ProjectSettingsController.tsx
+++ administration/src/bp-modules/project-settings/ProjectSettingsController.tsx
@@ -1,20 +1,12 @@
+import { Stack } from '@mui/material'
 import React, { ReactElement, useContext } from 'react'
 import { useTranslation } from 'react-i18next'
-import styled from 'styled-components'
 
 import { useWhoAmI } from '../../WhoAmIProvider'
 import { Role } from '../../generated/graphql'
 import RenderGuard from '../../mui-modules/components/RenderGuard'
 import { ProjectConfigContext } from '../../project-configs/ProjectConfigContext'
 import ApiTokenSettings from './ApiTokenSettings'
-
-const ProjectSettingsContainer = styled.div`
-  display: flex;
-  width: 100%;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-`
 
 const ProjectSettingsController = (): ReactElement => {
   const { userImportApiEnabled } = useContext(ProjectConfigContext)
@@ -25,9 +17,9 @@ const ProjectSettingsController = (): ReactElement => {
 
   return (
     <RenderGuard condition={showProjectSettings} error={{ description: t('notAuthorizedForProjectSettings') }}>
-      <ProjectSettingsContainer>
+      <Stack sx={{ flexGrow: 1, justifyContent: 'safe center', alignItems: 'center', overflow: 'auto', padding: 4 }}>
         <ApiTokenSettings showPepperSection={isProjectAdminWithUserImportApiEnabled} />
-      </ProjectSettingsContainer>
+      </Stack>
     </RenderGuard>
   )
 }
