b3f907357683547e21a555797a89a1a3a1c56d6a
bahaa <bahaa.tuffaha@yahoo.com>
2025-07-14 20:11:11 +0300

    2339: Downgraded plugin serialization and kotlinx-serialization-json

--- frontend/android/buildSrc/build.gradle.kts
+++ frontend/android/buildSrc/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     `kotlin-dsl`
-    kotlin("plugin.serialization") version "2.2.0"
+    kotlin("plugin.serialization") version "2.0.21"
 }
 
 repositories {
@@ -9,5 +9,5 @@ repositories {
 
 dependencies {
     implementation(gradleApi())
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
 }
