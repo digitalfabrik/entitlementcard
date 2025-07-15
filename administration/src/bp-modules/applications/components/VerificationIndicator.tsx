413836201f140bd2743d4cf64a7cf5657cd1351c
André Wachter <andre.wachter@tuerantuer.org>
2025-07-15 13:56:35 +0200

    2235: Use MUI components for `<VerificationIndicator>`

--- administration/src/bp-modules/applications/components/VerificationIndicator.tsx
+++ administration/src/bp-modules/applications/components/VerificationIndicator.tsx
@@ -1,49 +1,39 @@
-import { Icon, Intent } from '@blueprintjs/core'
+/* eslint-disable react/destructuring-assignment */
+import { Cancel, CheckCircle, Help } from '@mui/icons-material'
+import { Box } from '@mui/material'
 import React, { ReactElement } from 'react'
-import styled from 'styled-components'
 
 import { VerificationStatus } from '../types'
 
-const StyledIndicator = styled.span`
-  display: inline-block;
-`
-
-type VerificationIndicatorProps = {
-  status: VerificationStatus
-  text?: string
-}
-
-const verifiedIcon = 'tick-circle'
-const rejectedIcon = 'cross-circle'
-const awaitingIcon = 'help'
-
-const getIconByStatus = (status: VerificationStatus): 'tick-circle' | 'cross-circle' | 'help' => {
-  switch (status) {
-    case VerificationStatus.Verified:
-      return verifiedIcon
-    case VerificationStatus.Pending:
-      return awaitingIcon
-    case VerificationStatus.Rejected:
-      return rejectedIcon
-  }
-}
-
-const getIntentByStatus = (status: VerificationStatus): Intent => {
+const colorByStatus = (status: VerificationStatus): 'success' | 'warning' | 'error' => {
   switch (status) {
     case VerificationStatus.Verified:
       return 'success'
     case VerificationStatus.Pending:
       return 'warning'
     case VerificationStatus.Rejected:
-      return 'danger'
+      return 'error'
   }
 }
 
-const VerificationIndicator = ({ status, text }: VerificationIndicatorProps): ReactElement => (
-  <StyledIndicator data-testid={`indicator-${status}`}>
-    <Icon icon={getIconByStatus(status)} intent={getIntentByStatus(status)} />
+const Icon = (p: { status: VerificationStatus }): ReactElement => {
+  const color = colorByStatus(p.status)
+
+  switch (p.status) {
+    case VerificationStatus.Verified:
+      return <CheckCircle color={color} />
+    case VerificationStatus.Pending:
+      return <Help color={color} />
+    case VerificationStatus.Rejected:
+      return <Cancel color={color} />
+  }
+}
+
+const VerificationIndicator = ({ status, text }: { status: VerificationStatus; text?: string }): ReactElement => (
+  <Box sx={{ display: 'inline-flex', alignItems: 'center' }} data-testid={`indicator-${status}`}>
+    <Icon status={status} />
     {text}
-  </StyledIndicator>
+  </Box>
 )
 
 export default VerificationIndicator
