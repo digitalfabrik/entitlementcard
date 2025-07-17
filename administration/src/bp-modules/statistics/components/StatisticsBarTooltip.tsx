90c568dd6ba11f8dde45649681ca05292439d085
Andy <fischer@integreat-app.de>
2025-07-17 11:03:23 +0200

    1760: Adjusted translation for no data, fixed height of color indicator, removed region name

--- administration/src/bp-modules/statistics/components/StatisticsBarTooltip.tsx
+++ administration/src/bp-modules/statistics/components/StatisticsBarTooltip.tsx
@@ -7,31 +7,38 @@ import { CardStatisticsResultModel } from '../../../generated/graphql'
 
 const ToolTipContainer = styled.div`
   background: white;
-  padding: 8px;
+  padding: 12px;
   border: 1px solid #b1b1b1;
   border-radius: 4px;
   display: flex;
   gap: 16px;
   align-items: center;
   box-shadow: 0 1px 3px rgb(0 0 0 / 10%), 0 1px 2px rgb(0 0 0 / 15%);
-  width: 200px;
+  width: 250px;
+  justify-content: space-between;
 `
 
-const ColorContainer = styled.span<{ color: string }>`
+const ColorContainer = styled.div<{ color: string }>`
   background-color: ${props => props.color};
   height: 32px;
-  width: 32px;
+  max-width: 32px;
+  flex: 1 1 32px;
+`
+
+const TextContainer = styled.div`
+  flex: 0 1 60%;
 `
 
 const ValueContainer = styled.span`
   font-weight: bold;
 `
 
-const StatisticsBarTooltip = ({ data, id, color, value }: BarTooltipProps<CardStatisticsResultModel>): ReactElement => {
+const StatisticsBarTooltip = ({ id, color, value }: BarTooltipProps<CardStatisticsResultModel>): ReactElement => {
   const { t } = useTranslation('statistics')
   return (
     <ToolTipContainer>
-      <ColorContainer color={color} /> {t(id.toString())} - {data.region}: <ValueContainer>{value}</ValueContainer>
+      <ColorContainer color={color} /> <TextContainer>{t(id.toString())}:</TextContainer>{' '}
+      <ValueContainer>{value}</ValueContainer>
     </ToolTipContainer>
   )
 }
