922782aea118f43859bfa97faa6e311131b1caf4
André Wachter <andre.wachter@tuerantuer.org>
2025-07-25 15:52:21 +0200

    2319: Add SQL for an undecided application

--- backend/src/dev/sql/applicationverifications.sql
+++ backend/src/dev/sql/applicationverifications.sql
@@ -5,7 +5,8 @@ VALUES
     (2,'contact@mail.org','Some Contact','Some Organization',NULL,current_timestamp,'access_key_02','NONE'),
     (3,'contact@mail.org','Some Contact','Some Organization',current_timestamp,NULL,'access_key_03','NONE'),
     (4,'contact@mail.org','Some Contact','Some Organization',NULL,current_timestamp,'access_key_04','NONE'),
-    (5,'contact@mail.org','Some Contact','Some Organization',NULL,NULL,'access_key_05','NONE')
+    (5,'contact@mail.org','Some Contact','Some Organization',NULL,NULL,'access_key_05','NONE'),
+    (11,'contact@mail.org','Some Contact','Some Organization',NULL,NULL,'access_key_06','NONE')
 ON CONFLICT ("accessKey") DO UPDATE SET
     "applicationId" = EXCLUDED."applicationId",
     "contactEmailAddress" = EXCLUDED."contactEmailAddress",
