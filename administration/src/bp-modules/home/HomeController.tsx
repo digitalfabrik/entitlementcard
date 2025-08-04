daf7d286510b14fcb9f18c5c649cefd731e1d086
André Wachter <andre.wachter@tuerantuer.org>
2025-08-04 13:50:38 +0200

    2305: Remove redundant button margins in `<HomeController>`

--- administration/src/bp-modules/home/HomeController.tsx
+++ administration/src/bp-modules/home/HomeController.tsx
@@ -3,16 +3,11 @@ import { Stack } from '@mui/material'
 import React, { ReactElement, useContext } from 'react'
 import { useTranslation } from 'react-i18next'
 import { NavLink } from 'react-router'
-import styled from 'styled-components'
 
 import { useWhoAmI } from '../../WhoAmIProvider'
 import { Role } from '../../generated/graphql'
 import RenderGuard from '../../mui-modules/components/RenderGuard'
 import { ProjectConfigContext } from '../../project-configs/ProjectConfigContext'
-
-const StyledButton = styled(Button)`
-  margin: 10px;
-`
 
 const HomeController = (): ReactElement => {
   const { applicationFeature, cardStatistics, cardCreation, userImportApiEnabled } = useContext(ProjectConfigContext)
@@ -25,42 +20,42 @@ const HomeController = (): ReactElement => {
       <RenderGuard allowedRoles={[Role.RegionAdmin, Role.RegionManager]}>
         {applicationFeature ? (
           <NavLink to='/applications'>
-            <StyledButton icon='form' text={t('incomingApplications')} />
+            <Button icon='form' text={t('incomingApplications')} />
           </NavLink>
         ) : null}
         {cardCreation ? (
           <NavLink to='/cards'>
-            <StyledButton icon='id-number' text={t('createCards')} />
+            <Button icon='id-number' text={t('createCards')} />
           </NavLink>
         ) : null}
       </RenderGuard>
       <RenderGuard allowedRoles={[Role.RegionAdmin, Role.ProjectAdmin]}>
         <NavLink to='/users'>
-          <StyledButton icon='people' text={t('administerUsers')} />
+          <Button icon='people' text={t('administerUsers')} />
         </NavLink>
         {cardStatistics.enabled ? (
           <NavLink to='/statistics'>
-            <StyledButton icon='stacked-chart' text={t('statistics')} />
+            <Button icon='stacked-chart' text={t('statistics')} />
           </NavLink>
         ) : null}
       </RenderGuard>
 
       <RenderGuard allowedRoles={[Role.RegionAdmin]} condition={applicationFeature !== undefined}>
         <NavLink to='/region'>
-          <StyledButton icon='path-search' text={t('administerRegions')} />
+          <Button icon='path-search' text={t('administerRegions')} />
         </NavLink>
       </RenderGuard>
 
       <RenderGuard
         condition={(role === Role.ProjectAdmin && userImportApiEnabled) || role === Role.ExternalVerifiedApiUser}>
         <NavLink to='/project'>
-          <StyledButton icon='projects' text={t('administerProjects')} />
+          <Button icon='projects' text={t('administerProjects')} />
         </NavLink>
       </RenderGuard>
 
       <RenderGuard allowedRoles={[Role.ProjectStoreManager]}>
         <NavLink to='/stores'>
-          <StyledButton icon='shop' text={t('administerStores')} />
+          <Button icon='shop' text={t('administerStores')} />
         </NavLink>
       </RenderGuard>
     </Stack>
