c85007b0b9cea416a2c010d27864cabe1d5acc72
tory <viktoria.seluianova@tuerantuer.org>
2025-08-06 16:33:36 +0200

    2353: Optimise getAuthContext() function to produce 1 db request instead of 2

--- backend/src/main/kotlin/app/ehrenamtskarte/backend/auth/Util.kt
+++ backend/src/main/kotlin/app/ehrenamtskarte/backend/auth/Util.kt
@@ -4,20 +4,21 @@ import app.ehrenamtskarte.backend.auth.database.AdministratorEntity
 import app.ehrenamtskarte.backend.auth.database.Administrators
 import app.ehrenamtskarte.backend.common.webservice.GraphQLContext
 import app.ehrenamtskarte.backend.exception.service.UnauthorizedException
-import org.jetbrains.exposed.dao.with
+import app.ehrenamtskarte.backend.projects.database.Projects
 import org.jetbrains.exposed.sql.transactions.transaction
 
 fun GraphQLContext.getAuthContext(): AuthContext {
     val jwtPayload = this.enforceSignedIn()
     return transaction {
-        val adminEntity = AdministratorEntity.find { Administrators.id eq jwtPayload.adminId }
-            .with(AdministratorEntity::project)
+        val row = (Administrators innerJoin Projects)
+            .select(Administrators.columns + Projects.columns)
+            .where { Administrators.id eq jwtPayload.adminId }
             .singleOrNull() ?: throw UnauthorizedException()
         AuthContext(
             adminId = jwtPayload.adminId,
-            admin = adminEntity,
-            projectId = adminEntity.projectId.value,
-            project = adminEntity.project.project,
+            admin = AdministratorEntity.wrapRow(row),
+            projectId = row[Projects.id].value,
+            project = row[Projects.project],
         )
     }
 }
