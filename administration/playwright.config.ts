b8c7f625b1386a627fbf91d0cb49f2d7fdd7bee1
tory <viktoria.seluianova@tuerantuer.org>
2025-08-30 13:55:35 +0200

    E2E tests: add screenshot on failure

--- administration/playwright.config.ts
+++ administration/playwright.config.ts
@@ -28,6 +28,7 @@ export default defineConfig({
     baseURL: 'http://localhost:3000',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    screenshot: 'only-on-failure'
   },
 
   /* Configure projects for major browsers */
