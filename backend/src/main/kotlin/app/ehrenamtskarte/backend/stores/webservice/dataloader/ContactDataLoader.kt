245122a5f248c52b2d9eeae5bfa6bffe493c6f96
André Wachter <andre.wachter@tuerantuer.org>
2025-09-02 17:20:32 +0200

    2447: Move `ContactsRepository` into `db.repositories`

--- backend/src/main/kotlin/app/ehrenamtskarte/backend/stores/webservice/dataloader/ContactDataLoader.kt
+++ backend/src/main/kotlin/app/ehrenamtskarte/backend/stores/webservice/dataloader/ContactDataLoader.kt
@@ -1,7 +1,7 @@
 package app.ehrenamtskarte.backend.stores.webservice.dataloader
 
 import app.ehrenamtskarte.backend.common.webservice.newNamedDataLoader
-import app.ehrenamtskarte.backend.stores.database.repos.ContactsRepository
+import app.ehrenamtskarte.backend.db.repositories.ContactsRepository
 import app.ehrenamtskarte.backend.stores.webservice.schema.types.Contact
 import org.jetbrains.exposed.sql.transactions.transaction
 
