2aaf97bbc264e8326fcd80f92e1f0700cc20b3d0
André Wachter <andre.wachter@tuerantuer.org>
2025-09-09 13:53:54 +0200

    2447: Move contents of `MapCsvToStore.kt` to `AcceptingStoresMutationService.kt`

--- backend/src/main/kotlin/app/ehrenamtskarte/backend/graphql/stores/AcceptingStoresMutationService.kt
+++ backend/src/main/kotlin/app/ehrenamtskarte/backend/graphql/stores/AcceptingStoresMutationService.kt
@@ -7,10 +7,12 @@ import app.ehrenamtskarte.backend.exception.service.ForbiddenException
 import app.ehrenamtskarte.backend.exception.webservice.exceptions.InvalidJsonException
 import app.ehrenamtskarte.backend.exception.webservice.exceptions.RegionNotUniqueException
 import app.ehrenamtskarte.backend.graphql.getAuthContext
+import app.ehrenamtskarte.backend.graphql.shared.context
 import app.ehrenamtskarte.backend.graphql.stores.schema.types.CSVAcceptingStore
 import app.ehrenamtskarte.backend.graphql.stores.schema.types.StoreImportReturnResultModel
-import app.ehrenamtskarte.backend.graphql.shared.context
-import app.ehrenamtskarte.backend.stores.utils.mapCsvToStore
+import app.ehrenamtskarte.backend.stores.COUNTRY_CODE
+import app.ehrenamtskarte.backend.stores.importer.common.types.AcceptingStore
+import app.ehrenamtskarte.backend.stores.importer.replaceNa
 import com.expediagroup.graphql.generator.annotations.GraphQLDescription
 import graphql.schema.DataFetchingEnvironment
 import org.jetbrains.exposed.dao.id.EntityID
@@ -79,4 +81,42 @@ class AcceptingStoresMutationService {
 
         return StoreImportReturnResultModel(numStoresCreated, acceptingStoreIdsToRemove.size, numStoresUntouched)
     }
+}
+
+fun mapCsvToStore(csvStore: CSVAcceptingStore): AcceptingStore {
+    val discount = listOf(csvStore.discountDE, csvStore.discountEN)
+        .filterNot { it.isNullOrEmpty() }
+        .joinToString("\n\n")
+    return AcceptingStore(
+        csvStore.name.clean()!!,
+        COUNTRY_CODE,
+        csvStore.location.clean()!!,
+        csvStore.postalCode.clean(),
+        csvStore.street.clean(),
+        csvStore.houseNumber.clean(),
+        additionalAddressInformation = "",
+        csvStore.longitude,
+        csvStore.latitude,
+        csvStore.categoryId,
+        csvStore.email.clean(false),
+        csvStore.telephone.clean(false),
+        csvStore.homepage.clean(false),
+        discount.clean(false),
+        freinetId = null,
+        districtName = null,
+    )
+}
+
+/** Returns null if string can't be trimmed f.e. empty string
+ * Removes subsequent whitespaces
+ * */
+fun String?.clean(removeSubsequentWhitespaces: Boolean = true): String? {
+    val trimmed = this?.replaceNa()?.trim()
+    if (trimmed.isNullOrEmpty()) {
+        return null
+    }
+    if (removeSubsequentWhitespaces) {
+        return trimmed.replace(Regex("""\s{2,}"""), " ")
+    }
+    return trimmed
 }
