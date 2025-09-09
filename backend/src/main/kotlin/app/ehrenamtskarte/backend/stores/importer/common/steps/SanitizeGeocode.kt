a45a36cd05f1e5ec3502eee86039eaca7ebd008d
André Wachter <andre.wachter@tuerantuer.org>
2025-09-09 13:31:10 +0200

    Move content of `Distance.kt` into `SanitizeGeocode.kt`

--- backend/src/main/kotlin/app/ehrenamtskarte/backend/stores/importer/common/steps/SanitizeGeocode.kt
+++ backend/src/main/kotlin/app/ehrenamtskarte/backend/stores/importer/common/steps/SanitizeGeocode.kt
@@ -2,8 +2,6 @@ package app.ehrenamtskarte.backend.stores.importer.common.steps
 
 import app.ehrenamtskarte.backend.stores.STREET_EXCLUDE_PATTERN
 import app.ehrenamtskarte.backend.stores.geocoding.FeatureFetcher
-import app.ehrenamtskarte.backend.stores.geocoding.isCloseToBoundingBox
-import app.ehrenamtskarte.backend.stores.geocoding.isInBoundingBox
 import app.ehrenamtskarte.backend.stores.importer.ImportConfig
 import app.ehrenamtskarte.backend.stores.importer.PipelineStep
 import app.ehrenamtskarte.backend.stores.importer.common.types.AcceptingStore
@@ -13,6 +11,8 @@ import kotlinx.coroutines.runBlocking
 import org.geojson.Feature
 import org.geojson.Point
 import org.slf4j.Logger
+import kotlin.math.cos
+import kotlin.math.max
 
 /**
  * Sanitize the postal code and the coordinates of the [AcceptingStore] using forward geocoding.
@@ -76,3 +76,40 @@ class SanitizeGeocode(config: ImportConfig, private val logger: Logger, httpClie
     private fun Feature.address(): LinkedHashMap<String, String> =
         this.getProperty<LinkedHashMap<String, String>>("address")
 }
+
+
+private const val DISTANCE_THRESHOLD_IN_KM = 1.0
+
+/**
+ * Returns whether the accepting store is closer than [DISTANCE_THRESHOLD_IN_KM] to the bounding box of the [feature]
+ */
+private fun AcceptingStore.isCloseToBoundingBox(feature: Feature): Boolean {
+    if (latitude == null || longitude == null) return false
+    return isCloseTo(feature.bbox, latitude, longitude, DISTANCE_THRESHOLD_IN_KM)
+}
+
+/**
+ * Returns whether the accepting store is positioned inside the bounding box [bbox]
+ */
+private fun AcceptingStore.isInBoundingBox(bbox: DoubleArray): Boolean {
+    if (latitude == null || longitude == null) return false
+    return bbox[0] <= longitude && longitude <= bbox[2] && bbox[1] <= latitude && latitude <= bbox[3]
+}
+
+/**
+ * Returns whether [latitude] and [longitude] are closer than [thresholdInKm] to the bounding box [bbox]
+ */
+private fun isCloseTo(bbox: DoubleArray, latitude: Double, longitude: Double, thresholdInKm: Double): Boolean {
+    // Constants taken from https://stackoverflow.com/a/1253545/3245533
+    val kmPerLatitudeDegree = 110.574
+    val kmPerLongitudeDegree = 111.320 * cos(latitude.degreesToRadians())
+
+    val deltaLatitude = thresholdInKm / kmPerLatitudeDegree
+    val deltaLongitude = thresholdInKm / kmPerLongitudeDegree
+
+    val longitudeInBounds = max(bbox[0] - longitude, longitude - bbox[2]) <= deltaLongitude
+    val latitudeInBounds = max(bbox[1] - latitude, latitude - bbox[3]) <= deltaLatitude
+    return longitudeInBounds && latitudeInBounds
+}
+
+private fun Double.degreesToRadians(): Double = this * Math.PI / 180
