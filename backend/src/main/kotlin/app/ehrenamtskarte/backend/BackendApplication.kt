1320d04b36636214cb0b16a9240c267bf3c8c52e
tory <viktoria.seluianova@tuerantuer.org>
2025-09-16 16:19:42 +0200

    2513: Migrate HealthController.kt to SpringBoot

--- backend/src/main/kotlin/app/ehrenamtskarte/backend/BackendApplication.kt
+++ backend/src/main/kotlin/app/ehrenamtskarte/backend/BackendApplication.kt
@@ -1,11 +1,6 @@
 package app.ehrenamtskarte.backend
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
 
 @SpringBootApplication
 class BackendApplication
-
-fun main(args: Array<String>) {
-	runApplication<BackendApplication>(*args)
-}
