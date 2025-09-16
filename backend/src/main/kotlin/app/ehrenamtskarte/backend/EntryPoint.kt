9e485d1c72ce552279f2d1fad4e39e112d509399
tory <viktoria.seluianova@tuerantuer.org>
2025-09-16 16:18:07 +0200

    2449: Refactor configuration loading

--- backend/src/main/kotlin/app/ehrenamtskarte/backend/EntryPoint.kt
+++ backend/src/main/kotlin/app/ehrenamtskarte/backend/EntryPoint.kt
@@ -1,6 +1,7 @@
 package app.ehrenamtskarte.backend
 
 import app.ehrenamtskarte.backend.config.BackendConfiguration
+import app.ehrenamtskarte.backend.config.ConfigurationLoader
 import app.ehrenamtskarte.backend.config.Environment
 import app.ehrenamtskarte.backend.db.Database
 import app.ehrenamtskarte.backend.db.entities.Migrations
@@ -23,26 +24,11 @@ import org.jetbrains.exposed.sql.exists
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.slf4j.LoggerFactory
 import org.springframework.boot.runApplication
-import org.springframework.context.support.beans
 import java.io.File
-import java.nio.file.Path
-import java.nio.file.Paths
 import java.sql.SQLException
 import java.util.TimeZone
-import kotlin.io.path.exists
 
 private val logger = LoggerFactory.getLogger("EntryPoint")
-
-private val defaultConfigFilePaths: List<Path> = listOf(
-    Paths.get(System.getProperty("user.dir"), "config.yml"),
-    Paths.get(System.getProperty("user.home"), ".config", "entitlementcard", "config.yml"),
-    Paths.get("/etc/entitlementcard/config.yml"),
-)
-
-private val defaultConfigResourceUrls: List<String> = listOf(
-    "config/config.local.yml",
-    "config/config.yml",
-)
 
 class Entry : CliktCommand() {
     private val config by option().file(canBeDir = false, mustBeReadable = true)
@@ -57,29 +43,12 @@ class Entry : CliktCommand() {
     private val geocodingHost by option()
 
     override fun run() {
-        val backendConfiguration = BackendConfiguration.load(
-            config?.let {
-                logger.info("Load backend configuration from explicit config file '$it'.")
-                it.toURI().toURL()
-            }
-                ?: defaultConfigFilePaths.firstNotNullOfOrNull {
-                    if (it.exists()) {
-                        logger.info("Load backend configuration from implicit config file '$it'.")
-                        it.toUri().toURL()
-                    } else {
-                        null
-                    }
-                }
-                ?: defaultConfigResourceUrls.firstNotNullOfOrNull {
-                    ClassLoader.getSystemResource(it)?.also { url ->
-                        logger.info("Load default backend configuration from resource '$url'.")
-                    }
-                }
-                ?: run {
-                    logger.error("No backend configuration found, this is a build error.")
-                    throw ProgramResult(statusCode = 4)
-                },
-        )
+        val backendConfiguration = try {
+            ConfigurationLoader.load(config)
+        } catch (e: IllegalStateException) {
+            logger.error(e.message)
+            throw ProgramResult(statusCode = 4)
+        }
 
         currentContext.obj = backendConfiguration.copy(
             environment = environment ?: backendConfiguration.environment,
@@ -131,8 +100,8 @@ class DbImportDev : CliktCommand("db-import-dev") {
 }
 
 class GraphQLExport : CliktCommand("graphql-export") {
-    //private val config by requireObject<BackendConfiguration>()
-    //private val path by argument(help = "Export GraphQL schema. Given ")
+    private val config by requireObject<BackendConfiguration>()
+    private val path by argument(help = "Export GraphQL schema. Given ")
 
     override fun help(context: Context): String = "Exports the GraphQL schema into the directory given by '--path'"
 
@@ -202,15 +171,7 @@ class Execute : CliktCommand() {
 
     override fun run() {
         Database.setupWithInitialDataAndMigrationChecks(config)
-        runApplication<BackendApplication> {
-            addInitializers(
-                beans {
-                    bean("backendConfiguration") {
-                        config
-                    }
-                }
-            )
-        }
+        runApplication<BackendApplication>()
     }
 }
 
