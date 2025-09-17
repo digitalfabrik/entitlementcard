36bfbef00fd5489dab9895a7bb2d4fc5184658c7
tory <viktoria.seluianova@tuerantuer.org>
2025-09-17 16:05:26 +0200

    2450: Setup integration test configuration

---
+++ backend/src/test/kotlin/app/ehrenamtskarte/backend/IntegrationTestConfig.kt
@@ -0,0 +1,16 @@
+package app.ehrenamtskarte.backend
+
+import app.ehrenamtskarte.backend.config.BackendConfiguration
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.context.annotation.Bean
+
+@TestConfiguration
+class IntegrationTestConfig {
+
+    @Bean
+    fun backendConfiguration(): BackendConfiguration {
+        val resource = ClassLoader.getSystemResource("config.test.yml")
+            ?: throw Exception("Configuration resource 'src/test/resources/config.test.yml' not found")
+        return BackendConfiguration.load(resource)
+    }
+}
