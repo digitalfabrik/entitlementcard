1b460f68b42a05924b5b82f0671c191702e76472
tory <viktoria.seluianova@tuerantuer.org>
2025-09-17 16:05:26 +0200

    2450: Setup integration test configuration

--- backend/src/test/kotlin/app/ehrenamtskarte/backend/IntegrationTest.kt
+++ backend/src/test/kotlin/app/ehrenamtskarte/backend/IntegrationTest.kt
@@ -1,22 +1,25 @@
 package app.ehrenamtskarte.backend
 
-import app.ehrenamtskarte.backend.config.BackendConfiguration
 import app.ehrenamtskarte.backend.config.PostgresConfig
 import app.ehrenamtskarte.backend.db.Database
 import app.ehrenamtskarte.backend.db.migration.MigrationUtils
 import app.ehrenamtskarte.backend.helper.TestAdministrators
 import app.ehrenamtskarte.backend.helper.TestFreinetAgencies
 import org.junit.jupiter.api.BeforeAll
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ContextConfiguration
 import org.testcontainers.containers.PostgreSQLContainer
 import org.testcontainers.utility.DockerImageName
 
 /**
  * Base class for integration tests that require a database connection
  */
-open class IntegrationTest {
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ContextConfiguration(classes = [IntegrationTestConfig::class])
+class IntegrationTest {
     companion object {
         private var postgisImage = DockerImageName.parse("postgis/postgis:13-3.0-alpine")
-        private var postgisContainer = PostgreSQLContainer(
+        internal var postgisContainer = PostgreSQLContainer(
             postgisImage.asCompatibleSubstituteFor("postgres"),
         )
 
@@ -26,9 +29,6 @@ open class IntegrationTest {
         }
 
         @JvmStatic
-        protected val config = loadTestConfig()
-
-        @JvmStatic
         @BeforeAll
         fun loadTestData() {
             TestAdministrators.createAll()
@@ -36,23 +36,16 @@ open class IntegrationTest {
         }
 
         private fun setupDatabase() {
-            val config = loadTestConfig()
-                .copy(
-                    postgres = PostgresConfig(
-                        postgisContainer.jdbcUrl,
-                        postgisContainer.username,
-                        postgisContainer.password,
-                    ),
-                )
+            val config = IntegrationTestConfig().backendConfiguration().copy(
+                postgres = PostgresConfig(
+                    postgisContainer.jdbcUrl,
+                    postgisContainer.username,
+                    postgisContainer.password,
+                ),
+            )
             val database = Database.setupWithoutMigrationCheck(config)
             MigrationUtils.applyRequiredMigrations(database)
             Database.setupWithInitialDataAndMigrationChecks(config)
-        }
-
-        private fun loadTestConfig(): BackendConfiguration {
-            val resource = ClassLoader.getSystemResource("config.test.yml")
-                ?: throw Exception("Configuration resource 'src/test/resources/config.test.yml' not found")
-            return BackendConfiguration.load(resource)
         }
     }
 }
