e2a0aa698e511c6c1e1fedbb3363e89511023b7e
Andy <fischer@integreat-app.de>
2025-09-23 19:16:52 +0200

    2477: create PopoverWrapper.tsx with a basic popover config using mui popover, use the wrapper for application status and role explanation

---
+++ administration/src/mui-modules/components/PopoverWrapper.tsx
@@ -0,0 +1,40 @@
+import { IconButton, Popover } from '@mui/material'
+import React, { ReactElement } from 'react'
+
+type PopoverWrapperProps = {
+  icon: ReactElement
+  children: ReactElement
+}
+
+const PopoverWrapper = ({ children, icon }: PopoverWrapperProps): ReactElement => {
+  const [anchorElement, setAnchorElement] = React.useState<HTMLButtonElement | null>(null)
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorElement(event.currentTarget)
+  }
+  const handleClose = () => {
+    setAnchorElement(null)
+  }
+
+  return (
+    <>
+      <Popover
+        open={Boolean(anchorElement)}
+        anchorEl={anchorElement}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'center',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'center',
+        }}>
+        {children}
+      </Popover>
+      <IconButton onClick={handleClick}>{icon}</IconButton>
+    </>
+  )
+}
+
+export default PopoverWrapper
