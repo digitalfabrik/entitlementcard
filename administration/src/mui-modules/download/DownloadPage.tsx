72092d00948d95c480ead614ee893ca56529c43f
Antonio Grieco <infotoni91@gmail.com>
2025-09-29 17:36:51 +0200

    Made changes according to PR comments

--- administration/src/mui-modules/download/DownloadPage.tsx
+++ administration/src/mui-modules/download/DownloadPage.tsx
@@ -1,5 +1,5 @@
 import { Box, Typography } from '@mui/material'
-import { styled } from '@mui/system'
+import MUILink from '@mui/material/Link'
 import React, { ReactElement, useContext } from 'react'
 import { useTranslation } from 'react-i18next'
 import { Link } from 'react-router'
@@ -7,29 +7,6 @@ import { Link } from 'react-router'
 import AppStoreLinks from '../../bp-modules/components/AppStoreLinks'
 import { ProjectConfigContext } from '../../project-configs/ProjectConfigContext'
 import { getBuildConfig } from '../../util/getBuildConfig'
-
-const AppIcon = styled('img')({
-  height: 100,
-  width: 100,
-  border: '1px solid rgba(0, 0, 0, 0.2)',
-  borderRadius: '20%',
-  marginBottom: '1rem',
-})
-
-const linkStyle = {
-  color: 'inherit',
-  textDecoration: 'none',
-  fontSize: '1rem',
-}
-
-const StyledAnchorLink = styled('a')({
-  ...linkStyle,
-  textDecoration: 'underline',
-})
-
-const StyledLink = styled(Link)({
-  ...linkStyle,
-})
 
 const DownloadPage = (): ReactElement => {
   const { t: regionSettingsTranslations } = useTranslation('regionSettings')
@@ -40,18 +17,46 @@ const DownloadPage = (): ReactElement => {
 
   return (
     <Box display='flex' flexDirection='column' alignItems='center' mx='auto' mt={10}>
-      <AppIcon alt={`${config.name} app icon`} src={`/icons/${config.projectId}.png`} aria-hidden />
-      <Typography variant='h5' component='h1'>
+      <Box
+        component='img'
+        sx={{
+          height: 100,
+          aspectRatio: 1,
+          marginBottom: '1rem',
+          borderRadius: '20%',
+          border: 1,
+          borderColor: 'grey.300',
+        }}
+        alt={`${config.name} app icon`}
+        src={`/icons/${config.projectId}.png`}
+        aria-hidden
+      />
+      <Typography variant='h5' component='h1' fontWeight={600}>
         {config.name}
       </Typography>
-      <StyledAnchorLink href='https://tuerantuer.de/digitalfabrik'>Tür an Tür - Digitalfabrik gGmbH</StyledAnchorLink>
-      <Typography variant='h6' component='h2' mt={6}>
+      <MUILink
+        href='https://tuerantuer.de/digitalfabrik'
+        target='_blank'
+        rel='noreferrer'
+        color='textPrimary'
+        underline='always'>
+        Tür an Tür - Digitalfabrik gGmbH
+      </MUILink>
+      <Typography variant='h5' component='h2' mt={6}>
         {activationTranslations('downloadNow')}
       </Typography>
       <AppStoreLinks playStoreLink={android.appStoreLink} appStoreLink={ios.appStoreLink} />
       <Box display='flex' flexWrap='wrap' justifyContent='center' gap={2}>
-        <StyledLink to='/imprint'>Impressum</StyledLink>
-        <StyledLink to='/data-privacy-policy'>{regionSettingsTranslations('dataPrivacy')}</StyledLink>
+        <Link to='/imprint'>
+          <MUILink color='textPrimary' underline='none'>
+            Impressum
+          </MUILink>
+        </Link>
+        <Link to='/data-privacy-policy'>
+          <MUILink color='textPrimary' underline='none'>
+            {regionSettingsTranslations('dataPrivacy')}
+          </MUILink>
+        </Link>
       </Box>
     </Box>
   )
