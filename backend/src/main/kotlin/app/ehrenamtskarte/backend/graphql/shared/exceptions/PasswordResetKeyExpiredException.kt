926e8bcaf8fbe8c2c6a4420eba9fa1e3fa3e9a41
tory <viktoria.seluianova@tuerantuer.org>
2025-10-08 15:45:17 +0200

    WIP

--- backend/src/main/kotlin/app/ehrenamtskarte/backend/graphql/shared/exceptions/PasswordResetKeyExpiredException.kt
+++
@@ -1,7 +0,0 @@
-package app.ehrenamtskarte.backend.graphql.shared.exceptions
-
-import app.ehrenamtskarte.backend.graphql.shared.types.GraphQLExceptionCode
-
-class PasswordResetKeyExpiredException : GraphQLBaseException(
-    GraphQLExceptionCode.PASSWORD_RESET_KEY_EXPIRED,
-)
