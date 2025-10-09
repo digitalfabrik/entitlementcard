207e2c39f1cb3d26b6d275c5456f7361b7aaf198
Andy <fischer@integreat-app.de>
2025-10-09 14:16:37 +0200

    2535: add docs to LinkBehavior.tsx

--- administration/src/mui-modules/util/LinkBehavior.tsx
+++ administration/src/mui-modules/util/LinkBehavior.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link, LinkProps } from 'react-router'
 
+// https://mui.com/material-ui/integrations/routing/#link
 const LinkBehavior = React.forwardRef<HTMLAnchorElement, Omit<LinkProps, 'to'> & { href: LinkProps['to'] }>(
   (props, ref) => {
     const { href, ...other } = props
