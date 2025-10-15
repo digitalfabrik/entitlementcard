5365625ee014c45fb49853b70e5c176e1d8b440f
André Wachter <andre.wachter@tuerantuer.org>
2025-10-15 12:45:01 +0200

    2513: Improve code style in `MapStyleController.kt`

--- backend/src/main/kotlin/app/ehrenamtskarte/backend/routes/MapStyleController.kt
+++ backend/src/main/kotlin/app/ehrenamtskarte/backend/routes/MapStyleController.kt
@@ -37,44 +37,53 @@ class MapStyleController(
 
     private fun initializeStyles(): Map<String, String> {
         val mapper = ObjectMapper()
+        val baseUrl = config.map.baseUrl
+        val style = loadStyle() ?: throw Error("Map style 'style.json' is missing!")
+
         return config.projects.associate { project ->
-            val style = loadStyle() ?: throw Error("Map style 'style.json' is missing!")
-            val newStyle = patchStyle(style) { id: String, source: JsonNode ->
-                val newSource = source.deepCopy<ObjectNode>()
-                val baseUrl = config.map.baseUrl
-                if (id == "physical_stores") {
-                    val tiles = ArrayNode(JsonNodeFactory.instance)
-                    tiles.add(baseUrl + "?project_id=" + project.id)
-                    newSource.replace("tiles", tiles)
-                } else if (id == "physical_stores_clustered") {
-                    val tiles = ArrayNode(JsonNodeFactory.instance)
-                    tiles.add(baseUrl + "?clustered=true&project_id=" + project.id)
-                    newSource.replace("tiles", tiles)
+            val newStyle = patchSources(style) { id: String, source: JsonNode ->
+                id to source.deepCopy<ObjectNode>().also {
+                    when (id) {
+                        "physical_stores" -> {
+                            it.replace(
+                                "tiles",
+                                ArrayNode(JsonNodeFactory.instance)
+                                    .add("$baseUrl?project_id=${project.id}"),
+                            )
+                        }
+                        "physical_stores_clustered" -> {
+                            it.replace(
+                                "tiles",
+                                ArrayNode(JsonNodeFactory.instance)
+                                    .add("$baseUrl?clustered=true&project_id=${project.id}"),
+                            )
+                        }
+                    }
                 }
-                Pair(id, newSource)
             }
-            val json = mapper.writeValueAsString(newStyle) ?: throw Error("Failed to create style.json")
-            Pair(project.id, json)
+
+            project.id to (mapper.writeValueAsString(newStyle) ?: throw Error("Failed to create style.json"))
         }
     }
 
-    private fun patchStyle(
-        style: JsonNode,
-        patcher: (id: String, source: JsonNode) -> Pair<String, JsonNode>,
-    ): JsonNode? {
-        val sources = style["sources"] ?: return null
-        val newSources = sources.properties().asSequence().map {
-            patcher(it.key, it.value)
-        }.toMap()
-        val newSourcesObject = ObjectNode(JsonNodeFactory.instance, newSources)
-        val newStyle = style.deepCopy<ObjectNode>() ?: return null
-        newStyle.replace("sources", newSourcesObject)
-        return newStyle
-    }
+    private fun patchSources(
+        style: ObjectNode,
+        patcher: (id: String, source: ObjectNode) -> Pair<String, JsonNode>,
+    ): ObjectNode =
+        style.also { styleDocument ->
+            styleDocument.replace(
+                "sources",
+                ObjectNode(
+                    JsonNodeFactory.instance,
+                    style["sources"].properties().asSequence().mapNotNull {
+                        (it.value as? ObjectNode)?.let { value -> patcher(it.key, value) }
+                    }.toMap(),
+                ),
+            )
+        }
 
-    private fun loadStyle(): JsonNode? =
-        ObjectMapper().let { mapper ->
-            val resource = ClassLoader.getSystemResource("styles/style.json")
-            mapper.readTree(resource.openStream().buffered())
+    private fun loadStyle(): ObjectNode? =
+        ClassLoader.getSystemResource("styles/style.json").openStream().buffered().use {
+            ObjectMapper().readTree(it) as? ObjectNode
         }
 }
