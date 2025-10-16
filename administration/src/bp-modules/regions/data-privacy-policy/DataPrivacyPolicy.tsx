2640477137d81856a16626b6884db7e924e26711
Antonio Grieco <infotoni91@gmail.com>
2025-10-16 23:11:56 +0200

    2482: Replace styled-components with MUI `Stack` in DataPrivacyPolicy

--- administration/src/bp-modules/regions/data-privacy-policy/DataPrivacyPolicy.tsx
+++ administration/src/bp-modules/regions/data-privacy-policy/DataPrivacyPolicy.tsx
@@ -1,25 +1,16 @@
-import { Typography } from '@mui/material'
+import { Stack, Typography } from '@mui/material'
 import React, { ReactElement, useContext } from 'react'
-import styled from 'styled-components'
 
 import { ProjectConfigContext } from '../../../project-configs/ProjectConfigContext'
-
-const Container = styled.div`
-  max-width: 750px;
-  margin: 10px;
-  display: flex;
-  flex-direction: column;
-  align-self: center;
-`
 
 const DataPrivacyPolicy = (): ReactElement => {
   const config = useContext(ProjectConfigContext)
   return (
-    <Container>
+    <Stack maxWidth='750px' margin={1} alignSelf='center'>
       <Typography variant='h4'>{config.dataPrivacyHeadline}</Typography>
       <config.dataPrivacyContent />
       {config.dataPrivacyAdditionalBaseContent && <config.dataPrivacyAdditionalBaseContent />}
-    </Container>
+    </Stack>
   )
 }
 
