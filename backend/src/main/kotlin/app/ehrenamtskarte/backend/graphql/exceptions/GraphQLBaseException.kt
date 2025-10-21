2f8a3e5016ac5a2fe3bc36998798c40a898fe222
tory <viktoria.seluianova@tuerantuer.org>
2025-10-21 14:45:10 +0200

    2451: Refactor GraphQLExceptionHandler to correspond previous CustomDataFetcherExceptionHandler used with Javalin

--- backend/src/main/kotlin/app/ehrenamtskarte/backend/graphql/exceptions/GraphQLBaseException.kt
+++ backend/src/main/kotlin/app/ehrenamtskarte/backend/graphql/exceptions/GraphQLBaseException.kt
@@ -3,28 +3,26 @@ package app.ehrenamtskarte.backend.graphql.exceptions
 import app.ehrenamtskarte.backend.graphql.shared.types.GraphQLExceptionCode
 import graphql.ErrorType
 import graphql.GraphQLError
-import graphql.execution.ResultPath
-import graphql.language.SourceLocation
+import graphql.schema.DataFetchingEnvironment
+import java.lang.RuntimeException
 
 open class GraphQLBaseException(
     val code: GraphQLExceptionCode,
     val extraExtensions: Map<String, Any> = emptyMap(),
     val errorType: ErrorType = ErrorType.DataFetchingException,
     override val message: String = "Error $code occurred.",
-) : RuntimeException(message) {
+) : RuntimeException() {
     val extensions: Map<String, Any> = buildMap {
         put("code", code)
         putAll(extraExtensions)
     }
 
-    fun toGraphQLError(path: ResultPath? = null, location: SourceLocation? = null): GraphQLError =
+    fun toGraphQLError(env: DataFetchingEnvironment? = null): GraphQLError =
         GraphQLError.newError()
             .errorType(errorType)
             .message(message)
             .extensions(extensions)
-            .apply {
-                path?.let { path(it) }
-                location?.let { location(it) }
-            }
+            .path(env?.executionStepInfo?.path)
+            .location(env?.field?.sourceLocation)
             .build()
 }
