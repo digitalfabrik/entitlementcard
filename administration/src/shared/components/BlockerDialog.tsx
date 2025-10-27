413b365137950f036f76112f0a8e6ac81f098981
André Wachter <andre.wachter@tuerantuer.org>
2025-10-27 14:32:50 +0100

    2575: Use standard `useBlocker()` for blocking navigation

---
+++ administration/src/shared/components/BlockerDialog.tsx
@@ -0,0 +1,30 @@
+/* eslint-disable react/destructuring-assignment */
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@mui/material'
+import React, { ReactElement } from 'react'
+import { useTranslation } from 'react-i18next'
+import type { Blocker } from 'react-router'
+
+export const BlockerDialog = (p: { title: string; message: string; blocker: Blocker }): ReactElement => {
+  const { t } = useTranslation('misc')
+
+  return (
+    <Dialog
+      open={p.blocker.state === 'blocked'}
+      onClose={p.blocker.reset}
+      aria-labelledby='alert-dialog-title'
+      aria-describedby='alert-dialog-description'>
+      <DialogTitle id='alert-dialog-title'>{p.title}</DialogTitle>
+      <DialogContent>
+        <DialogContentText id='alert-dialog-description' variant='body2'>
+          {p.message}
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={p.blocker.reset}>{t('cancel')}</Button>
+        <Button onClick={p.blocker.proceed} color='primary' variant='contained' autoFocus>
+          {t('confirm')}
+        </Button>
+      </DialogActions>
+    </Dialog>
+  )
+}
