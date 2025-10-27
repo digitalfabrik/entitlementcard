5a5e1c124acef62f37201a6b1590f452637e8457
André Wachter <andre.wachter@tuerantuer.org>
2025-10-27 14:32:50 +0100

    2575: Use standard `useBlocker()` for blocking navigation

--- administration/src/bp-modules/cards/ImportCardsController.tsx
+++ administration/src/bp-modules/cards/ImportCardsController.tsx
@@ -1,12 +1,12 @@
 import React, { ReactElement } from 'react'
 import { useTranslation } from 'react-i18next'
-import { useNavigate } from 'react-router'
+import { useBlocker, useNavigate } from 'react-router'
 
 import { useWhoAmI } from '../../WhoAmIProvider'
 import { Region, Role } from '../../generated/graphql'
 import CenteredCircularProgress from '../../mui-modules/base/CenteredCircularProgress'
 import RenderGuard from '../../mui-modules/components/RenderGuard'
-import useBlockNavigation from '../../util/useBlockNavigation'
+import { BlockerDialog } from '../../shared/components/BlockerDialog'
 import GenerationFinished from './CardsCreatedMessage'
 import CreateCardsButtonBar from './CreateCardsButtonBar'
 import ImportCardsInput from './ImportCardsInput'
@@ -19,10 +19,9 @@ const InnerImportCardsController = ({ region }: { region: Region }): ReactElemen
   const navigate = useNavigate()
   const { t } = useTranslation('cards')
 
-  useBlockNavigation({
-    when: cards.length > 0,
-    message: t('dataWillBeLostWarning'),
-  })
+  const blocker = useBlocker(
+    ({ currentLocation, nextLocation }) => currentLocation.pathname !== nextLocation.pathname && cards.length > 0
+  )
 
   const goBack = () => {
     if (!cards.length) {
@@ -32,36 +31,40 @@ const InnerImportCardsController = ({ region }: { region: Region }): ReactElemen
     }
   }
 
-  if (cardGenerationStep === 'loading') {
-    return <CenteredCircularProgress />
+  switch (cardGenerationStep) {
+    case 'loading':
+      return <CenteredCircularProgress />
+    case 'finished':
+      return (
+        <GenerationFinished
+          reset={() => {
+            setCards([])
+            setCardGenerationStep('input')
+          }}
+        />
+      )
+    case 'input':
+      return (
+        <>
+          {cards.length === 0 ? (
+            <ImportCardsInput setCards={setCards} region={region} />
+          ) : (
+            <CardImportTable cards={cards} />
+          )}
+          <CreateCardsButtonBar
+            cards={cards}
+            goBack={goBack}
+            generateCardsPdf={() => generateCardsPdf()}
+            generateCardsCsv={() => generateCardsCsv()}
+          />
+          <BlockerDialog
+            title={t('shared:warningDialogTitle')}
+            message={t('dataWillBeLostWarning')}
+            blocker={blocker}
+          />
+        </>
+      )
   }
-
-  if (cardGenerationStep === 'finished') {
-    return (
-      <GenerationFinished
-        reset={() => {
-          setCards([])
-          setCardGenerationStep('input')
-        }}
-      />
-    )
-  }
-
-  return (
-    <>
-      {cards.length === 0 ? (
-        <ImportCardsInput setCards={setCards} region={region} />
-      ) : (
-        <CardImportTable cards={cards} />
-      )}
-      <CreateCardsButtonBar
-        cards={cards}
-        goBack={goBack}
-        generateCardsPdf={() => generateCardsPdf()}
-        generateCardsCsv={() => generateCardsCsv()}
-      />
-    </>
-  )
 }
 
 const ImportCardsController = (): ReactElement => {
