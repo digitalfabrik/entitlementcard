de0efdcc1087c4d1de7281e3166fb50f9229a436
Andy <fischer@integreat-app.de>
2025-11-05 15:38:14 +0100

    2470: add StoreListTable.tsx

---
+++ administration/src/bp-modules/stores/StoreListTable.tsx
@@ -0,0 +1,98 @@
+import MoreVertRoundedIcon from '@mui/icons-material/MoreVertRounded'
+import { Box, IconButton, Link, SxProps } from '@mui/material'
+import { DataGrid, GridColDef } from '@mui/x-data-grid'
+import { TFunction } from 'i18next'
+import React, { ReactElement } from 'react'
+import { useTranslation } from 'react-i18next'
+
+import { AcceptingStoresData } from '../applications/types'
+
+const customToolBarStyles: SxProps = {
+  '.MuiButtonBase-root': {
+    '&[aria-label="Exportieren"], &[aria-label="Filter"], &[aria-label="Spalten"]': {
+      display: 'none',
+    },
+  },
+  '.MuiDataGrid-toolbar': { '.MuiDivider-vertical': { display: 'none' } },
+}
+
+// TODO 2471 Implement Menu and enable cell
+const EditCell = (): ReactElement => (
+  <IconButton aria-label='edit-cell'>
+    <MoreVertRoundedIcon />
+  </IconButton>
+)
+const EMailCell = ({ email }: { email: string }): ReactElement => <Link href={`mailto:${email}`}>{email}</Link>
+
+const columns = (t: TFunction): GridColDef[] => [
+  {
+    field: 'id',
+    headerName: '',
+    width: 60,
+    renderCell: () => <EditCell />,
+  },
+  {
+    field: 'name',
+    headerName: t('storeListHeadlineName'),
+    width: 250,
+  },
+  {
+    field: 'category',
+    headerName: t('storeListHeadlineCategory'),
+    width: 200,
+    valueGetter: (_, row: AcceptingStoresData) => row.category.name,
+  },
+  {
+    field: 'address',
+    headerName: t('storeListHeadlineAddress'),
+    width: 300,
+    valueGetter: (_, row: AcceptingStoresData) =>
+      row.physicalStore
+        ? `${row.physicalStore.address.street}, ${row.physicalStore.address.postalCode} ${row.physicalStore.address.location}`
+        : '',
+  },
+  {
+    field: 'telephone',
+    headerName: t('storeListHeadlineTelephone'),
+    width: 160,
+    valueGetter: (_, row: AcceptingStoresData) => row.contact.telephone,
+  },
+  {
+    field: 'email',
+    headerName: t('storeListHeadlineEmail'),
+    width: 250,
+    renderCell: ({ row }: { row: AcceptingStoresData }) =>
+      row.contact.email ? <EMailCell email={row.contact.email} /> : undefined,
+    valueGetter: (_, row: AcceptingStoresData) => row.contact.email,
+  },
+]
+
+const StoreListTable = ({ data }: { data: AcceptingStoresData[] }): ReactElement => {
+  const { t } = useTranslation('stores')
+
+  return (
+    <Box sx={{ height: '75vh', width: '100%' }}>
+      <DataGrid
+        rows={data}
+        showToolbar
+        sx={{
+          '.MuiTablePagination-selectLabel': { m: 0 },
+          '.MuiTablePagination-displayedRows': { m: 0 },
+          ...customToolBarStyles,
+        }}
+        columns={columns(t)}
+        initialState={{
+          sorting: {
+            sortModel: [{ field: 'name', sort: 'asc' }],
+          },
+        }}
+        disableRowSelectionOnClick
+        hideFooterPagination
+        hideFooter
+        disableColumnMenu
+      />
+    </Box>
+  )
+}
+
+export default StoreListTable
