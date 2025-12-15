04b8e2b0b5420ab9cda5a41795cda400b2794377
André Wachter <andre.wachter@tuerantuer.org>
2025-12-15 14:57:39 +0100

    Define build constants in vite

--- administration/vite.config.ts
+++ administration/vite.config.ts
@@ -1,16 +1,43 @@
-import type { UserConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { defineConfig, UserConfig } from 'vite'
+import { execFile } from 'node:child_process'
+import { join } from 'node:path'
 
-export default {
-  appType: 'spa',
-  plugins: [
-    react()
-  ],
-  build: {
-    assetsInlineLimit: 10000,
-    target: '>0.2% in DE and not dead'
-  },
-  server: {
-    port: 3000
-  }
-} satisfies UserConfig
+const execFileAsync = promisify(execFile)
+
+export default defineConfig(async () => {
+  // eslint-disable-next-line @typescript-eslint/no-use-before-define
+  const commitHash = await currentCommitHash()
+
+  return {
+    appType: 'spa',
+    plugins: [
+      react()
+    ],
+    build: {
+      assetsInlineLimit: 10000
+    },
+    define: {
+      VITE_BUILD_API_BASE_URL: JSON.stringify('http://localhost:8000'),
+      VITE_BUILD_VERSION_NAME: JSON.stringify('DEV'),
+      VITE_BUILD_COMMIT: JSON.stringify(commitHash)
+    },
+    preview: {
+      open: true,
+    },
+    server: {
+      port: 3000,
+    },
+  } satisfies UserConfig
+})
+
+// eslint-disable-next-line prefer-arrow/prefer-arrow-functions
+async function currentCommitHash() {
+  return (
+    await execFileAsync('git', ['log', '-n1', '--format=%h'], {
+      cwd: join(__dirname, '..'),
+      encoding: 'utf8',
+    })
+  ).stdout
+}
