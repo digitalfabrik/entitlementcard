03d5cedabb08040df029d86e112593f0437ed967
André Wachter <andre.wachter@tuerantuer.org>
2025-12-15 14:57:39 +0100

    Define build constants in vite

--- administration/vite.config.ts
+++ administration/vite.config.ts
@@ -1,16 +1,77 @@
-import type { UserConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/* eslint-disable import/no-extraneous-dependencies */
 
-export default {
-  appType: 'spa',
-  plugins: [
-    react()
-  ],
-  build: {
-    assetsInlineLimit: 10000,
-    target: '>0.2% in DE and not dead'
-  },
-  server: {
-    port: 3000
+/* eslint-disable @typescript-eslint/no-use-before-define */
+
+/* eslint-disable prefer-arrow/prefer-arrow-functions */
+
+/* eslint-disable no-else-return */
+import react from '@vitejs/plugin-react'
+import { execFile } from 'node:child_process'
+import { join } from 'node:path'
+import { env } from 'node:process'
+import { promisify } from 'node:util'
+import { UserConfig, defineConfig } from 'vite'
+
+import versionJson from '../version.json'
+
+const execFileAsync = promisify(execFile)
+
+type ViteMode = 'development' | 'production'
+
+export default defineConfig(async configEnv => {
+  const commitHash = await currentCommitHash()
+
+  return {
+    appType: 'spa',
+    plugins: [react()],
+    build: {
+      assetsInlineLimit: 10000,
+    },
+    define: {
+      VITE_BUILD_API_BASE_URL: JSON.stringify(apiBaseUrl(configEnv.mode as ViteMode)),
+      VITE_BUILD_VERSION_NAME: JSON.stringify(versionName(configEnv.mode as ViteMode)),
+      VITE_BUILD_COMMIT: JSON.stringify(commitHash),
+    },
+    preview: {
+      open: true,
+    },
+    server: {
+      port: 3000,
+    },
+  } satisfies UserConfig
+})
+
+function apiBaseUrl(mode: ViteMode): string | undefined {
+  if (mode === 'development') {
+    console.log('vite.config.ts: VITE_BUILD_API_BASE_URL="http://localhost:8000"')
+  } else {
+    console.log('vite.config.ts: VITE_BUILD_API_BASE_URL=undefined (use release constants)')
+    return undefined
   }
-} satisfies UserConfig
+}
+
+function versionName(mode: ViteMode): string {
+  if (mode === 'development') {
+    console.log('vite.config.ts: VITE_BUILD_VERSION_NAME="development"')
+    return 'development'
+  } else if (env.NEW_VERSION_NAME) {
+    console.log(
+      `vite.config.ts: VITE_BUILD_VERSION_NAME="${env.NEW_VERSION_NAME}" (from env variable NEW_VERSION_NAME)`,
+    )
+    return env.NEW_VERSION_NAME
+  } else {
+    console.log(
+      `vite.config.ts: VITE_BUILD_VERSION_NAME="${versionJson.versionName}" (from ../version.json)`,
+    )
+    return versionJson.versionName
+  }
+}
+
+async function currentCommitHash() {
+  return (
+    await execFileAsync('git', ['log', '-n1', '--format=%h'], {
+      cwd: join(__dirname, '..'),
+      encoding: 'utf8',
+    })
+  ).stdout
+}
