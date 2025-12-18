9996b6c0b97c886b45b12ce3d869766310e47d4d
André Wachter <andre.wachter@tuerantuer.org>
2025-12-18 12:38:08 +0100

    Generate asset files for projects with vite

--- administration/src/project-configs/getDeeplinkingConfigs.ts
+++
@@ -1,9 +0,0 @@
-import { DeeplLinkingConfig, buildConfigBayern, buildConfigKoblenz, buildConfigNuernberg } from 'build-configs'
-
-const getDeepLinkingConfigs = (): DeeplLinkingConfig[] => [
-  buildConfigBayern.common.deepLinking,
-  buildConfigNuernberg.common.deepLinking,
-  buildConfigKoblenz.common.deepLinking,
-]
-
-export default getDeepLinkingConfigs
--- administration/vite.config.ts
+++ administration/vite.config.ts
@@ -1,30 +1,43 @@
+/* eslint-disable import/no-extraneous-dependencies */
+
+/* eslint-disable prefer-arrow/prefer-arrow-functions */
+
+/* eslint-disable @typescript-eslint/no-use-before-define */
 import react from '@vitejs/plugin-react'
-// eslint-disable-next-line import/no-extraneous-dependencies
-import tsconfigPaths from 'vite-tsconfig-paths'
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { defineConfig, UserConfig } from 'vite'
+import { type DeeplLinkingConfig, buildConfigBayern, buildConfigKoblenz, buildConfigNuernberg } from 'build-configs'
 import { execFile } from 'node:child_process'
 import { join } from 'node:path'
+import { promisify } from 'node:util'
+import { UserConfig, defineConfig } from 'vite'
+import generateFile, { GenerateFile } from 'vite-plugin-generate-file'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 const execFileAsync = promisify(execFile)
 
 export default defineConfig(async () => {
-  // eslint-disable-next-line @typescript-eslint/no-use-before-define
   const commitHash = await currentCommitHash()
 
   return {
     appType: 'spa',
     plugins: [
       react(),
-      tsconfigPaths()
+      tsconfigPaths(),
+      generateFile(
+        buildFilesFromConfigs([
+          buildConfigBayern.common.deepLinking,
+          buildConfigNuernberg.common.deepLinking,
+          buildConfigKoblenz.common.deepLinking,
+        ])
+      ),
     ],
     build: {
-      assetsInlineLimit: 10000
+      outDir: 'build',
+      assetsInlineLimit: 10000,
     },
     define: {
       VITE_BUILD_API_BASE_URL: JSON.stringify('http://localhost:8000'),
       VITE_BUILD_VERSION_NAME: JSON.stringify('DEV'),
-      VITE_BUILD_COMMIT: JSON.stringify(commitHash)
+      VITE_BUILD_COMMIT: JSON.stringify(commitHash),
     },
     preview: {
       open: true,
@@ -35,7 +48,6 @@ export default defineConfig(async () => {
   } satisfies UserConfig
 })
 
-// eslint-disable-next-line prefer-arrow/prefer-arrow-functions
 async function currentCommitHash() {
   return (
     await execFileAsync('git', ['log', '-n1', '--format=%h'], {
@@ -43,4 +55,44 @@ async function currentCommitHash() {
       encoding: 'utf8',
     })
   ).stdout
+}
+
+function buildFilesFromConfigs(projectLinkinConfigs: DeeplLinkingConfig[]): GenerateFile[] {
+  return projectLinkinConfigs.flatMap(config => [
+    {
+      type: 'json',
+      output: join(config.projectName, 'assetlinks.json'),
+      // https://developer.android.com/training/app-links/verify-android-applinks#web-assoc
+      data: [
+        {
+          relation: ['delegate_permission/common.handle_all_urls'],
+          target: {
+            namespace: 'android_app',
+            package_name: config.android.applicationId,
+            sha256_cert_fingerprints: [config.android.sha256CertFingerprint],
+          },
+        },
+      ],
+    },
+    {
+      type: 'json',
+      output: join(config.projectName, 'apple-app-site-association'),
+      data: {
+        applinks: {
+          apps: [],
+          details: [
+            {
+              appID: config.ios.appleAppSiteAssociationAppId,
+              components: [
+                {
+                  '/': config.ios.path,
+                  comment: config.ios.pathComment,
+                },
+              ],
+            },
+          ],
+        },
+      },
+    },
+  ])
 }
