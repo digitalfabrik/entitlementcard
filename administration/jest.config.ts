9fa2e39458436fc125597d8edcec230b1d3189a4
André Wachter <andre.wachter@tuerantuer.org>
2026-01-13 11:04:44 +0100

    WIP

--- administration/jest.config.ts
+++ administration/jest.config.ts
@@ -9,7 +9,26 @@ const config: JestConfigWithTsJest = {
   testEnvironment: 'jsdom',
   verbose: true,
   automock: false,
+  extensionsToTreatAsEsm: ['.ts', '.tsx'],
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+        tsconfig: {
+          verbatimModuleSyntax: false,
+          module: 'esnext',
+          moduleResolution: 'bundler',
+          target: 'esnext',
+        },
+      },
+    ],
+  },
   moduleNameMapper: {
+    '^build-configs$': '<rootDir>/../../build-configs/src/index.ts',
+    // Strips .js extensions from imports, allowing Jest to resolve TypeScript files when the code
+    // imports with .js extensions
+    '^(\\.{1,2}/.*)\\.js$': '$1',
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|pdf)$':
       '<rootDir>/__mocks__/fileMock.ts',
     '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.ts',
