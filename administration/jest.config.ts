7b7383518bb1e588a6d0f2e05c1cfb1b883bd2b3
André Wachter <andre.wachter@tuerantuer.org>
2026-01-14 14:24:12 +0100

    Fix jest compatibility for ESM modules

--- administration/jest.config.ts
+++ administration/jest.config.ts
@@ -1,5 +1,4 @@
-import type { JestConfigWithTsJest } from 'ts-jest'
-import { createDefaultEsmPreset } from 'ts-jest'
+import { type JestConfigWithTsJest, createDefaultEsmPreset } from 'ts-jest'
 
 process.env.TZ = 'GMT'
 
@@ -9,6 +8,35 @@ const config: JestConfigWithTsJest = {
   testEnvironment: 'jsdom',
   verbose: true,
   automock: false,
+  transform: {
+    // Also process Js files, so we can support 'build-configs'.
+    '^.+\\.(tsx?|js)$': [
+      'ts-jest',
+      {
+        diagnostics: {
+          ignoreCodes: [1343],
+        },
+        astTransformers: {
+          before: [
+            {
+              // https://www.npmjs.com/package/ts-jest-mock-import-meta
+              path: 'ts-jest-mock-import-meta',
+              options: {
+                metaObjectReplacement: {
+                  env: {
+                    BASE_URL: '/',
+                    DEV: false,
+                    MODE: 'test',
+                    PROD: false,
+                  },
+                },
+              },
+            },
+          ],
+        },
+      },
+    ],
+  },
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|pdf)$':
       '<rootDir>/__mocks__/fileMock.ts',
