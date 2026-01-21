b4f6513802971cf5bce527559ff2dfc76835961f
André Wachter <andre.wachter@tuerantuer.org>
2026-01-21 12:47:25 +0100

    2825: Gather element functions in `elements.ts`

--- administration/src/project-configs/bayern/pdf.ts
+++ administration/src/project-configs/bayern/pdf.ts
@@ -1,4 +1,4 @@
-import type { InfoParams } from '../../cards/pdf/pdfTextElement'
+import type { InfoParams } from '../../cards/pdf/elements'
 import { BavariaCardType } from '../../generated/card_pb'
 import PlainDate from '../../util/PlainDate'
 import type { PdfConfig } from '../getProjectConfig'
