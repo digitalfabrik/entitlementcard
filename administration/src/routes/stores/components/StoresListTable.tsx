2601a01f78654285e880af5dd20cff6db5b12498
André Wachter <andre.wachter@tuerantuer.org>
2026-01-29 13:52:43 +0100

    2498: Inline code in `<StoresListTable>`

--- administration/src/routes/stores/components/StoresListTable.tsx
+++ administration/src/routes/stores/components/StoresListTable.tsx
@@ -1,6 +1,5 @@
 import { Box, Link, SxProps } from '@mui/material'
-import { DataGrid, GridColDef } from '@mui/x-data-grid'
-import { TFunction } from 'i18next'
+import { DataGrid } from '@mui/x-data-grid'
 import React, { ReactElement } from 'react'
 import { useTranslation } from 'react-i18next'
 
@@ -20,50 +19,6 @@ const customToolBarStyles: SxProps = {
 const EMailCell = ({ email }: { email: string }): ReactElement => (
   <Link href={`mailto:${email}`}>{email}</Link>
 )
-const columns = (t: TFunction, editStore: (storeId: number) => void): GridColDef[] => [
-  {
-    field: 'id',
-    headerName: '',
-    width: 60,
-    renderCell: ({ row }: { row: AcceptingStoresData }) => (
-      <TableMenu storeId={row.id} onEditStore={editStore} />
-    ),
-  },
-  {
-    field: 'name',
-    headerName: t('storesListColumnName'),
-    width: 250,
-  },
-  {
-    field: 'category',
-    headerName: t('storesListColumnCategory'),
-    width: 200,
-    valueGetter: (_, row: AcceptingStoresData) => row.category.name,
-  },
-  {
-    field: 'address',
-    headerName: t('storesListColumnAddress'),
-    width: 300,
-    valueGetter: (_, row: AcceptingStoresData) =>
-      row.physicalStore
-        ? `${row.physicalStore.address.street}, ${row.physicalStore.address.postalCode} ${row.physicalStore.address.location}`
-        : '',
-  },
-  {
-    field: 'telephone',
-    headerName: t('storesListColumnTelephone'),
-    width: 160,
-    valueGetter: (_, row: AcceptingStoresData) => row.contact.telephone,
-  },
-  {
-    field: 'email',
-    headerName: t('storesListColumnEmail'),
-    width: 250,
-    renderCell: ({ row }: { row: AcceptingStoresData }) =>
-      row.contact.email ? <EMailCell email={row.contact.email} /> : undefined,
-    valueGetter: (_, row: AcceptingStoresData) => row.contact.email,
-  },
-]
 
 const StoresListTable = ({
   data,
@@ -84,7 +39,6 @@ const StoresListTable = ({
           '.MuiTablePagination-displayedRows': { m: 0 },
           ...customToolBarStyles,
         }}
-        columns={columns(t, editStore)}
         initialState={{
           columns: {
             columnVisibilityModel: {
@@ -98,6 +52,48 @@ const StoresListTable = ({
         }}
         disableRowSelectionOnClick
         disableColumnMenu
+        columns={[
+          {
+            field: 'id',
+            headerName: '',
+            width: 60,
+            renderCell: ({ row }) => <TableMenu storeId={row.id} onEditStore={editStore} />,
+          },
+          {
+            field: 'name',
+            headerName: t('storesListColumnName'),
+            width: 250,
+          },
+          {
+            field: 'category',
+            headerName: t('storesListColumnCategory'),
+            width: 200,
+            valueGetter: (_, row: AcceptingStoresData) => row.category.name,
+          },
+          {
+            field: 'address',
+            headerName: t('storesListColumnAddress'),
+            width: 300,
+            valueGetter: (_, row: AcceptingStoresData) =>
+              row.physicalStore
+                ? `${row.physicalStore.address.street}, ${row.physicalStore.address.postalCode} ${row.physicalStore.address.location}`
+                : '',
+          },
+          {
+            field: 'telephone',
+            headerName: t('storesListColumnTelephone'),
+            width: 160,
+            valueGetter: (_, row: AcceptingStoresData) => row.contact.telephone,
+          },
+          {
+            field: 'email',
+            headerName: t('storesListColumnEmail'),
+            width: 250,
+            renderCell: ({ row }) =>
+              row.contact.email ? <EMailCell email={row.contact.email} /> : undefined,
+            valueGetter: (_, row: AcceptingStoresData) => row.contact.email,
+          },
+        ]}
       />
     </Box>
   )
