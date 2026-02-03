635a0f2b2af71f1af26453fe184902aedc0464c9
tory <viktoria.seluianova@tuerantuer.org>
2026-02-03 16:56:53 +0100

    2696: Do not show loading widget when we get store summary from cache

--- frontend/lib/map/preview/accepting_store_preview.dart
+++ frontend/lib/map/preview/accepting_store_preview.dart
@@ -3,6 +3,7 @@ import 'package:ehrenamtskarte/graphql_gen/graphql_queries/stores/accepting_stor
 import 'package:ehrenamtskarte/map/preview/accepting_store_preview_card.dart';
 import 'package:ehrenamtskarte/map/preview/models.dart';
 import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 
 class AcceptingStorePreview extends StatelessWidget {
   final int physicalStoreId;
@@ -15,6 +16,7 @@ class AcceptingStorePreview extends StatelessWidget {
 
     return Query$AcceptingStoresByPhysicalStoreIds$Widget(
       options: Options$Query$AcceptingStoresByPhysicalStoreIds(
+        fetchPolicy: FetchPolicy.cacheAndNetwork,
         variables: Variables$Query$AcceptingStoresByPhysicalStoreIds(
           project: projectId,
           physicalStoreIds: [physicalStoreId],
@@ -30,18 +32,19 @@ class AcceptingStorePreview extends StatelessWidget {
 
           final fetchedData = result.parsedData;
 
-          if (result.isLoading || fetchedData == null) {
+          if (result.isLoading && fetchedData == null) {
             return const AcceptingStorePreviewCard(isLoading: true);
+          }
+
+          if (fetchedData == null) {
+            throw Exception('Fetched data is null.');
           }
 
           final stores = fetchedData.stores;
           if (stores.length != 1) {
             throw Exception('Server unexpectedly returned an array of the wrong size.');
           }
-          final store = stores.firstOrNull;
-          if (store == null) {
-            throw Exception('ID not found.');
-          }
+          final store = stores.first;
           return AcceptingStorePreviewCard(isLoading: false, acceptingStore: AcceptingStoreModel.fromGraphql(store));
         } on Exception catch (e) {
           debugPrint(e.toString());
