dbb44855f73276f92cd102f1f3a9bbed29555f70
André Wachter <andre.wachter@tuerantuer.org>
2026-02-11 13:01:10 +0100

    2825: Move and simplify QR drawing code

--- administration/src/cards/pdf/pdfFactory.ts
+++ administration/src/cards/pdf/pdfFactory.ts
@@ -72,7 +72,7 @@ const fillContentAreas = async (
   }
 
   pdfConfig.elements?.dynamicActivationQrCodes.forEach(configOptions =>
-    pdfQrCodeElement(configOptions, { page, qrCode: dynamicCode }),
+    pdfQrCodeElement(page, dynamicCode, configOptions),
   )
 
   if (pdfConfig.elements?.deepLinkArea) {
@@ -96,7 +96,7 @@ const fillContentAreas = async (
       }
 
       pdfConfig.elements.staticVerificationQrCodes.forEach(configOptions =>
-        pdfQrCodeElement(configOptions, { page, qrCode: staticCode }),
+        pdfQrCodeElement(page, staticCode, configOptions),
       )
     } else {
       throw Error(
