290d13a65b7184c4ad3a660d26aa16b52faf3671
André Wachter <andre.wachter@tuerantuer.org>
2026-02-25 12:46:34 +0100

    2832: Fix logging in  `exportApplicationToCsv()`

--- administration/src/routes/applications/utils/exportApplicationToCsv.ts
+++ administration/src/routes/applications/utils/exportApplicationToCsv.ts
@@ -59,13 +59,13 @@ export const exportApplicationToCsv = (application: Application, config: Project
     downloadDataUri(blob, `${surname}_${forenames}_${creationDate}.csv`)
   } catch (error) {
     if (error instanceof ApplicationToCsvError) {
-      console.error(error.message)
       throw new ApplicationToCsvError(i18next.t('errors:applicationToCsvError'))
     } else if (error instanceof ApplicationDataIncompleteError) {
-      console.error(error.message)
       throw new ApplicationDataIncompleteError(
         i18next.t('errors:applicationDataIncompleteException'),
       )
+    } else {
+      console.error(error)
     }
   }
 }
