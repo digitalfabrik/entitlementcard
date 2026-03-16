4a7ef3c68e074368776a500deed98c18d815b839
Andy <fischer@integreat-app.de>
2026-03-16 14:20:43 +0100

    2678: use Stack instead of Box, make container accessible via containerSx

--- administration/src/routes/antrag-einsehen/ApplicationApplicantController.tsx
+++ administration/src/routes/antrag-einsehen/ApplicationApplicantController.tsx
@@ -1,11 +1,10 @@
-import { Stack } from '@mui/material'
+import { StackProps } from '@mui/material'
 import React, { ReactElement, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { useParams } from 'react-router'
 import { Temporal } from 'temporal-polyfill'
 
 import AlertBox from '../../components/AlertBox'
-import CenteredStack from '../../components/CenteredStack'
 import PageLayout from '../../components/PageLayout'
 import { ApplicationStatus, useGetApplicationByApplicantQuery } from '../../generated/graphql'
 import getQueryResult from '../../util/getQueryResult'
@@ -16,6 +15,12 @@ import {
   parseApplication,
 } from '../applications/utils/application'
 import ApplicationApplicantView from './ApplicationApplicantView'
+
+const centeredContainerSx: StackProps['sx'] = {
+  alignSelf: 'center',
+  justifyContent: 'center',
+  p: 2,
+}
 
 const ApplicationApplicantController = ({ providedKey }: { providedKey: string }): ReactElement => {
   const { t } = useTranslation('applicationApplicant')
@@ -28,59 +33,50 @@ const ApplicationApplicantController = ({ providedKey }: { providedKey: string }
     return applicationQueryHandler.component
   }
   const application = parseApplication(applicationQueryHandler.data.application)
-
   if (application.status === ApplicationStatus.Withdrawn) {
     return (
-      <PageLayout>
-        <CenteredStack>
-          <AlertBox severity='info' description={t('alreadyWithdrawn')} />
-        </CenteredStack>
+      <PageLayout containerSx={centeredContainerSx}>
+        <AlertBox severity='info' description={t('alreadyWithdrawn')} />
       </PageLayout>
     )
   }
   if (isWithdrawn) {
     return (
-      <PageLayout>
-        <CenteredStack>
-          <AlertBox severity='info' description={t('withdrawConfirmation')} />
-        </CenteredStack>
+      <PageLayout containerSx={centeredContainerSx}>
+        <AlertBox severity='info' description={t('withdrawConfirmation')} />
       </PageLayout>
     )
   }
 
   if (applicationWasAlreadyProcessed(application.status) && !!application.statusResolvedDate) {
     return (
-      <PageLayout>
-        <CenteredStack>
-          <AlertBox
-            severity={getAlertSeverityByApplicationStatus(application.status)}
-            title={t(
-              application.status === ApplicationStatus.Rejected
-                ? 'titleStatusRejected'
-                : 'titleStatusApproved',
-            )}
-            description={
-              <ApplicationStatusNote
-                statusResolvedDate={Temporal.Instant.from(application.statusResolvedDate)}
-                status={application.status}
-                showIcon={false}
-              />
-            }
-          />
-        </CenteredStack>
+      <PageLayout containerSx={centeredContainerSx}>
+        <AlertBox
+          severity={getAlertSeverityByApplicationStatus(application.status)}
+          title={t(
+            application.status === ApplicationStatus.Rejected
+              ? 'titleStatusRejected'
+              : 'titleStatusApproved',
+          )}
+          description={
+            <ApplicationStatusNote
+              statusResolvedDate={Temporal.Instant.from(application.statusResolvedDate)}
+              status={application.status}
+              showIcon={false}
+            />
+          }
+        />
       </PageLayout>
     )
   }
 
   return (
-    <PageLayout>
-      <Stack sx={{ alignSelf: 'center', justifyContent: 'flex-start', flexGrow: 1 }}>
-        <ApplicationApplicantView
-          application={application}
-          onWithdraw={() => setIsWithdrawn(true)}
-          providedKey={providedKey}
-        />
-      </Stack>
+    <PageLayout containerSx={{ alignSelf: 'center', justifyContent: 'flex-start' }}>
+      <ApplicationApplicantView
+        application={application}
+        onWithdraw={() => setIsWithdrawn(true)}
+        providedKey={providedKey}
+      />
     </PageLayout>
   )
 }
