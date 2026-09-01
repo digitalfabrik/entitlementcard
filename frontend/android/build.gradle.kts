8dd18ddebf55c611c92ae2c86d547c827dec6771
Andy <fischer@integreat-app.de>
2026-09-01 13:54:15 +0200

    1462: add workaround to build.gradle.kts

--- frontend/android/build.gradle.kts
+++ frontend/android/build.gradle.kts
@@ -12,6 +12,13 @@ allprojects {
 subprojects {
     project.layout.buildDirectory.set(file("${rootProject.layout.buildDirectory.get()}/${project.name}"))
     project.evaluationDependsOn(":app")
+
+    // maplibre_gl 0.27.0 only applies the Kotlin plugin itself below AGP 9, assuming AGP 9+
+    // https://github.com/maplibre/flutter-maplibre-gl/pull/914
+    // This workaround is required until https://github.com/digitalfabrik/entitlementcard/issues/3126
+    if (project.name == "maplibre_gl") {
+        pluginManager.apply("org.jetbrains.kotlin.android")
+    }
 }
 
 tasks.register<Delete>("clean") {
